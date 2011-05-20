4debdc5e5d1fd06f4d56f3d3ea2f8bcf2a1b6f0f
Ben Word <ben@benword.com>
2011-05-19 19:09:42 -0600

    h5bp update: new conditional comment magic for the <html> tag, more info at http://goo.gl/MmY2F

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if lt IE 9]>      <html class="no-js oldie" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+
 <head>
 	<meta charset="utf-8">
 
