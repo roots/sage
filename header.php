eab54c9adbdbc6e47921bc403cb90218fd705a5f
Ben Word <ben@benword.com>
2011-06-01 07:57:14 -0600

    h5bp replaced the semicolon with a comma in the viewport line back in october, somehow missed this -- thx james sims for pointing it out

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 	<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
 	
-	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<?php echo get_roots_stylesheets(); ?>
 	
