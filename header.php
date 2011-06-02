22e0a2eebe402de49963e18f34fdcfc8276fb863
Ben Word <ben@benword.com>
2011-06-02 11:59:12 -0600

    (h5bp updates) modernizr 2.0, newer respond.js

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 	
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 
-	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-1.7.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-2.0.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/respond.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.1.min.js">\x3C/script>')</script>
