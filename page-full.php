7695922d93c93595e0ba42923e8ab715e4b6da16
Mason Stewart <masondesu@gmail.com>
2011-06-27 17:30:41 -0400

    #main had the wrong class on page-full.php. Fixed

--- page-full.php
+++ page-full.php
@@ -6,7 +6,7 @@ get_header(); ?>
 	<?php roots_content_before(); ?>
 		<div id="content" class="<?php echo roots_container_class; ?>">	
 		<?php roots_main_before(); ?>
-			<div id="main" class="<?php echo roots_container_class; ?>" role="main">
+			<div id="main" class="<?php echo $roots_options['main_class']; ?>" role="main">
 				<div class="container">
 					<?php roots_loop_before(); ?>
 					<?php get_template_part('loop', 'page'); ?>
