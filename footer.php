d92a532ad2f381c19315dd9aa6d45ca0dc74eda5
Ben Word <ben@benword.com>
2011-06-26 18:24:45 -0600

    (h5bp updates) chrome frame install prompt for ie7, small change to the escaping of </script> for jquery local fallback

--- footer.php
+++ footer.php
@@ -9,7 +9,14 @@
 		</footer>
 		<?php roots_footer_after(); ?>	
 	</div><!-- /#wrap -->
+
 <?php wp_footer(); ?>
 <?php roots_footer(); ?>
+
+	<!--[if lt IE 7]>
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+		<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+	<![endif]-->
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-2.0.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/respond.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.1.min.js">\x3C/script>')</script>
+	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.1.min.js"><\/script>')</script>
 
 	<?php wp_head(); ?>
 	<?php roots_head(); ?>
