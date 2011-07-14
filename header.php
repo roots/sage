206cb0b1fc4d60790bfc6e2812336f2a1fadad3e
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2011-07-14 11:22:22 -0600

    Updated to jQuery 1.6.2

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
 
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-2.0.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/respond.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.1.min.js"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
 
 	<?php wp_head(); ?>
 	<?php roots_head(); ?>
