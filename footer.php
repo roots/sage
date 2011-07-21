dd77a89b342b526e4b6538209169f7075518cf93
Ben Word <ben@benword.com>
2011-07-20 19:22:18 -0600

    h5bp updates

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 <?php roots_footer(); ?>
 
 	<!--[if lt IE 7]>
-		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
-		<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 	<![endif]-->
 
 </body>
