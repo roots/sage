ef721e7f6ec53b6dfaa78ded9ad10c66745e4cbe
Ben Word <ben@benword.com>
2011-08-12 11:05:29 -0600

    h5bp updates - now up to date with h5bp 2.0

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 
 	<?php roots_stylesheets(); ?>
 	
