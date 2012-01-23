3c9bc21e323be6a2456fe98a8c499f87ebaeb942
Ben Word <ben@benword.com>
2012-01-23 10:55:36 -0700

    removing custom image header for logo - closes #223

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
       <div class="container">
 
         <a id="logo" href="<?php echo home_url(); ?>/">
-          <img src="<?php echo get_header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="<?php bloginfo('name'); ?>">
+          <img src="http://placehold.it/300x75" width="300" height="75" alt="<?php bloginfo('name'); ?>">
         </a>
 
         <nav id="nav-main" role="navigation">
