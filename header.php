01696fde63a799868fffb31b7c30e3b22802c3dc
Ben Word <ben@benword.com>
2012-01-29 18:24:19 -0700

    move roots_head above wp_head, fixes #260

--- header.php
+++ header.php
@@ -18,8 +18,8 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
-  <?php wp_head(); ?>
   <?php roots_head(); ?>
+  <?php wp_head(); ?>
 
 </head>
 
