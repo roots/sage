63256f4667fc047281ddc5a704a03181adb43416
Ben Word <ben@benword.com>
2012-02-29 17:33:27 -0700

    Fix #300 - Add comment-reply script when applicable

--- inc/roots-scripts.php
+++ inc/roots-scripts.php
@@ -6,6 +6,10 @@ function roots_scripts() {
     wp_register_script('jquery', '', '', '', false);
   }
 
+  if (is_single() && comments_open() && get_option('thread_comments')) {
+    wp_enqueue_script('comment-reply');
+  }
+
   wp_register_script('roots_plugins', get_template_directory_uri() . '/js/plugins.js', false, null, false);
   wp_register_script('roots_script', get_template_directory_uri() . '/js/script.js', false, null, false);
   wp_enqueue_script('roots_plugins');
