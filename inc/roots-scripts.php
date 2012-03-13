c90d5ef9163d60201813ab6b58de22b78d15cc5d
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2012-03-12 20:18:43 -0400

    Proper base script URLs for WPMU/Child Themes

--- inc/roots-scripts.php
+++ inc/roots-scripts.php
@@ -10,8 +10,14 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_register_script('roots_plugins',  '/js/plugins.js', false, null, false);
-  wp_register_script('roots_script', '/js/script.js', false, null, false);
+  if (is_multisite() || is_child_theme()) {
+    $base = get_template_directory_uri();
+  } else {
+    $base = '';
+  }
+
+  wp_register_script('roots_plugins', $base . '/js/plugins.js', false, null, false);
+  wp_register_script('roots_script', $base . '/js/script.js', false, null, false);
   wp_enqueue_script('roots_plugins');
   wp_enqueue_script('roots_script');
 }
