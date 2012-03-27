c118e05637833788a5d060233d6fbf999408f9cb
Yulian <yul.yordanov@post-scriptum.info>
2012-03-27 18:08:35 +0300

    Added default WP-core comment form. Removed the unnecessary code from the old form.

--- comments.php
+++ comments.php
@@ -72,28 +72,13 @@
 
 <?php if (comments_open()) { ?>
   <section id="respond">
-    <h3><?php comment_form_title(__('Leave a Reply', 'roots'), __('Leave a Reply to %s', 'roots')); ?></h3>
-    <p class="cancel-comment-reply"><?php cancel_comment_reply_link(); ?></p>
     <?php if (get_option('comment_registration') && !is_user_logged_in()) { ?>
       <p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.', 'roots'), wp_login_url(get_permalink())); ?></p>
     <?php } else { ?>
-      <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-        <?php if (is_user_logged_in()) { ?>
-          <p><?php printf(__('Logged in as <a href="%s/wp-admin/profile.php">%s</a>.', 'roots'), get_option('siteurl'), $user_identity); ?> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php __('Log out of this account', 'roots'); ?>"><?php _e('Log out &raquo;', 'roots'); ?></a></p>
-        <?php } else { ?>
-          <label for="author"><?php _e('Name', 'roots'); if ($req) _e(' (required)', 'roots'); ?></label>
-          <input type="text" class="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?>>
-          <label for="email"><?php _e('Email (will not be published)', 'roots'); if ($req) _e(' (required)', 'roots'); ?></label>
-          <input type="email" class="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?>>
-          <label for="url"><?php _e('Website', 'roots'); ?></label>
-          <input type="url" class="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3">
-        <?php } ?>
-        <label for="comment"><?php _e('Comment', 'roots'); ?></label>
-        <textarea name="comment" id="comment" class="input-xlarge" tabindex="4"></textarea>
-        <input name="submit" class="btn btn-primary" type="submit" id="submit" tabindex="5" value="<?php _e('Submit Comment', 'roots'); ?>">
-        <?php comment_id_fields(); ?>
-        <?php do_action('comment_form', $post->ID); ?>
-      </form>
+    <?php $roots_comment_args = array(
+		'label_submit'         => __( 'Submit Comment', 'roots' ),
+		'comment_notes_after'  => '' ); 
+	comment_form( $roots_comment_args ); ?>
     <?php } // if registration required and not logged in ?>
   </section><!-- /#respond -->
 <?php } ?>
