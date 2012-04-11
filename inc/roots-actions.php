76f9422c49c44a3210ad6c67c650c6767c340ed8
SyntaxErr <peter.hieu.nguyen@gmail.com>
2012-04-11 09:11:13 -0300

    Move Google Analytics to head and use asynchronous syntax.

--- inc/roots-actions.php
+++ inc/roots-actions.php
@@ -12,12 +12,17 @@ function roots_google_analytics() {
   $roots_google_analytics_id = GOOGLE_ANALYTICS_ID;
   if ($roots_google_analytics_id !== '') {
     echo "\n\t<script>\n";
-    echo "\t\tvar _gaq=[['_setAccount','$roots_google_analytics_id'],['_trackPageview'],['_trackPageLoadTime']];\n";
-    echo "\t\t(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];\n";
-    echo "\t\tg.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';\n";
-    echo "\t\ts.parentNode.insertBefore(g,s)}(document,'script'));\n";
+    echo "\t\tvar _gaq = _gaq || [];\n";
+    echo "\t\t_gaq.push(['_setAccount', '$roots_google_analytics_id']);\n";
+    echo "\t\t_gaq.push(['_trackPageview']);\n";
+    echo "\n";
+    echo "\t\t(function() {\n";
+    echo "\t\t\tvar ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n";
+    echo "\t\t\tga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n";
+    echo "\t\t\tvar s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n";
+    echo "\t\t})();\n";
     echo "\t</script>\n";
   }
 }
 
-add_action('roots_footer', 'roots_google_analytics');
+add_action('roots_head', 'roots_google_analytics');
