30dca38ea25e4b714ae7ad27136749e3bfd4a94f
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2012-04-11 12:39:42 -0300

    Google Analytics automatically tracks load time

--- inc/roots-actions.php
+++ inc/roots-actions.php
@@ -12,7 +12,7 @@ function roots_google_analytics() {
   $roots_google_analytics_id = GOOGLE_ANALYTICS_ID;
   if ($roots_google_analytics_id !== '') {
     echo "\n\t<script>\n";
-    echo "\t\tvar _gaq=[['_setAccount','$roots_google_analytics_id'],['_trackPageview'],['_trackPageLoadTime']];\n";
+    echo "\t\tvar _gaq=[['_setAccount','$roots_google_analytics_id'],['_trackPageview']];\n";
     echo "\t\t(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];\n";
     echo "\t\tg.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';\n";
     echo "\t\ts.parentNode.insertBefore(g,s)}(document,'script'));\n";
