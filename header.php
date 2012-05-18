3f05432e424b39154e87e471ae7c8ba85ed97b44
Brian Gallagher <b.gallagh3r@gmail.com>
2012-05-17 20:40:54 -0400

    Fixed broken link on line 15: header.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 
   <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-2.5.3.min.js"></script>
 
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
 
   <?php roots_head(); ?>
