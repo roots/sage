88fd827d042f681295c9bd2800cac2386f72304f
Ben Word <ben@benword.com>
2012-05-29 21:25:02 -0600

    Default header should use pill nav if not using topbar

--- templates/header-default.php
+++ templates/header-default.php
@@ -5,7 +5,7 @@
       <?php bloginfo('name'); ?>
     </a>
     <nav id="nav-main" role="navigation">
-      <?php wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
+      <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills')); ?>
     </nav>
   </div>
 </header>
