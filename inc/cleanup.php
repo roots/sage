957b502f98f45d6af6c0ad858cc0dc1eca40a810
Ben Word <ben@benword.com>
2012-06-11 21:39:50 -0600

    Remove unnecessary remove_action's on wp_head as of 3.2.1
    See http://core.trac.wordpress.org/ticket/18128

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -176,9 +176,6 @@ function roots_head_cleanup() {
   remove_action('wp_head', 'feed_links_extra', 3);
   remove_action('wp_head', 'rsd_link');
   remove_action('wp_head', 'wlwmanifest_link');
-  remove_action('wp_head', 'index_rel_link');
-  remove_action('wp_head', 'parent_post_rel_link', 10, 0);
-  remove_action('wp_head', 'start_post_rel_link', 10, 0);
   remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
   remove_action('wp_head', 'wp_generator');
   remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
