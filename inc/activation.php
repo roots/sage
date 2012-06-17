4850e1823afb5a9ac5ba970ac95c61e46d237544
Weslly Honorato <weslly.honorato@gmail.com>
2012-06-17 12:30:00 -0300

    Support only wp 3.4 on activation

--- inc/activation.php
+++ inc/activation.php
@@ -69,14 +69,7 @@ function roots_theme_activation_options_render_page() { ?>
 
   <div class="wrap">
     <?php screen_icon(); ?>
-    <?php
-    if ((double) get_bloginfo('version') <= 3.4) {
-      $theme_name = wp_get_theme();
-    } else {
-      $theme_name = get_current_theme();
-    }
-    ?>
-    <h2><?php printf(__('%s Theme Activation', 'roots'), $theme_name ); ?></h2>
+    <h2><?php printf(__('%s Theme Activation', 'roots'), wp_get_theme() ); ?></h2>
     <?php settings_errors(); ?>
 
     <form method="post" action="options.php">
