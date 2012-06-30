038c2773ddf7e6ddd9c9dc482c15728783915e35
jdcauley <jordan@cauley.co>
2012-06-30 18:11:30 -0300

    Addition of visitor Menu

--- functions.php
+++ functions.php
@@ -25,6 +25,7 @@ function roots_setup() {
   // Register wp_nav_menu() menus (http://codex.wordpress.org/Function_Reference/register_nav_menus)
   register_nav_menus(array(
     'primary_navigation' => __('Primary Navigation', 'roots'),
+    'visitor' => __( 'Visitor Menu' )
   ));
 
   // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
