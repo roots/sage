a9598a1b549b2e6aadfa9c5faa9241ee38c68b48
Ben Word <ben@benword.com>
2012-07-15 01:01:41 -0500

    Add some comments to the config

--- inc/config.php
+++ inc/config.php
@@ -1,10 +1,13 @@
 <?php
+/**
+ * Roots configuration and constants
+ */
 
-add_theme_support('root-relative-urls');
-add_theme_support('rewrite-urls');
-add_theme_support('h5bp-htaccess');
-add_theme_support('bootstrap-responsive');
-add_theme_support('bootstrap-top-navbar');
+add_theme_support('root-relative-urls');    // Enable relative URLs
+add_theme_support('rewrite-urls');          // Enable URL rewrites
+add_theme_support('h5bp-htaccess');         // Enable HTML5 Boilerplate's .htaccess
+add_theme_support('bootstrap-responsive');  // Enable Bootstrap's responsive CSS
+add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's fixed navbar
 
 // Set the content width based on the theme's design and stylesheet
 if (!isset($content_width)) { $content_width = 940; }
@@ -15,7 +18,7 @@ define('CONTAINER_CLASSES',         'row');
 define('MAIN_CLASSES',              'span8');
 define('SIDEBAR_CLASSES',           'span4');
 define('FULLWIDTH_CLASSES',         'span12');
-define('GOOGLE_ANALYTICS_ID',       '');
+define('GOOGLE_ANALYTICS_ID',       '');        // UA-XXXXX-Y
 
 define('WP_BASE',                   wp_base_dir());
 define('THEME_NAME',                next(explode('/themes/', get_template_directory())));
