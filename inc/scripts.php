704ee09f2bf260257282e401e7b4dec13b66b208
Ben Word <ben@benword.com>
2012-07-15 01:03:18 -0500

    Fix #478 - Resolve some clean URL issues, re-open #395

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,19 +1,23 @@
 <?php
+/**
+ * Scripts and stylesheets
+ */
 
 function roots_scripts() {
-  wp_enqueue_style('roots_bootstrap_style', '/css/bootstrap.css', false, null);
+  wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/css/bootstrap.css', false, null);
 
   if (current_theme_supports('bootstrap-responsive')) {
-    wp_enqueue_style('roots_bootstrap_responsive_style', '/css/bootstrap-responsive.css', array('roots_bootstrap_style'), null);
+    wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
   }
 
   // If you're not using Bootstrap, include H5BP's main.css:
-  // wp_enqueue_style('roots_style', '/css/main.css', false, null);
+  // wp_enqueue_style('roots_h5bp', '/css/main.css', false, null);
 
-  wp_enqueue_style('roots_app_style', '/css/app.css', false, null);
+  wp_enqueue_style('roots_app', get_template_directory_uri() . '/css/app.css', false, null);
 
+  // Load style.css from child theme
   if (is_child_theme()) {
-    wp_enqueue_style('roots_child_style', get_stylesheet_uri());
+    wp_enqueue_style('roots_child', get_stylesheet_uri(), false, null);
   }
 
   if (!is_admin()) {
@@ -25,8 +29,8 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_register_script('roots_plugins', '/js/plugins.js', false, null, false);
-  wp_register_script('roots_main', '/js/main.js', false, null, false);
+  wp_register_script('roots_plugins', get_template_directory_uri() . '/js/plugins.js', false, null, false);
+  wp_register_script('roots_main', get_template_directory_uri() . '/js/main.js', false, null, false);
   wp_enqueue_script('roots_plugins');
   wp_enqueue_script('roots_main');
 }
