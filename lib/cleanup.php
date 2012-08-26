b9cc39839695ecb73e9fc02f1e584fc991b946b3
Linus Metzler <linus.metzler@gmail.com>
2012-08-26 22:36:30 +0300

    fixed dropdown bug
    when a dropdown parent contains a link itself, a data-target="#" is needed

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -471,7 +471,7 @@ class Roots_Nav_Walker extends Walker_Nav_Menu {
     $attributes .= ! empty($item->target)     ? ' target="' . esc_attr($item->target    ) .'"' : '';
     $attributes .= ! empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn       ) .'"' : '';
     $attributes .= ! empty($item->url)        ? ' href="'   . esc_attr($item->url       ) .'"' : '';
-    $attributes .= ($args->has_children)    ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
+    $attributes .= ($args->has_children)    ? ' class="dropdown-toggle" data-toggle="dropdown" data-target="#"' : '';
 
     $item_output  = $args->before;
     $item_output .= '<a'. $attributes .'>';
