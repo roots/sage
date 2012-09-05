f9959b4da0559fc7e00e93e9bd2f283b05c0d2d7
Ben Word <ben@benword.com>
2012-09-04 23:34:13 -0500

    Update to Bootstrap 2.1.1, jQuery 1.8.1, Modernizr 2.6.2

--- lib/scripts.php
+++ lib/scripts.php
@@ -9,8 +9,8 @@
  * 4. /child-theme/style.css (if a child theme is activated)
  *
  * Enqueue scripts in the following order:
- * 1. /theme/assets/js/vendor/modernizr-2.6.1.min.js  (in head.php)
- * 2. jquery-1.8.0.min.js via Google CDN              (in head.php)
+ * 1. /theme/assets/js/vendor/modernizr-2.6.2.min.js  (in head.php)
+ * 2. jquery-1.8.1.min.js via Google CDN              (in head.php)
  * 3. /theme/assets/js/plugins.js
  * 4. /theme/assets/js/main.js
  */
