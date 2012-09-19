267e24e9a3060b5a2bda67641cfc74eb8384c0fa
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2012-09-19 13:53:01 -0400

    Move Google Analytics code into footer template
    Once again, HTML tags should be in the templates when possible.
    
    actions.php is no longer needed now as well.

--- templates/footer.php
+++ templates/footer.php
@@ -3,4 +3,13 @@
   <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
 </footer>
 
+<?php if (GOOGLE_ANALYTICS_ID): ?>
+<script>
+  var _gaq=[['_setAccount','<?php echo GOOGLE_ANALYTICS_ID; ?>'],['_trackPageview']];
+  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+<?php endif; ?>
+
 <?php wp_footer(); ?>
