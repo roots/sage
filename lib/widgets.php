88f9863cf236d9743b123302f7edfe8b1ec9e9f1
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2012-09-20 10:24:27 -0600

    Fix initial display

--- lib/widgets.php
+++ lib/widgets.php
@@ -121,10 +121,11 @@ class Roots_Vcard_Widget extends WP_Widget {
 
   function form($instance) {
     foreach($this->fields as $name => $label) {
+      ${$name} = isset($instance[$name]) ? esc_attr($instance[$name]) : '';
     ?>
     <p>
       <label for="<?php echo esc_attr($this->get_field_id($name)); ?>"><?php _e("{$label}:", 'roots'); ?></label>
-      <input class="widefat" id="<?php echo esc_attr($this->get_field_id($name)); ?>" name="<?php echo esc_attr($this->get_field_name($name)); ?>" type="text" value="<?php echo esc_attr(${$name}); ?>">
+      <input class="widefat" id="<?php echo esc_attr($this->get_field_id($name)); ?>" name="<?php echo esc_attr($this->get_field_name($name)); ?>" type="text" value="<?php echo ${$name}; ?>">
     </p>
     <?php
     }
