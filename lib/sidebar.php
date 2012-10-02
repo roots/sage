c02f877b59ece6cb572edaba87b711ecfbf1b2e2
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2012-10-02 15:52:31 -0400

    Fix callback reference

--- lib/sidebar.php
+++ lib/sidebar.php
@@ -20,8 +20,8 @@ class Roots_Sidebar {
     $this->conditionals = $conditionals;
     $this->templates    = $templates;
 
-    $conditionals = array_map($this->check_conditional_tag, $this->conditionals);
-    $templates    = array_map($this->check_page_template, $this->templates);
+    $conditionals = array_map(array($this, 'check_conditional_tag'), $this->conditionals);
+    $templates    = array_map(array($this, 'check_page_template'), $this->templates);
 
     if (in_array(true, $conditionals) || in_array(true, $templates)) {
       $this->display = false;
