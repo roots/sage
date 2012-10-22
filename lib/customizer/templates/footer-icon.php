b867ee4548964f134ae73f2a54411893d4d4f88d
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-22 20:47:40 +0300

    moved the footer icon css to the less file

--- lib/customizer/templates/footer-icon.php
+++ lib/customizer/templates/footer-icon.php
@@ -2,31 +2,6 @@
 function shoestrap_footer_icon(){ ?>
   <?php if (current_user_can( 'edit_theme_options' )){ ?>
     <style>
-      #shoestrap_icon{
-        position: fixed;
-        bottom: 0;
-        left: 0;
-      }
-      #shoestrap_icon a{
-        width: 100px;
-        height: 20px;
-        display: block;
-        background: #08c;
-        background: url(<?php echo plugins_url('/img/bottom-corner.png', __FILE__ ); ?>) no-repeat bottom left;
-        color: #fff;
-        font-size: 50px;
-        opacity: 0.3;
-        text-decoration: none;
-        padding: 60px 0 20px 5px;
-        -webkit-transition: all 350ms linear;
-        -moz-transition: all 350ms linear;
-        -ms-transition: all 350ms linear;
-        -o-transition: all 350ms linear;
-        transition: all 350ms linear;
-      }
-      #shoestrap_icon a:hover{
-        opacity: 1;
-      }
     </style>
     <div id="shoestrap_icon">
       <?php
