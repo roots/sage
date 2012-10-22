2c602d9961897b652c25063e189c02ee33b6c707
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-22 18:38:35 +0300

    removing rewrites

--- functions.php
+++ functions.php
@@ -9,10 +9,9 @@ require_once locate_template('/lib/utils.php');                 // Utility funct
 require_once locate_template('/lib/init.php');                  // Initial theme setup and constants
 require_once locate_template('/lib/sidebar.php');               // Sidebar class
 require_once locate_template('/lib/config.php');                // Configuration
-require_once locate_template('/lib/activation.php');            // Theme activation
+require_once locate_template('/lib/menus.php');            // Theme activation
 require_once locate_template('/lib/cleanup.php');               // Cleanup
 require_once locate_template('/lib/nav.php');                   // Custom nav modifications
-require_once locate_template('/lib/htaccess.php');              // Rewrites for assets, H5BP .htaccess
 require_once locate_template('/lib/widgets.php');               // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');               // Scripts and stylesheets
 require_once locate_template('/lib/post-types.php');            // Custom post types
