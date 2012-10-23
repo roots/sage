df2cd02f825680a28a01032d95b3200313f011b6
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-23 12:26:17 +0300

    added theme support for rewrites

--- lib/config.php
+++ lib/config.php
@@ -5,8 +5,6 @@
 
 // Enable theme features
 add_theme_support('root-relative-urls');    // Enable relative URLs
-add_theme_support('rewrite-urls');          // Enable URL rewrites
-add_theme_support('h5bp-htaccess');         // Enable HTML5 Boilerplate's .htaccess
 add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's fixed navbar
 
 
