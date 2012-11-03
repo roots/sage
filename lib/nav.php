cba75c9f9b8ea339298cc5a8fe0e031e576a5d2e
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-03 16:28:46 +0200

    compatibility with ubermenu https://github.com/retlehs/roots/commit/ac6057ecf4dcb1c5a2dacebd617fb761d5d8a919

--- lib/nav.php
+++ lib/nav.php
@@ -60,7 +60,7 @@ class Shoestrap_Nav_Walker extends Walker_Nav_Menu {
 function shoestrap_nav_menu_css_class($classes, $item) {
   $slug = sanitize_title($item->title);
   $classes = preg_replace('/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes);
-  $classes = preg_replace('/((menu|page)[-_\w+]+)+/', '', $classes);
+  $classes = preg_replace('/^((menu|page)[-_\w+]+)+/', '', $classes);
 
   $classes[] = 'menu-' . $slug;
 
