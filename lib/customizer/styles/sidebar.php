c2730e41f55e463afb60feed287df920b99e64e9
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-05 10:39:58 +0200

    Taking into account the secondary sidebar when calculating sidebar and main classes

--- lib/customizer/styles/sidebar.php
+++ lib/customizer/styles/sidebar.php
@@ -1,29 +1,102 @@
 <?php
 
 function shoestrap_sidebar_width( $target, $echo = false ) {
-  $sidebar_width = get_theme_mod( 'shoestrap_aside_width' );
+  $first  = get_theme_mod( 'shoestrap_aside_width' );
+  $second = get_theme_mod( 'shoestrap_secondary_width' );
   
-  if ( $sidebar_width == '2' ) {
-    $main    = 'span10';
-    $sidebar = 'span2';
-  } elseif ($sidebar_width == '3') {
-    $main    = 'span9';
-    $sidebar = 'span3';
-  } elseif ($sidebar_width == '5') {
-    $main    = 'span7';
-    $sidebar = 'span5';
-  } elseif ($sidebar_width == '6') {
-    $main    = 'span6';
-    $sidebar = 'span6';
-  } else { // default value
-    $main    = 'span8';
-    $sidebar = 'span4';
+  if ( !is_active_sidebar( 'sidebar-secondary' ) ) {
+    if ( $first == '2' ) {
+      $main    = 'span10';
+      $sidebar = 'span2';
+    } elseif ( $first == '3' ) {
+      $main    = 'span9';
+      $sidebar = 'span3';
+    } elseif ( $first == '5' ) {
+      $main    = 'span7';
+      $sidebar = 'span5';
+    } elseif ( $first == '6' ) {
+      $main    = 'span6';
+      $sidebar = 'span6';
+    } else { // default value
+      $main    = 'span8';
+      $sidebar = 'span4';
+    }
+  } else {
+    if ( $second == '2' ) {
+      if ( $first == '2' ) {
+        $main      = 'span8';
+        $primary   = 'span2';
+        $secondary = 'span2';
+      } elseif ( $first == '3' ) {
+        $main      = 'span7';
+        $primary   = 'span3';
+        $secondary = 'span2';
+      } elseif ( $first == '5' ) {
+        $main      = 'span5';
+        $primary   = 'span5';
+        $secondary = 'span2';
+      } elseif ( $first == '6' ) {
+        $main      = 'span4';
+        $primary   = 'span6';
+        $secondary = 'span2';
+      } else {
+        $main      = 'span6';
+        $primary   = 'span4';
+        $secondary = 'span2';
+      }
+    } elseif ( $second == '3' ) {
+      if ( $first == '2' ) {
+        $main      = 'span7';
+        $primary   = 'span2';
+        $secondary = 'span3';
+      } elseif ( $first == '3' ) {
+        $main      = 'span6';
+        $primary   = 'span3';
+        $secondary = 'span3';
+      } elseif ( $first == '5' ) {
+        $main      = 'span4';
+        $primary   = 'span5';
+        $secondary = 'span3';
+      } elseif ( $first == '6' ) {
+        $main      = 'span3';
+        $primary   = 'span6';
+        $secondary = 'span3';
+      } else {
+        $main      = 'span5';
+        $primary   = 'span4';
+        $secondary = 'span3';
+      }
+    } elseif ( $second == '4' ) {
+      if ( $first == '2' ) {
+        $main      = 'span6';
+        $primary   = 'span2';
+        $secondary = 'span4';
+      } elseif ( $first == '3' ) {
+        $main      = 'span5';
+        $primary   = 'span3';
+        $secondary = 'span4';
+      } elseif ( $first == '5' ) {
+        $main      = 'span3';
+        $primary   = 'span5';
+        $secondary = 'span4';
+      } elseif ( $first == '6' ) {
+        $main      = 'span2';
+        $primary   = 'span6';
+        $secondary = 'span4';
+      } else {
+        $main      = 'span4';
+        $primary   = 'span4';
+        $secondary = 'span4';
+      }
+    }
   }
   
-  if ( $target == 'main' ) {
-    $class = $main;
+  if ( $target == 'primary' ) {
+    $class = $primary;
+  } elseif ( $target == 'secondary' ) {
+    $class = $secondary;
   } else {
-    $class = $sidebar;
+    $class = $main;
   }
   
   if ( $echo ) {
