cacbb20c15acf940e57cbde34f4e9fcd88406c10
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-05 06:54:06 +0100

    fixing styling for login buttons

--- lib/customizer/templates/loginbutton.php
+++ lib/customizer/templates/loginbutton.php
@@ -4,22 +4,20 @@ function shoestrap_login_button() {
     
   $show_login_link = get_theme_mod( 'shoestrap_header_loginlink' );
   
-  $content = '<li>';
   if ( is_user_logged_in() ){
     $link  = wp_logout_url( get_permalink() );
-    $label = __( 'Logout', 'bootstrap_commerce' );
+    $label = __( 'Logout', 'shoestrap' );
   }
   else {
     $link  = wp_login_url( get_permalink() );
-    $label = __( 'Login/Register', 'bootstrap_commerce' );
+    $label = __( 'Login/Register', 'shoestrap' );
   }
-  $content .= '<a href="' . $link . '">';
+  $content .= '<a class="pull-right" style="padding: 10px;" href="' . $link . '">';
   $content .= '<i class="icon-user"></i> ' . $label;
   $content .= '</a>';
-  $content .= '</li>';
   
   if ( $show_login_link != '0' ) {
     echo $content;
   }
 }
-add_action( 'shoestrap_nav_top_right', 'shoestrap_login_button', 8 );
+add_action( 'shoestrap_nav_top_right', 'shoestrap_login_button', 11 );
