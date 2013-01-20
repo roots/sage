e24d0562d791dd2ba106eac4a95e8bd6167270d0
Ben Word <ben@benword.com>
2013-01-20 15:21:04 -0600

    Fix #643 - Fix PHP notice on search with no results

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', get_post_format()); ?>
+<?php if (have_posts()) : ?>
+  <?php get_template_part('templates/content', get_post_format()); ?>
+<?php endif; ?>
