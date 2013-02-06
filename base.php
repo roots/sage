714982fa42c09e63937a82f8c56819ae10a705c2
Ben Word <ben@benword.com>
2013-02-06 14:30:39 -0600

    Fix #639 - Add do_action('get_header')

--- base.php
+++ base.php
@@ -4,6 +4,7 @@
   <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
   <?php
+    do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
     if (current_theme_supports('bootstrap-top-navbar')) {
       get_template_part('templates/header-top-navbar');
