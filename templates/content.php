aa4ebe693c6986e1e8fdfd81d3ccfa347d2539c0
Ben Word <ben@benword.com>
2013-02-12 14:54:14 -0600

    Remove disabled post nav links

--- templates/content.php
+++ templates/content.php
@@ -26,13 +26,9 @@
     <ul class="pager">
       <?php if (get_next_posts_link()) : ?>
         <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-      <?php else: ?>
-        <li class="previous disabled"><a><?php _e('&larr; Older posts', 'roots'); ?></a></li>
       <?php endif; ?>
       <?php if (get_previous_posts_link()) : ?>
         <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
-      <?php else: ?>
-        <li class="next disabled"><a><?php _e('Newer posts &rarr;', 'roots'); ?></a></li>
       <?php endif; ?>
     </ul>
   </nav>
