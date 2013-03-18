beda3400a5ef55f41bfcafd46f95f0323451afbc
sfradel <sfradel@gmail.com>
2013-03-18 21:34:49 +0200

    Comment reply js does not work because no appropriate id attribute exists

--- lib/comments.php
+++ lib/comments.php
@@ -30,7 +30,7 @@ class Roots_Walker_Comment extends Walker_Comment {
 
   <li <?php comment_class('media comment-' . get_comment_ID()); ?>>
     <?php echo get_avatar($comment, $size = '64'); ?>
-    <div class="media-body">
+    <div id="comment-<?php comment_ID(); ?>" class="media-body">
       <h4 class="media-heading"><?php echo get_comment_author_link(); ?></h4>
       <time datetime="<?php echo comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a></time>
       <?php edit_comment_link(__('(Edit)', 'roots'), '', ''); ?>
