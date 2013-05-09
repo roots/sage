9ac6bc698ebce6b6f180b3827166848edfd2442b
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2013-05-09 17:04:26 -0400

    Use SERVER_NAME to avoid parsing site_url()
    site_url() was previously retrieved and parsed many times per page
    before. This is cleaner and done for optimization purposes.

--- lib/init.php
+++ lib/init.php
@@ -30,7 +30,7 @@ if (!defined('__DIR__')) { define('__DIR__', dirname(__FILE__)); }
 // Define helper constants
 $get_theme_name = explode('/themes/', get_template_directory());
 
-define('THEME_NAME',                next($get_theme_name));
-define('RELATIVE_PLUGIN_PATH',      str_replace(home_url() . '/', '', plugins_url()));
-define('RELATIVE_CONTENT_PATH',     str_replace(home_url() . '/', '', content_url()));
-define('THEME_PATH',                RELATIVE_CONTENT_PATH . '/themes/' . THEME_NAME);
+define('THEME_NAME',            next($get_theme_name));
+define('RELATIVE_PLUGIN_PATH',  str_replace(home_url() . '/', '', plugins_url()));
+define('RELATIVE_CONTENT_PATH', str_replace(home_url() . '/', '', content_url()));
+define('THEME_PATH',            RELATIVE_CONTENT_PATH . '/themes/' . THEME_NAME);
