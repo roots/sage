008761a49a7312037ffc934222e4e88022bf381d
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-05-12 23:03:51 +0200

    Use new analytics.js for Google Analytics tracking code
    This replaces the former ga.js file used for Google Analytics tracking.
    [More info in the GA docs](https://developers.google.com/analytics/devguides/collection/analyticsjs/)

--- lib/scripts.php
+++ lib/scripts.php
@@ -65,12 +65,14 @@ function roots_jquery_local_fallback($src, $handle) {
 
 function roots_google_analytics() { ?>
 <script>
-  var _gaq=[['_setAccount','<?php echo GOOGLE_ANALYTICS_ID; ?>'],['_trackPageview']];
-  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', '<?php echo GOOGLE_ANALYTICS_ID; ?>');
+  ga('send', 'pageview');
 </script>
 <?php }
 if (GOOGLE_ANALYTICS_ID) {
-  add_action('wp_footer', 'roots_google_analytics', 20);
+  add_action('wp_head', 'roots_google_analytics', 20);
 }
