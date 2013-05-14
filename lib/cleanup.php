de206a2cf74d944fff45d9659967d2ad69c0b519
Louis-Rémi Babé <public@lrbabe.com>
2013-05-14 20:06:33 +0300

    Only display media if it is meaningful

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -95,6 +95,17 @@ function roots_wp_title($title) {
 add_filter('wp_title', 'roots_wp_title', 10);
 
 /**
+ * Clean up output of stylesheet <link> tags
+ */
+function roots_clean_style_tag($input) {
+  preg_match_all("!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches);
+  // Only display media if it is meaningful
+  $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
+  return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
+}
+add_filter('style_loader_tag', 'roots_clean_style_tag');
+
+/**
  * Add and remove body_class() classes
  */
 function roots_body_class($classes) {
