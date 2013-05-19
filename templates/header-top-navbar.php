1d7b5ab1a1ace0f2e91b1c8def8e9590eddd92bb
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-20 00:41:26 +0300

    tweaking the header-top-navbar template

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,24 +1,30 @@
-<header class="banner navbar navbar-static-top" role="banner">
-  <div class="navbar-inner">
-    <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+<header id="banner" class="topnavbar <?php echo shoestrap_navbar_class(); ?>" role="banner">
+  <div class="container">
+  <a class="btn navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </a>
+
+    <?php if ( get_theme_mod( 'navbar_brand' ) != 0 ) : ?>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/">
+        <?php if ( get_theme_mod( 'navbar_logo' ) == 1 ) : ?>
+          <?php shoestrap_logo(); ?>
+        <?php else : ?>
+          <?php bloginfo('name'); ?>
+        <?php endif; ?>
       </a>
-      <a class="brand" href="<?php echo home_url(); ?>/">
-        <?php bloginfo('name'); ?>
-      </a>
-      <?php do_action( 'shoestrap_pre_main_nav' ); ?>
-      <nav class="nav-main nav-collapse collapse" role="navigation">
-        <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
-          endif;
-        ?>
-      </nav>
-      <?php do_action( 'shoestrap_post_main_nav' ); ?>
-    </div>
+    <?php endif; ?>
+
+    <?php do_action( 'shoestrap_pre_main_nav' ); ?>
+    <nav class="nav-main nav-collapse collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'menu_class' => shoestrap_nav_class_pull() ) );
+        endif;
+      ?>
+    </nav>
+    <?php do_action( 'shoestrap_post_main_nav' ); ?>
   </div>
   <?php do_action( 'shoestrap_post_navbar' ); ?>
 </header>
