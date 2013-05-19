96d18f41a5140ecd165eca3fa4ed05a964810fee
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-20 00:22:33 +0300

    layout improvements

--- lib/customizer/functions.layout.php
+++ lib/customizer/functions.layout.php
@@ -4,9 +4,9 @@
  * Calculates the classes of the main area, main sidebar and secondary sidebar
  */
 function shoestrap_section_class( $target, $echo = false ) {
-  $layout = get_theme_mod( 'layout' );
-  $first  = get_theme_mod( 'layout_primary_width' );
-  $second = get_theme_mod( 'layout_secondary_width' );
+  $layout = intval( get_theme_mod( 'layout' ) );
+  $first  = intval( get_theme_mod( 'layout_primary_width' ) );
+  $second = intval( get_theme_mod( 'layout_secondary_width' ) );
   
   $base   = 'col col-lg-';
   // Set some defaults so that we can change them depending on the selected template
@@ -14,25 +14,26 @@ function shoestrap_section_class( $target, $echo = false ) {
   $primary    = NULL;
   $secondary  = NULL;
   $wrapper    = NULL;
-  $wrap       = false;
 
   if ( is_active_sidebar( 'sidebar-secondary' ) && is_active_sidebar( 'sidebar-primary' ) ) {
-    if ( $layout >= 3 ) {
-      $main       = $base . ceil( ( 12 - $first ) * ( 12 - $second ) / 12 );
-      $primary    = $base . ( 12 - ceil( ( 12 - $first ) * ( 12 - $second ) / 12 ) );
+    if ( $layout == 5 ) {
+      $main       = $base . ( 12 - floor( ( 12 * $first ) / ( 12 - $secondary ) ) );
+      $primary    = $base . floor( ( 12 * $first ) / ( 12 - $secondary ) );
       $secondary  = $base . $second;
       $wrapper    = $base . ( 12 - $second );
-      $wrap       = true;
+    } elseif ( $layout >= 3 ) {
+      $main       = $base . ( 12 - $first - $second );
+      $primary    = $base . $first;
+      $secondary  = $base . $second;
     } elseif ( $layout >= 1 ) {
       $main       = $base . ( 12 - $first );
       $primary    = $base . $first;
-      $secondary  = NULL;
+      $secondary  = $base . $second;
     }
   } elseif ( !is_active_sidebar( 'sidebar-secondary' ) && is_active_sidebar( 'sidebar-primary' ) ) {
     if ( $layout >= 1 ) {
       $main       = $base . ( 12 - $first );
       $primary    = $base . $first;
-      $secondary  = NULL;
     }
   } elseif ( is_active_sidebar( 'sidebar-secondary' ) && !is_active_sidebar( 'sidebar-primary' ) ) {
     if ( $layout >= 3 ) {
@@ -61,20 +62,31 @@ function shoestrap_section_class( $target, $echo = false ) {
   else
     $class = $main;
 
-  // echo or return the result.
-  if ( $echo )
-    echo $class;
-  else
-    return $class;
-
-  if ( $target == 'wrap' )
-    return $wrap;
+  if ( $target != 'wrap'  ) {
+    // echo or return the result.
+    if ( $echo )
+      echo $class;
+    else
+      return $class;
+  } else {
+    if ( $layout == 5 )
+      return true;
+    else
+      return false;
+  }
 }
 
 /*
  * If any css should be applied to fix the layout, enter it here.
  */
 function shoestrap_sidebars_positioning_css() {
-  $layout = get_theme_mod( 'shoestrap_layout' );
+  $layout = get_theme_mod( 'layout' );
+
+  echo '<style>';
+  if ( $layout == 2 || $layout == 3 || $layout == 5 )
+    echo 'div.main{float:right;}';
+
+  echo '</style>';
+
 }
 add_action( 'wp_head', 'shoestrap_sidebars_positioning_css' );
