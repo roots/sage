ad9b979c9c0774a2ab90aaf8824ff30dcc0bd425
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-20 02:19:23 +0300

    fixing the customizer toggling icon

--- lib/customizer/functions.footer.php
+++ lib/customizer/functions.footer.php
@@ -22,7 +22,7 @@ function footer_icon() { ?>
       <?php
       $current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
       $href = add_query_arg( 'url', urlencode( $current_url ), wp_customize_url() ); ?>
-      <a href="<?php echo $href; ?>"><i class="glyphicon-cogs"></i></a>
+      <a href="<?php echo $href; ?>"><i class="glyphicon glyphicon-cogs"></i></a>
     </div>
   <?php } ?>
   </div>
