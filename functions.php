340ed03cb2025bc13b1da06c99a72caba2e60947
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-20 18:27:33 +0300

    Include the options framework in functions.php

--- functions.php
+++ functions.php
@@ -31,3 +31,5 @@ require_once locate_template('/lib/customizer/functions.layout.php');         //
 require_once locate_template('/lib/customizer/functions.logo.php');           // Extra functions for the "Logo" Customizer section
 require_once locate_template('/lib/customizer/functions.navbar.php');         // Extra functions for the "NavBar" Customizer section
 require_once locate_template('/lib/customizer/functions.typography.php');     // Extra functions for the "Typography" Customizer section
+
+require_once locate_template('/admin/index.php'); // Slightly Modified Options Framework
