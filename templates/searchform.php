ef0fa8411c81ca2414cb88dc5068c8ac2118e87f
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-20 19:41:08 +0300

    The searchform now uses input-group

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,5 +1,7 @@
-<form role="search" method="get" id="searchform" class="form-search" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" id="searchform" class="form-search input-group" action="<?php echo home_url('/'); ?>">
   <label class="hide" for="s"><?php _e('Search for:', 'roots'); ?></label>
   <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="search-query" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
-  <input type="submit" id="searchsubmit" value="<?php _e('Search', 'roots'); ?>" class="btn">
+  <span class="input-group-btn">
+  	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'roots'); ?>" class="btn btn-info">
+  </span>
 </form>
