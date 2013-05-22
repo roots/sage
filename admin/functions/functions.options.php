5bb08641b89fd3e62dd1e539da0d7631f2476694
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-22 16:05:46 +0300

    Migrating options

--- admin/functions/functions.options.php
+++ admin/functions/functions.options.php
@@ -98,8 +98,117 @@ if (!function_exists('of_options')) {
 // Set the Options Array
 global $of_options;
 
-// Layout Settings
+// General Options
+$of_options[] = array(
+  "name"      => "General",
+  "type"      => "heading"
+);
 
+$of_options[] = array(
+  "name"      => "Logo",
+  "desc"      => "Upload a logo image using the media uploader, or define the URL directly. Use the shortcodes [site_url] or [site_url_secure] for setting default URLs",
+  "id"        => "logo",
+  "std"       => "",
+  "type"      => "media"
+);
+
+$of_options[] = array(
+  "name"      => "No gradients - \"Flat\" look.",
+  "desc"      => "This option will disable all gradients in your site, giving it a cleaner look. Default: OFF.",
+  "id"        => "general_flat",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 0,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Border-Radius",
+  "desc"      => "You can adjust the corner-radius of all elements in your site here. This will affect buttons, navbars, widgets and many more. Default: 4",
+  "id"        => "layout_secondary_width",
+  "std"       => 4,
+  "min"       => 0,
+  "step"      => 1,
+  "max"       => 50,
+  "advanced"  => true,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "sliderui"
+);
+
+$of_options[] = array(
+  "name"      => "Featured Images on Archives",
+  "desc"      => "Display featured Images on post archives (such as categories, tags, month view etc). Default: OFF.",
+  "id"        => "feat_img_archive",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 0,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Archives Featured Image Width",
+  "desc"      => "Select the width of your featured images on post archives. Default: 550px",
+  "id"        => "feat_img_archive_width",
+  "std"       => 550,
+  "min"       => 100,
+  "step"      => 1,
+  "max"       => 1600,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "sliderui"
+);
+
+$of_options[] = array(
+  "name"      => "Archives Featured Image Height",
+  "desc"      => "Select the height of your featured images on post archives. Default: 300px",
+  "id"        => "feat_img_archive_height",
+  "std"       => 300,
+  "min"       => 50,
+  "step"      => 1,
+  "max"       => 1000,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "sliderui"
+);
+
+$of_options[] = array(
+  "name"      => "Featured Images on Posts",
+  "desc"      => "Display featured Images on posts. Default: OFF.",
+  "id"        => "feat_img_post",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 0,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Posts Featured Image Width",
+  "desc"      => "Select the width of your featured images on single posts. Default: 550px",
+  "id"        => "feat_img_post_width",
+  "std"       => 550,
+  "min"       => 100,
+  "step"      => 1,
+  "max"       => 1600,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "sliderui"
+);
+
+$of_options[] = array(
+  "name"      => "Posts Featured Image Height",
+  "desc"      => "Select the height of your featured images on single posts. Default: 300px",
+  "id"        => "feat_img_post_height",
+  "std"       => 300,
+  "min"       => 50,
+  "step"      => 1,
+  "max"       => 1000,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "sliderui"
+);
+
+// Layout Settings
 $of_options[] = array(
   "name"      => "Layout Settings",
   "type"      => "heading"
@@ -134,13 +243,13 @@ $of_options[] = array(
 );
 
 $of_options[] = array(
-  "name"      => "Try checkbox",
-  "desc"      => "OFF by default. If you want to display the sidebars in your frontpage, turn this ON.",
-  "id"        => "layout_sidebar_on_front_test",
+  "name"      => "Fluid Layout",
+  "desc"      => "OFF by default. If you turn this ON, then the layout of your site will become fluid, spanning accross the whole width of your screen.",
+  "id"        => "fluid",
   "less"      => true,
   "customizer"=> array(),
   "std"       => 0,
-  "type"      => "checkbox"
+  "type"      => "switch"
 );
 
 $of_options[] = array(
@@ -226,6 +335,106 @@ $of_options[] = array(
   "less"      => true,
   "customizer"=> array(),
   "type"      => "sliderui"
+);
+
+$of_options[] = array(
+  "name"      => "Columns Gutter",
+  "desc"      => "The space between the columns in your grid. Default: 30px",
+  "id"        => "layout_gutter",
+  "std"       => 30,
+  "min"       => 0,
+  "step"      => 2,
+  "max"       => 100,
+  "advanced"  => true,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "sliderui"
+);
+
+// Colors
+$of_options[] = array(
+  "name"      => "Site Colors",
+  "type"      => "heading"
+);
+
+$of_options[] = array(
+  "name"      => "Background Color",
+  "desc"      => "Pick a background color for your site. Default: #ffffff.",
+  "id"        => "color_body_bg",
+  "std"       => "#ffffff",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Text Color",
+  "desc"      => "Pick a color for your site's main text. Default: #333333.",
+  "id"        => "color_text",
+  "std"       => "#333333",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Links Color",
+  "desc"      => "Pick a color for your site's links. Default: #428bca.",
+  "id"        => "color_links",
+  "std"       => "#428bca",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Brand Colors: Primary",
+  "desc"      => "Select your primary branding color. This will affect various areas of your site, including the color of your primary buttons, the background of some elements and many more. Default: #428bca.",
+  "id"        => "color_brand_primary",
+  "std"       => "#428bca",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Brand Colors: Success",
+  "desc"      => "Select your branding color for success messages etc. Default: #5cb85c.",
+  "id"        => "color_brand_success",
+  "std"       => "#5cb85c",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Brand Colors: Warning",
+  "desc"      => "Select your branding color for warning messages etc. Default: #f0ad4e.",
+  "id"        => "color_brand_warning",
+  "std"       => "#f0ad4e",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Brand Colors: Danger",
+  "desc"      => "Select your branding color for success messages etc. Default: #d9534f.",
+  "id"        => "color_brand_danger",
+  "std"       => "#d9534f",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Brand Colors: Info",
+  "desc"      => "Select your branding color for info messages etc. It will also be used for the Search button color as well as other areas where it semantically makes sense to use an \"info\" class. Default: #5bc0de.",
+  "id"        => "color_brand_info",
+  "std"       => "#5bc0de",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
 );
 
 // NavBar Settings
@@ -343,6 +552,197 @@ $of_options[] = array(
   "type"      => "sliderui"
 );
 
+// TODO: Make this a dropdown or an image control so that people can select among more than 1 styles.
+$of_options[] = array(
+  "name"      => "Alternative style for NavBars",
+  "desc"      => "You can use an alternative menu style for your NavBars. OFF by default. ",
+  "id"        => "navbar_altmenu",
+  "std"       => 0,
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "switch"
+);
+
+// Jumbotron (Hero)
+$of_options[] = array(
+  "name"      => "Jumbotron",
+  "type"      => "heading"
+);
+
+$of_options[] = array(
+  "name"      => "Jumbotron Background Color",
+  "desc"      => "Select the background color for your Jumbotron area. Please note that this area will only be visible if you assign a widget to the \"Jumbotron\" Widget Area. Default: #EEEEEE.",
+  "id"        => "jumbotron_bg",
+  "std"       => "#EEEEEE",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Background Image",
+  "desc"      => "Upload a logo image using the media uploader, or define the URL directly. Use the shortcodes [site_url] or [site_url_secure] for setting default URLs",
+  "id"        => "jumbotron_bg_img",
+  "std"       => "",
+  "type"      => "media"
+);
+
+$of_options[] = array(
+  "name"      => "Background Repeat",
+  "desc"      => "Select how (or if) the selected background should be tiled. Default: Tile",
+  "id"        => "jumbotron_bg_repeat",
+  "std"       => "repeat",
+  "type"      => "radio",
+  "options"   => array(
+    'no-repeat'  => __( 'No Repeat', 'shoestrap' ),
+    'repeat'     => __( 'Tile', 'shoestrap' ),
+    'repeat-x'   => __( 'Tile Horizontally', 'shoestrap' ),
+    'repeat-y'   => __( 'Tile Vertically', 'shoestrap' ),
+  ),
+);
+
+$of_options[] = array(
+  "name"      => "Background Alignment",
+  "desc"      => "Select how the selected background should be horizontally aligned. Default: Left",
+  "id"        => "jumbotron_bg_pos_x",
+  "std"       => "repeat",
+  "type"      => "radio",
+  "options"   => array(
+    'left'    => __( 'Left', 'shoestrap' ),
+    'right'   => __( 'Right', 'shoestrap' ),
+    'center'  => __( 'Center', 'shoestrap' ),
+  ),
+);
+
+$of_options[] = array(
+  "name"      => "Jumbotron Color",
+  "desc"      => "Select the text color for your Jumbotron area. Please note that this area will only be visible if you assign a widget to the \"Jumbotron\" Widget Area. Default: #333333.",
+  "id"        => "jumbotron_color",
+  "std"       => "#333333",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Display Jumbotron only on the Frontpage.",
+  "desc"      => "When Turned OFF, the Jumbotron area is displayed in all your pages. If you wish to completely disable the Jumbotron, then please remove the widgets assigned to its area and it will no longer be displayed. Default: ON",
+  "id"        => "jumbotron_visibility",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 1,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Full-Width",
+  "desc"      => "When Turned ON, the Jumbotron is no longer restricted by the width of your page, taking over the full width of your screen. This option is useful when you have assigned a slider widget on the Jumbotron area and you want its width to be the maximum width of the screen. Default: OFF.",
+  "id"        => "jumbotron_nocontainer",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 1,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Use fittext script for the title.",
+  "desc"      => "Use the fittext script to enlarge or scale-down the font-size of the widget title to fit the Jumbotron area. Default: OFF",
+  "id"        => "jumbotron_title_fit",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 0,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Center-align the content.",
+  "desc"      => "Turn this on to center-align the contents of the Jumbotron area. Default: OFF",
+  "id"        => "jumbotron_title_fit",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 0,
+  "type"      => "switch"
+);
+
+// Header
+$of_options[] = array(
+  "name"      => "Header",
+  "type"      => "heading"
+);
+
+$of_options[] = array(
+  "name"      => "Display the Header.",
+  "desc"      => "Turn this ON to display the header. Default: OFF",
+  "id"        => "header_toggle",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 0,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Display branding on your Header.",
+  "desc"      => "Turn this ON to display branding (Sitename or Logo)on your Header. Default: ON",
+  "id"        => "header_branding",
+  "less"      => true,
+  "customizer"=> array(),
+  "std"       => 1,
+  "type"      => "switch"
+);
+
+$of_options[] = array(
+  "name"      => "Header Background Color",
+  "desc"      => "Select the background color for your header. Default: #EEEEEE.",
+  "id"        => "header_bg",
+  "std"       => "#EEEEEE",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Header Text Color",
+  "desc"      => "Select the text color for your header. Default: #333333.",
+  "id"        => "header_color",
+  "std"       => "#333333",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+// Footer
+$of_options[] = array(
+  "name"      => "Footer",
+  "type"      => "heading"
+);
+
+$of_options[] = array(
+  "name"      => "Footer Background Color",
+  "desc"      => "Select the background color for your footer. Default: #ffffff.",
+  "id"        => "footer_bg",
+  "std"       => "#ffffff",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Footer Text Color",
+  "desc"      => "Select the text color for your footer. Default: #333333.",
+  "id"        => "footer_color",
+  "std"       => "#333333",
+  "less"      => true,
+  "customizer"=> array(),
+  "type"      => "color"
+);
+
+$of_options[] = array(
+  "name"      => "Footer Text",
+  "desc"      => "The text that will be displayed in your footer. Default: your site's name.",
+  "id"        => "footer_text",
+  "std"       => get_bloginfo( 'name' ),
+  "type"      => "text"
+);
 
 
 // Backup Options
