c330eb960aa1fc868d3652fe26a8c5a5fba4c1df
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-22 22:27:39 +0300

    Background Image function

---
+++ lib/customizer/functions.background.php
@@ -0,0 +1,32 @@
+<?php
+
+function shoestrap_background_css() {
+  // $background is the saved custom image, or the default image.
+  $background = set_url_scheme( get_theme_mod( 'background_image' ) );
+  // $color is the saved custom color.
+  // A default has to be specified in style.css. It will not be printed here.
+  $color = get_theme_mod( 'jumbotron_bg' );
+
+  if ( ! $background && ! $color )
+    return;
+
+  $style = $color ? "background-color: #$color;" : '';
+
+  if ( $background ) {
+    $image = " background-image: url('$background');";
+
+    $repeat = get_theme_mod( 'background_repeat', 'repeat' );
+    if ( ! in_array( $repeat, array( 'no-repeat', 'repeat-x', 'repeat-y', 'repeat' ) ) )
+      $repeat = 'repeat';
+    $repeat = " background-repeat: $repeat;";
+
+    $position = get_theme_mod( 'background_position_x', 'left' );
+    if ( ! in_array( $position, array( 'center', 'right', 'left' ) ) )
+      $position = 'left';
+    $position = " background-position: top $position;";
+
+    $style .= $image . $repeat . $position;
+  }
+  echo '<style>body{' . trim( $style ) . ';}</style>';
+}
+add_action( 'wp_head', 'shoestrap_background_css' );
