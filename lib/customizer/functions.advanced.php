646334dd9355c8a7180f4cb954726db3b3313370
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-24 20:52:02 +0300

    bugfix

--- lib/customizer/functions.advanced.php
+++ lib/customizer/functions.advanced.php
@@ -7,7 +7,7 @@ function shoestrap_user_css() {
   $header_scripts = shoestrap_getVariable( 'user_css' );
   echo '<style>' . $header_scripts . '</style>';
 }
-add_action( 'wp_head', 'shoestrap_custom_header_scripts', 200 );
+add_action( 'wp_head', 'shoestrap_user_css', 200 );
 
 /*
  * echo any custom JS the user has written to the footer of the page
@@ -16,4 +16,4 @@ function shoestrap_user_js() {
   $footer_scripts = shoestrap_getVariable( 'user_js' );
   echo '<script>' . $footer_scripts . '</script>';
 }
-add_action( 'wp_footer', 'shoestrap_custom_footer_scripts', 200 );
+add_action( 'wp_footer', 'shoestrap_user_js', 200 );
