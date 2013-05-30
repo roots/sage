0402af77c7d3593819883ac780e7fe7b301b81b4
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-30 07:11:20 +0300

    content template modifications

--- templates/content.php
+++ templates/content.php
@@ -4,6 +4,19 @@
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
-    <?php the_excerpt(); ?>
+    <?php
+    do_action( 'shoestrap_pre_entry_summary' );
+    if ( !has_action( 'shoestrap_do_the_excerpt' ) )
+      the_excerpt();
+    else
+      do_action( 'shoestrap_do_the_excerpt' );
+
+    do_action( 'shoestrap_post_entry_summary' )
+    ?>
   </div>
+  <?php if ( has_action( 'shoestrap_entry_footer' ) ) : ?>
+  <footer>
+    <?php do_action( 'shoestrap_entry_footer' ); ?>
+  </footer>
+  <?php endif; ?>
 </article>
