fa61c7fcc9a0db28b144b8588567288ae4fc729c
Dovy Paukstys <dovy@rtcollab.com>
2013-06-01 01:01:12 -0600

    Few small notes for myself. Last remaining items for the customizer. Yay.

--- lib/admin/init.php
+++ lib/admin/init.php
@@ -1,9 +1,26 @@
 <?php
 
 require_once locate_template('/lib/admin/functions/functions.admin.php'); 
-require_once locate_template('/lib/admin/functions/functions.options.php'); 
+//require_once locate_template('/lib/admin/functions/functions.options.php'); 
 require_once locate_template('/lib/admin/functions/functions.interface.php'); 
 require_once locate_template('/lib/admin/functions/functions.customizer.php'); 
 require_once locate_template('/lib/admin/functions/functions.customizer.standard.php'); 
 
 
+/*
+
+CUSTOMIZER TODO
+
+Customizer Basic
+	Fix Checkboxes!!!
+
+Customizer Pro
+	Hide Areas (Fold)
+	Font Selector (Google Font & Regular)
+	Borders
+
+Options Panel
+	Advanced Typography Type
+	
+
+*/
