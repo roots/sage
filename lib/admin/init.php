999978269d857d64c0fee912e59f259c93cfa551
Dovy Paukstys <dovy@rtcollab.com>
2013-06-01 01:02:32 -0600

    Whoops, we want our function.options back.

--- lib/admin/init.php
+++ lib/admin/init.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once locate_template('/lib/admin/functions/functions.admin.php'); 
-//require_once locate_template('/lib/admin/functions/functions.options.php'); 
+require_once locate_template('/lib/admin/functions/functions.options.php'); 
 require_once locate_template('/lib/admin/functions/functions.interface.php'); 
 require_once locate_template('/lib/admin/functions/functions.customizer.php'); 
 require_once locate_template('/lib/admin/functions/functions.customizer.standard.php'); 
