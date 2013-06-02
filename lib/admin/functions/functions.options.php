592bc9d516c0e297532a4f0e060c17a61ab4c579
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-02 22:43:58 +0300

    Moving backup options to a module

--- lib/admin/functions/functions.options.php
+++ lib/admin/functions/functions.options.php
@@ -133,30 +133,6 @@ if (!function_exists('of_options_shoestrap')) {
 
     $of_options = array();
 
-    // Backup Options
-    $of_options[] = array(
-      "name"      => __("Backup Options", "shoestrap"),
-      "type"      => "heading"
-    );
-
-    $of_options[] = array(
-      "name"      => __("Backup and Restore Options", "shoestrap"),
-      "id"        => "of_backup",
-      "std"       => "",
-      "type"      => "backup",
-      "desc"      => __('You can use the two buttons below to backup your current options, and then restore it back at a later time. This is useful if you want to experiment on the options but would like to keep the old settings in case you need it back.', "shoestrap"),
-    );
-
-    $of_options[] = array(
-      "name"      => __("Transfer Theme Options Data", "shoestrap"),
-      "id"        => "of_transfer",
-      "std"       => "",
-      "type"      => "transfer",
-      "desc"      => __('You can tranfer the saved options data between different installs by copying the text inside the text box. To import data from another install, replace the data in the text box with the one from another install and click "Import Options".', "shoestrap"),
-    );
-
-    do_action('smof_options_modifier');
-
     $smof_details = array();
     foreach($of_options as $option) {
       $smof_details[$option['id']] = $option;
