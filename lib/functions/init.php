4e605bba820ae73006be409eae28b7634fba27a4
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-03 17:21:16 +0300

    moved the modules parser t load.modules.php

--- lib/functions/init.php
+++ lib/functions/init.php
@@ -4,17 +4,4 @@ require_once locate_template('/admin/index.php'); // Slightly Modified Options F
 require_once locate_template('/lib/admin/init.php'); // Shoestrap SMOF modifications
 
 require_once locate_template('/lib/functions/helper.functions.php');         // Helper functions for the customizer
-
-// Include all modules
-$modules_path = new RecursiveDirectoryIterator(locate_template('/lib/modules/'));
-$recIterator  = new RecursiveIteratorIterator($modules_path);
-$regex        = new RegexIterator($recIterator, '/\/module.php$/i');
-
-foreach($regex as $item) {
-  require_once $item->getPathname();
-}
-
-// Add extra features
-if (locate_template('/lib/extensions/init.php')) {
- require_once locate_template('/lib/extensions/init.php');
-}
+require_once locate_template('/lib/modules/load.modules.php');         // Helper functions for the customizer
---
+++ lib/modules/load.modules.php
@@ -0,0 +1,10 @@
+<?php
+
+// Include all modules
+$modules_path = new RecursiveDirectoryIterator( locate_template( '/lib/modules/' ) );
+$recIterator  = new RecursiveIteratorIterator( $modules_path );
+$regex        = new RegexIterator( $recIterator, '/\/module.php$/i' );
+
+foreach( $regex as $item ) {
+  require_once $item->getPathname();
+}
