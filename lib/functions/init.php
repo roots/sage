e09f7d9fe27736ab69e54b8fa902e650137dfe3a
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-03 17:47:17 +0300

    removing deprecated references

--- lib/functions/init.php
+++ lib/functions/init.php
@@ -1,7 +1,3 @@
 <?php
 
-require_once locate_template('/admin/index.php'); // Slightly Modified Options Framework
-require_once locate_template('/lib/admin/init.php'); // Shoestrap SMOF modifications
-
-require_once locate_template('/lib/functions/helper.functions.php');         // Helper functions for the customizer
 require_once locate_template('/lib/modules/load.modules.php');         // Helper functions for the customizer
