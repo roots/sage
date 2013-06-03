fd1c5a9933f317042b9a4be53d0b86d6286f0706
Dovy Paukstys <dovy@rtcollab.com>
2013-06-03 11:40:50 -0600

    Remove the double backup as well as added the source for SMOF to the repo.

--- lib/modules/core.presets/module.php
+++ lib/modules/core.presets/module.php
@@ -57,28 +57,6 @@ if ( !function_exists( 'shoestrap_module_presets_options' ) ) {
       "options"   => $preset_styles,
     );
 
-    // Backup Options
-    $of_options[] = array(
-      "name"      => __("Backup Options", "shoestrap"),
-      "type"      => "heading"
-    );
-
-    $of_options[] = array(
-      "name"      => __("Backup and Restore Options", "shoestrap"),
-      "id"        => "of_backup",
-      "std"       => "",
-      "type"      => "backup",
-      "desc"      => __('You can use the two buttons below to backup your current options, and then restore it back at a later time. This is useful if you want to experiment on the options but would like to keep the old settings in case you need it back.', "shoestrap"),
-    );
-
-    $of_options[] = array(
-      "name"      => __("Transfer Theme Options Data", "shoestrap"),
-      "id"        => "of_transfer",
-      "std"       => "",
-      "type"      => "transfer",
-      "desc"      => __('You can tranfer the saved options data between different installs by copying the text inside the text box. To import data from another install, replace the data in the text box with the one from another install and click "Import Options".', "shoestrap"),
-    );
-
     do_action( 'shoestrap_module_presets_options_modifier' );
 
     $smof_details = array();
