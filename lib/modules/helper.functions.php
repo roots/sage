8a039204e8dd6d2d5766db24ddc19662fd637fae
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-04 20:22:42 +0300

    removed redirection to theme options on activation

--- lib/modules/helper.functions.php
+++ lib/modules/helper.functions.php
@@ -106,10 +106,3 @@ function shoestrap_mix_colors( $hex1, $hex2, $percentage ) {
 
   return '#'.$r_hex.$g_hex.$b_hex;
 }
-
-// Redirect to settings page on theme activation
-global $pagenow;
-if ( is_admin() && isset( $_GET['activated'] ) && $pagenow == 'themes.php' ) {
-  wp_redirect( admin_url( 'themes.php?page=optionsframework' ) );
-  exit;
-}
