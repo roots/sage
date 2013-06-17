8a92e600def2b057b2a93dee2c33e3eac5ae7642
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-17 19:50:57 +0300

    get_template_directory() instead of locate_template()for modules
    Core modules can't be overriden.
    That's why they are called CORE modules.

--- lib/modules/load.modules.php
+++ lib/modules/load.modules.php
@@ -1,10 +1,10 @@
 <?php
 
 // Helper functions required BEFORE the modules are loaded
-require_once locate_template('/lib/modules/helper.functions.php');
+require_once get_template_directory() . '/lib/modules/helper.functions.php';
 
-// Include all modules
-$modules_path = new RecursiveDirectoryIterator( locate_template( '/lib/modules/' ) );
+// Include all modules from the shoestrap theme (NOT the child themes)
+$modules_path = new RecursiveDirectoryIterator( get_template_directory() . '/lib/modules/' );
 $recIterator  = new RecursiveIteratorIterator( $modules_path );
 $regex        = new RegexIterator( $recIterator, '/\/module.php$/i' );
 
