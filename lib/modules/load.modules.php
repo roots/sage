f70878aa4f925df587f3c0aace0c461178e6e435
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-18 21:15:10 +0300

    simplified module loading for previous PHP versions

--- lib/modules/load.modules.php
+++ lib/modules/load.modules.php
@@ -4,18 +4,11 @@
 require_once get_template_directory() . '/lib/modules/helper.functions.php';
 
 // PHP version control
-$php_ver_arr = explode( '.', phpversion() );
-$php_ver_arr_2 = explode('-', $php_ver_arr[2]);
-
-if ( ($php_ver_arr[0] >= 5) && ($php_ver_arr[1] > 2) ){
-	add_action( 'init', 'shoestrap_include_modules');
-}
-elseif ( ($php_ver_arr[0] >= 5) && ($php_ver_arr[1] = 2) && ($php_ver_arr_2[0] >= 11)){
-	add_action( 'init', 'shoestrap_include_modules');
-	}
-else {
-	add_action( 'init', 'shoestrap_include_modules_fallback');
-}
+$phpversion = phpversion();
+if ( version_compare( $phpversion, '5.2.11', '<' ) )
+  shoestrap_include_modules();
+else
+  shoestrap_include_modules_fallback();
 
 // Use 'RecursiveDirectoryIterator' if >= 5.2.11
 function shoestrap_include_modules(){
@@ -23,17 +16,16 @@ function shoestrap_include_modules(){
 	$modules_path = new RecursiveDirectoryIterator( get_template_directory() . '/lib/modules/' );
 	$recIterator  = new RecursiveIteratorIterator( $modules_path );
 	$regex        = new RegexIterator( $recIterator, '/\/module.php$/i' );
-	
+
 	foreach( $regex as $item ) {
 	  require_once $item->getPathname();
 	}
 }
 
-// Fallback in 'glob' if != 5.2.11
-function shoestrap_include_modules_fallback(){
+// Fallback in 'glob' if < 5.2.11
+function shoestrap_include_modules_fallback() {
 	// Include all modules from the shoestrap theme (NOT the child themes)
-	foreach(glob(get_template_directory() . '/lib/modules/*/module.php') as $module) 
-	{	
+	foreach( glob( get_template_directory() . '/lib/modules/*/module.php' ) as $module ) {
 		require_once $module;
 	}
 }
