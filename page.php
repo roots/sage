96282b8c02420d31ed74cdf2f5ba3b2276354a7a
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-18 16:55:58 +0300

    renaming shoestrap_page_header hook to shoestrap_page_header_override

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php
 
-if ( !has_action( 'shoestrap_page_header' ) )
+if ( !has_action( 'shoestrap_page_header_override' ) )
 	get_template_part('templates/page', 'header');
 else
-	do_action( 'shoestrap_page_header' );
+	do_action( 'shoestrap_page_header_override' );
 
 if ( !has_action( 'shoestrap_single_page_content' ) )
 	get_template_part('templates/content', 'page');
