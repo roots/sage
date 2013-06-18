4fc2ba7f1620326c30ce7428d192e19aa139a2a0
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-18 17:08:34 +0300

    bugfix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( !has_action( 'shoestrap_page_header_override' )
+if ( !has_action( 'shoestrap_page_header_override' ) )
   get_template_part('templates/page', 'header');
 else
   do_action( 'shoestrap_page_header_override' );
