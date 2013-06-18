0c29b0e7c40974b765938093a95ca103accd2d74
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-18 16:54:37 +0300

    simple syntax improvement

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ if ( !has_action( 'shoestrap_page_header_override' )
   get_template_part('templates/page', 'header');
 else
   do_action( 'shoestrap_page_header_override' );
-
-do_action( 'shoestrap_index_begin' );
 ?>
+
+<?php do_action( 'shoestrap_index_begin' ); ?>
 
 <?php if (!have_posts()) : ?>
   <div class="alert">
