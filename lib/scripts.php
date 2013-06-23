2a347f73412d4cfcaec13c54eb2abc616561da20
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-23 21:04:23 +0300

    Update scripts.php

--- lib/scripts.php
+++ lib/scripts.php
@@ -3,8 +3,9 @@
  * Enqueue scripts and stylesheets
  *
  * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/style.css
- * 4. /child-theme/style.css (if a child theme is activated)
+ * 1. /theme/assets/css/bootstrap.css
+ * 2. /theme/assets/css/bootstrap-responsive.css
+ * 3. /theme/assets/css/app.css
  *
  * Enqueue scripts in the following order:
  * 1. jquery-1.10.1.min.js via Google CDN
