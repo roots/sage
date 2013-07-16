50bd47a868f17740395ba36f01e7d98f05e8eb77
Dovy Paukstys <dovy@rtcollab.com>
2013-07-16 11:54:12 -0600

    Activation issues.

--- lib/modules/core.admin/addons/functions/functions.admin.php
+++ lib/modules/core.admin/addons/functions/functions.admin.php
@@ -5,17 +5,24 @@ $smof_details = array();
 function shoestrap_of_save_options_after($data) {
 	global $smof_details, $smof_data;
 	$lessChanged = false;
-
 	if (is_array($data['data'])) {
 		foreach ( $smof_details as $key=>$option ) {
 		  if ( isset($option['less']) ) {
-		    if ( $smof_data[$key] != $data['data'][$key] ) {
+		    if ( isset($smof_data[$key]) && $smof_data[$key] != $data['data'][$key] ) {
 		      $lessChanged = true;
 		    }
 		  }
+		  if (isset($option['urlrewrite'])) {
+		  	set_theme_mod( 'urlrewrite', 1 );
+		  }
 		}		
-	} else if (($data['data'] != $smof_data[$data['key']]) && $smof_data[$data['key']]['less'] == true) {
-		$lessChanged = true;
+	} else if (($data['data'] != $smof_data[$data['key']])) {
+		if ($smof_data[$data['key']]['less'] == true) {
+			$lessChanged = true;	
+		}
+		if ($smof_data[$data['key']]['urlrewrite'] == true) {
+			set_theme_mod( 'urlrewrite', 1 );
+		}
 	}
 
   if ( $lessChanged == true ){
@@ -31,7 +38,7 @@ function shoepress_optionsframework_admin_init_before($data) {
 
 	if (empty($data['smof_data']['smof_init']) || empty($smof_details)) {
 		foreach ($data['of_options'] as $option) {
-			if ($option['id'] != "")
+			if (isset($option['id']) && $option['id'] != "")
 				$smof_details[$option['id']] = $option;
 		}
 	}
