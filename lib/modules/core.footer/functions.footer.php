384ead043910019c66ab1e46a6787b766b69b953
Dovy Paukstys <dovy@rtcollab.com>
2013-07-16 11:55:53 -0600

    Hide on mobile.

--- lib/modules/core.footer/functions.footer.php
+++ lib/modules/core.footer/functions.footer.php
@@ -58,7 +58,7 @@ function shoestrap_footer_icon() {
   <?php if (current_user_can( 'edit_theme_options' ) && !isset( $wp_customize ) ){ ?>
     <style>
     </style>
-    <div id="shoestrap_icon">
+    <div id="shoestrap_icon" class="visible-lg">
       <?php
       $current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
       $href = add_query_arg( 'url', urlencode( $current_url ), wp_customize_url() ); ?>
