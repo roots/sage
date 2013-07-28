3bde5dbf7736e223afcfc3117ec536fc70bb0239
Foxaii <nick@foxaii.com>
2013-07-28 13:21:50 +0100

    BS3 markup for templates

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,18 +1,18 @@
 <header class="banner navbar navbar-static-top" role="banner">
   <div class="navbar-inner">
     <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <button data-target=".nav-main" data-toggle="collapse" type="button" class="navbar-toggle">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-      </a>
-      <a class="brand" href="<?php echo home_url(); ?>/">
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/">
         <?php bloginfo('name'); ?>
       </a>
       <nav class="nav-main nav-collapse collapse" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
+            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
           endif;
         ?>
       </nav>
