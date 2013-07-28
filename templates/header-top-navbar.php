34696d2a80babeda5f69564c829befb2ccac80a0
Ben Word <ben@benword.com>
2013-07-28 11:52:42 -0500

    Clean up top navbar markup

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,21 +1,19 @@
 <header class="banner navbar navbar-static-top" role="banner">
-  <div class="navbar-inner">
-    <div class="container">
-      <button data-target=".nav-main" data-toggle="collapse" type="button" class="navbar-toggle">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/">
-        <?php bloginfo('name'); ?>
-      </a>
-      <nav class="nav-main nav-collapse collapse" role="navigation">
-        <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-          endif;
-        ?>
-      </nav>
-    </div>
+  <div class="container">
+    <a class="navbar-brand" href="<?php echo home_url(); ?>/">
+      <?php bloginfo('name'); ?>
+    </a>
+    <button data-target=".nav-main" data-toggle="collapse" type="button" class="navbar-toggle">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <nav class="nav-main nav-collapse collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+    </nav>
   </div>
 </header>
