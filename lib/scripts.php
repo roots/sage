9cddab47a62b3620d21dfe6c8372128c6336e4c3
Jackson Whelan <jw@jacksonwhelan.com>
2013-08-06 19:06:01 -0400

    Add cache busting version number for app.css based on last file modified time.

--- lib/scripts.php
+++ lib/scripts.php
@@ -18,7 +18,7 @@
 function roots_scripts() {
   wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
   wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
-  wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
+  wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false,  filemtime( get_template_directory() . '/assets/css/app.css'));
 
   // Load style.css from child theme
   if (is_child_theme()) {
