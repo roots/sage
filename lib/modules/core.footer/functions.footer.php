f5991455bb9b9559d281d16f6420a21eedb93b4f
Aristeides Stathopoulos <aristath@gmail.com>
2013-08-06 19:42:09 +0300

    changing cog to point to admin panel

--- lib/modules/core.footer/functions.footer.php
+++ lib/modules/core.footer/functions.footer.php
@@ -56,13 +56,8 @@ function shoestrap_footer_icon() {
   global $wp_customize;
   ?>
   <?php if (current_user_can( 'edit_theme_options' ) && !isset( $wp_customize ) ){ ?>
-    <style>
-    </style>
     <div id="shoestrap_icon" class="visible-lg">
-      <?php
-      $current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-      $href = add_query_arg( 'url', urlencode( $current_url ), wp_customize_url() ); ?>
-      <a href="<?php echo $href; ?>"><i class="icon icon-cogs"></i></a>
+      <a href="<?php echo admin_url( 'themes.php?page=optionsframework' ); ?>"><i class="icon icon-cogs"></i></a>
     </div>
   <?php } ?>
   </div>
