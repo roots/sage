45b6d0207e91f8f0e6fc8bbdd015638f6ef8a33f
Aristeides Stathopoulos <aristath@gmail.com>
2013-08-08 19:38:54 +0300

    fixes #275

--- lib/modules/core.header/functions.navbar.php
+++ lib/modules/core.header/functions.navbar.php
@@ -13,42 +13,19 @@ function shoestrap_nav_class_pull() {
  * The template for the primary navbar searchbox
  */
 function shoestrap_navbar_pre_searchbox() {
-  if ( shoestrap_getVariable( 'navbar_nav_right' ) == '1' ) {
-    $show_searchbox = shoestrap_getVariable( 'navbar_search' );
-    if ( $show_searchbox == '1' ) { ?>
-      <form role="search" method="get" id="searchform" class="form-search pull-right navbar-form" action="<?php echo home_url('/'); ?>">
-        <label class="hide" for="s"><?php _e('Search for:', 'roots'); ?></label>
-        <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="form-control search-query" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
-      </form>
-      <?php
-    }
+  $show_searchbox = shoestrap_getVariable( 'navbar_search' );
+  if ( $show_searchbox == '1' ) { ?>
+    <form role="search" method="get" id="searchform" class="form-search pull-right navbar-form" action="<?php echo home_url('/'); ?>">
+      <label class="hide" for="s"><?php _e('Search for:', 'roots'); ?></label>
+      <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="form-control search-query" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+    </form>
+    <?php
   }
 }
 add_action( 'shoestrap_pre_main_nav', 'shoestrap_navbar_pre_searchbox', 11 );
 
 
 
-
-/*
- * The template for the primary navbar searchbox
- */
-function shoestrap_navbar_post_searchbox() {
-  if ( shoestrap_getVariable( 'navbar_nav_right' ) != '1' ) {
-    $show_searchbox = shoestrap_getVariable( 'navbar_search' );
-    if ( $show_searchbox == '1' ) { ?>
-      <ul class="pull-right nav nav-collapse clearfix"><li>
-      <?php do_action('shoestrap_pre_searchform'); ?>
-      <form role="search" method="get" id="searchform" class="form-search navbar-search" action="<?php echo home_url('/'); ?>">
-        <label class="hide" for="s"><?php _e('Search for:', 'shoestrap'); ?></label>
-        <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="search-query" placeholder="<?php _e('Search', 'shoestrap'); ?> <?php bloginfo('name'); ?>">
-      </form>
-      <?php do_action('shoestrap_after_searchform'); ?>
-      </li></ul>
-      <?php
-    }
-  }
-}
-add_action( 'shoestrap_post_main_nav', 'shoestrap_navbar_post_searchbox', 11 );
 
 function shoestrap_navbar_class( $navbar = 'main') {
   $fixed    = shoestrap_getVariable( 'navbar_fixed' );
