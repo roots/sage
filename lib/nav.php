4d605dba46d1c8d8cbe574b3ba99b704ddd51b38
Flavius Andrei <flavius@artworkmonger.net>
2013-08-11 04:38:31 +0300

    wp_nav_menu() 'depth' argument now works
    the depth of the submenus is forced to 2 levels, hence wp_nav_menu's depth argument was not being used.
    
    now a user can set the desired depth and the menu item with a submenu won't display the parent menu 'caret' when depth is set to 1 (level)

--- lib/nav.php
+++ lib/nav.php
@@ -23,7 +23,7 @@ class Roots_Nav_Walker extends Walker_Nav_Menu {
     $item_html = '';
     parent::start_el($item_html, $item, $depth, $args);
 
-    if ($item->is_dropdown && ($depth === 0)) {
+    if ($item->is_dropdown && ($depth === 0 && $args->depth !== 1)) {
       $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $item_html);
       $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
     }
@@ -80,9 +80,9 @@ function roots_nav_menu_args($args = '') {
     $roots_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
   }
 
-  if (current_theme_supports('bootstrap-top-navbar')) {
-    $roots_nav_menu_args['depth'] = 2;
-  }
+  //if (current_theme_supports('bootstrap-top-navbar')) {
+    //$roots_nav_menu_args['depth'] = 2;
+  //}
 
   if (!$args['walker']) {
     $roots_nav_menu_args['walker'] = new Roots_Nav_Walker();
