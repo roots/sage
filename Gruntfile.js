30ace3c6477b0d93ba22031cbb4b20b05c2559d5
Weslly Honorato <weslly.honorato@gmail.com>
2013-08-24 17:23:14 -0300

    Added browser live reloading to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,17 @@ module.exports = function(grunt) {
           '<%= jshint.all %>'
         ],
         tasks: ['jshint', 'uglify', 'version']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: false
+        },
+        files: [
+          'assets/css/main.min.css',
+          'assets/js/scripts.min.js'
+        ]
       }
     },
     clean: {
