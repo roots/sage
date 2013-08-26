0f4dd585b7ed3265779c3561464666d0ae3023ad
Aristeides Stathopoulos <aristath@gmail.com>
2013-08-26 12:07:59 +0300

    Some Shoestrap files

--- lib/scripts.php
+++ lib/scripts.php
@@ -3,15 +3,23 @@
  * Enqueue scripts and stylesheets
  *
  * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/main.min.css
+ * 1. /theme/assets/css/bootstrap.css
+ * 2. /theme/assets/css/bootstrap-responsive.css
+ * 3. /theme/assets/css/app.css
  *
  * Enqueue scripts in the following order:
  * 1. jquery-1.10.2.min.js via Google CDN
  * 2. /theme/assets/js/vendor/modernizr-2.6.2.min.js
- * 3. /theme/assets/js/main.min.js (in footer)
+ * 3. /theme/assets/js/plugins.js (in footer)
+ * 4. /theme/assets/js/main.js    (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c9f2155733c68254f446eb06faa23581');
+
+  // Ensure we're not on the customize page. Conflicts with LESS
+  // global $wp_customize;
+  // if ( !isset( $wp_customize ) ) {
+    wp_enqueue_style('shoestrap_css', shoestrap_css( 'url' ), false, null);
+  // }
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -27,10 +35,24 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '2a3e700c4c6e3d70a95b00241a845695', true);
-  wp_enqueue_script('modernizr');
+  wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/bootstrap.min.js', false, null, true);
+  wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
   wp_enqueue_script('jquery');
-  wp_enqueue_script('roots_scripts');
+  wp_enqueue_script('modernizr');
+  wp_enqueue_script('roots_plugins');
+  wp_enqueue_script('roots_main');
+
+  if ( shoestrap_getVariable( 'pjax' ) == 1 ) {
+    wp_register_script('jquery_pjax', get_template_directory_uri() . '/assets/js/jquery.pjax.js', false, null, true);
+    wp_enqueue_script('jquery_pjax');
+  }
+
+  if ( shoestrap_getVariable( 'retina_toggle' ) == 1 ) {
+    wp_register_script('retinajs', get_template_directory_uri() . '/assets/js/vendor/retina.js', false, null, true);
+    wp_enqueue_script('retinajs');
+  }
+  wp_register_script('fitvids', get_template_directory_uri() . '/assets/js/vendor/jquery.fitvids.js', false, null, true);
+  wp_enqueue_script('fitvids');
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
