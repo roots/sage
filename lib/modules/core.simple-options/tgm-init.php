0c31478525cb055a4da4dab4be83a2b57c76d89a
Dovy Paukstys <dovy@rtcollab.com>
2013-08-30 10:26:33 -0600

    Small update fix for SOF.

--- lib/modules/core.simple-options/tgm-init.php
+++ lib/modules/core.simple-options/tgm-init.php
@@ -11,7 +11,7 @@ function shoestrap_register_required_plugins() {
 			'version' 		=> '0.0.2',
 			'force_activation' 	=> true,
 			'external_url' 		=> 'http://github.com/SimpleRain/SimpleOptions/',
-			'source' 		=> dirname(__FILE__).'/SimpleOptions-0.3.1.zip', // The plugin source
+			'source' 		=> 'https://github.com/SimpleRain/SimpleOptions/archive/0.4.1.zip', // The plugin source
 		),
 	);
 
