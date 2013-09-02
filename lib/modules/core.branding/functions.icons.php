fc58034210619a0cb870d3e7d6d7d77268726b4f
Aristeides Stathopoulos <aristath@gmail.com>
2013-09-02 21:40:21 +0300

    Branding module syntax improvements

--- lib/modules/core.branding/functions.icons.php
+++ lib/modules/core.branding/functions.icons.php
@@ -16,13 +16,12 @@ function shoestrap_icons() {
   	$iphone_icon_retina = matthewruddy_image_resize( $apple_icon_item['url'], 57, 57, true, true );
   	$ipad_icon          = matthewruddy_image_resize( $apple_icon_item['url'], 72, 72, true, false );
   	$ipad_icon_retina   = matthewruddy_image_resize( $apple_icon_item['url'], 72, 72, true, true );
-  ?>
+    ?>
     <!-- For iPhone --><link rel="apple-touch-icon-precomposed" href="<?php echo $iphone_icon['url'] ?>">
     <!-- For iPhone 4 Retina display --><link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $iphone_icon_retina['url'] ?>">
     <!-- For iPad --><link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $ipad_icon['url'] ?>">
     <!-- For iPad Retina display --><link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $ipad_icon_retina['url'] ?>">
-  <?php endif;
-
-//  echo $content;
+    <?php
+  endif;
 }
 add_action( 'wp_head', 'shoestrap_icons' );
