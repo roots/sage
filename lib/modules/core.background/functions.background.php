f3ce206d0ec4b1f0da1ee7edce70769c4f9da856
Aristeides Stathopoulos <aristath@gmail.com>
2013-09-04 19:13:48 +0300

    Background improvements

--- lib/modules/core.background/functions.background.php
+++ lib/modules/core.background/functions.background.php
@@ -2,57 +2,57 @@
 
 function shoestrap_background_css( ) {
 
-	if ( shoestrap_getVariable( 'background_image_toggle' ) == 0 && shoestrap_getVariable( 'background_pattern_toggle' ) == 0 )
+  $image_toggle   = shoestrap_getVariable( 'background_image_toggle' );
+  $bg_custom_img  = shoestrap_getVariable( 'background_custom_image' );
+  $bg_img         = shoestrap_getVariable( 'background_image' );
+  $pattern_toggle = shoestrap_getVariable( 'background_pattern_toggle' );
+  $bg_pattern     = shoestrap_getVariable( 'background_pattern' );
+  $bg_color       = shoestrap_getVariable( 'color_body_bg' );
+
+	if ( $image_toggle == 0 && shoestrap_getVariable( 'background_pattern_toggle' ) == 0 )
 		return;
 
   $style    = '';
   $image    = '';
   $repeat   = '';
   $position = '';
+
   // $background is the saved custom image, or the default image.
-  if ( shoestrap_getVariable( 'background_image_toggle' ) == 1 ) {
-    if ( shoestrap_getVariable( 'background_custom_image' ) != "" ) {
-    	$background = set_url_scheme( shoestrap_getVariable( 'background_custom_image' ) );
-    	$background = $background['url'];
-    }  else if ( shoestrap_getVariable( 'background_image' ) != "" ) {
-    	$background = set_url_scheme( shoestrap_getVariable( 'background_image' ) );
-    	$background = $background['url'];
-    }
-  } else if ( shoestrap_getVariable( 'background_pattern_toggle' ) == 1 && shoestrap_getVariable( 'background_pattern' ) != "" ) {
-    $background = set_url_scheme( shoestrap_getVariable( 'background_pattern' ) );
-    $background = $background['url'];
-  }
+  if ( $image_toggle == 1 ) :
 
-  if ( shoestrap_getVariable( 'color_body_bg' ) != "" )
-    $color = '#' . str_replace( '#', '', shoestrap_getVariable( 'color_body_bg' ) ) . ';';
+    if ( $bg_custom_img != '' )
+      $background = set_url_scheme( $bg_custom_img['url'] );
+    elseif ( $bg_img != '' )
+      $background = set_url_scheme( $bg_img['url'] );
 
-  if ( shoestrap_getVariable( 'color_body_bg_opacity' ) != 100 ) {
+  elseif ( $pattern_toggle == 1 && $bg_pattern != '' ) :
+    $background = set_url_scheme( $bg_pattern['url'] );
+  endif;
+
+  $color = '#' . str_replace( '#', '', $bg_color ) . ';';
+
+  if ( shoestrap_getVariable( 'color_body_bg_opacity' ) != 100 ) :
     $rgb      = shoestrap_get_rgb( $color, true );
     $opacity  = ( shoestrap_getVariable( 'color_body_bg_opacity' ) ) / 100;
     $color   .= 'background: rgba(' . $rgb . ',' . $opacity . ');';
-  }
-
-  if ( !isset($background) && !isset($color) )
-    return;
+  endif;
 
   if ( shoestrap_getVariable( 'background_fixed_toggle' ) == 1 )
     $style .= "background-attachment: fixed;";
 
-  if ( isset($background) )
+  if ( isset( $background ) )
     $image .= "background-image: url( '$background' );";
 
-  if ( shoestrap_getVariable( 'background_image_toggle' ) == 1 && ( shoestrap_getVariable( 'background_custom_image' ) != "" || shoestrap_getVariable( 'background_image' ) != "" ) ) {
-    if ( shoestrap_getVariable( 'background_image_position_toggle' ) == 0 ) {
-      $style .= "background-size: cover;";
-      $style .= "-webkit-background-size: cover;";
-      $style .= "-moz-background-size: cover;";
-      $style .= "-o-background-size: cover;";
-      $style .= "background-position: 50% 50%;";
+  if ( $image_toggle == 1 && ( $bg_custom_img != '' || $bg_img != '' ) ) :
 
-      if ( shoestrap_getVariable( 'jumbotron_background_fixed_toggle' ) == 0 )
-        $style .= "background-repeat: no-repeat;";
+    if ( shoestrap_getVariable( 'background_image_position_toggle' ) == 0 ) :
+      $style .= 'background-size: cover;';
+      $style .= '-webkit-background-size: cover;';
+      $style .= '-moz-background-size: cover;';
+      $style .= '-o-background-size: cover;';
+      $style .= 'background-position: 50% 50%;';
 
-    } else {
+    else :
       // Not fixed position, custom
       $repeat = shoestrap_getVariable( 'background_repeat' );
 
@@ -69,8 +69,8 @@ function shoestrap_background_css( ) {
         $position = 'left';
 
       $position = " background-position: top $position;";
-    }
-  }
+    endif;
+  endif;
 
   $style .= $image . $repeat . $position;
 
