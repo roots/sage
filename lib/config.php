6b468866f5372bb627c45c3364ab187b2e41a03b
Werther <werther.d@gmail.com>
2013-09-11 12:08:15 +0200

    Can you add an option for dropdown menu?
    Hover on menu item with submenu shows the dropdown menu and the menu item maintain the original href value

--- lib/config.php
+++ lib/config.php
@@ -8,6 +8,7 @@ add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's top navbar
 add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails component on [gallery]
 add_theme_support('nice-search');           // Enable /?s= to /search/ redirect
 add_theme_support('jquery-cdn');            // Enable to load jQuery from the Google CDN
+// add_theme_support('dropdown_menu_hover');   // Enable dropdown menu on hover
 
 /**
  * Configuration values
