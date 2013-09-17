92213e5abaf1180febaeaa9828106646d530d136
Aristeides Stathopoulos <aristath@gmail.com>
2013-09-17 20:57:27 +0300

    Fixing full width images

--- lib/modules/load.modules.php
+++ lib/modules/load.modules.php
@@ -3,6 +3,7 @@
 // Prioritize loading of some necessary core modules
 require_once get_template_directory() . '/lib/modules/core.redux/module.php';
 require_once get_template_directory() . '/lib/modules/core/module.php';
+require_once get_template_directory() . '/lib/modules/core.layout/module.php';
 require_once get_template_directory() . '/lib/modules/core.images/module.php';
 
 // PHP version control
