56bd3d811ef9f646a6208bf1d6adcc2cbcf46d35
Marcel Turi <m@turi.co>
2013-09-30 20:01:02 +1300

    Move Google Analytics from bottom of <body> to bottom of <head>
    Based on Google's own recommendations on placement of the analytics.js JavaScript snippet.
    
    https://developers.google.com/analytics/devguides/collection/analyticsjs/

--- lib/scripts.php
+++ lib/scripts.php
@@ -52,16 +52,16 @@ function roots_jquery_local_fallback($src, $handle = null) {
 add_action('wp_head', 'roots_jquery_local_fallback');
 
 function roots_google_analytics() { ?>
-<script>
-  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-  e.src='//www.google-analytics.com/analytics.js';
-  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-  ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>');ga('send','pageview');
-</script>
+  <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>');ga('send','pageview');
+  </script>
 
 <?php }
 if (GOOGLE_ANALYTICS_ID && !current_user_can('manage_options')) {
-  add_action('wp_footer', 'roots_google_analytics', 20);
+  add_action('wp_head', 'roots_google_analytics', 20);
 }
