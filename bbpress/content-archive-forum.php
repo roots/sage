77149896d2acc9b4c26c6e985e37a72100d03911
Aristeides Stathopoulos <aristath@gmail.com>
2013-10-17 16:12:25 +0300

    started working on bbpress templates

---
+++ bbpress/content-archive-forum.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Archive Forum Content Part
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<div id="bbpress-forums">
+
+	<?php if ( bbp_allow_search() ) : ?>
+		<div class="bbp-search-form">
+			<?php bbp_get_template_part( 'form', 'search' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<hr />
+
+	<?php do_action( 'bbp_template_before_forums_index' ); ?>
+
+	<?php if ( bbp_has_forums() ) : ?>
+		<?php bbp_get_template_part( 'loop',     'forums'    ); ?>
+	<?php else : ?>
+		<?php bbp_get_template_part( 'feedback', 'no-forums' ); ?>
+	<?php endif; ?>
+
+	<?php do_action( 'bbp_template_after_forums_index' ); ?>
+
+</div>
---
+++ bbpress/loop-topics.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Topics Loop
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<?php do_action( 'bbp_template_before_topics_loop' ); ?>
+
+<ul id="bbp-forum-<?php bbp_forum_id(); ?>" class="bbp-topics list-group">
+
+	<li class="bbp-header list-group-item">
+
+		<ul class="forum-titles row list-unstyled">
+			<li class="bbp-topic-title col-md-6"><?php _e( 'Topic', 'bbpress' ); ?></li>
+			<li class="bbp-topic-voice-count col-md-2 text-center"><?php _e( 'Voices', 'bbpress' ); ?></li>
+			<li class="bbp-topic-reply-count col-md-2 text-center"><?php bbp_show_lead_topic() ? _e( 'Replies', 'bbpress' ) : _e( 'Posts', 'bbpress' ); ?></li>
+			<li class="bbp-topic-freshness col-md-2"><?php _e( 'Freshness', 'bbpress' ); ?></li>
+		</ul>
+
+	</li>
+
+	<li class="bbp-body list-group-item">
+		<?php while ( bbp_topics() ) : bbp_the_topic(); ?>
+			<?php bbp_get_template_part( 'loop', 'single-topic' ); ?>
+		<?php endwhile; ?>
+	</li>
+
+</ul><!-- #bbp-forum-<?php bbp_forum_id(); ?> -->
+
+<?php do_action( 'bbp_template_after_topics_loop' );
