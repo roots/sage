fddcfd9a543cbf97ccdfc70c146ca06d441e085a
underquol <under.quol@gmail.com>
2013-10-30 19:12:27 +0400

    For safety, replace the detailed information during the login.

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,6 @@
 /**
  * Custom functions
  */
+
+// For safety, replace the detailed information during the login
+add_filter('login_errors', create_function('$a', "return __('Error! Try to go again.', 'roots');"));
