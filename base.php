0058d807107a5dcb188e2ee1f2a6f457da0115ff
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-26 19:37:47 +0200

    rewriting base.php

--- base.php
+++ base.php
@@ -7,139 +7,115 @@
     </div>
   <![endif]-->
 
-  <?php
+  <?php if ( shoestrap_getVariable( 'site_style' ) == 'boxed' ) : ?>
+    <div class="container boxed-container">
+  <?php endif; ?>
 
-  // If the user has selected "boxed" style, add a container div
-  if ( shoestrap_getVariable( 'site_style' ) == 'boxed' )
-    echo '<div class="container boxed-container">';
+  <?php do_action( 'get_header' ); ?>
 
-  // the "get_header" hook
-  do_action( 'get_header' );
+  <?php if ( shoestrap_getVariable( 'navbar_toggle' ) == 1 ) : ?>
+    <?php do_action( 'shoestrap_pre_navbar' ); ?>
+    <?php if ( !has_action( 'shoestrap_header_top_navbar_override' ) ) : ?>
+      <?php get_template_part( 'templates/header-top-navbar' ); ?>
+    <?php else : ?>
+      <?php do_action( 'shoestrap_header_top_navbar_override' ); ?>
+    <?php endif; ?>
+  <?php else : ?>
+    <?php if ( !has_action( 'shoestrap_header_override' ) ) : ?>
+      <?php get_template_part( 'templates/header' ); ?>
+    <?php else : ?>
+      <?php do_action( 'shoestrap_header_override' ); ?>
+    <?php endif; ?>
+  <?php endif; ?>
 
-  // Use Bootstrap's navbar if enabled
-  if ( shoestrap_getVariable( 'navbar_toggle' ) == 1 ) :
-    // the "shoestrap_pre_navbar" hook
-    do_action( 'shoestrap_pre_navbar' );
+  <?php do_action( 'shoestrap_post_navbar' ); ?>
 
-    // Override the header-top-navbar template file if there is a "shoestrap_header_top_navbar_override" action
-    if ( !has_action( 'shoestrap_header_top_navbar_override' ) )
-      get_template_part( 'templates/header-top-navbar' );
-    else
-      do_action( 'shoestrap_header_top_navbar_override' );
+  <?php if ( shoestrap_getVariable( 'site_style' ) == 'boxed' ) : ?>
+    </div>
+  <?php endif; ?>
 
-  // If the navbar is disabled, load the menu header
-  else :
-    // Override the header template file if there is a "shoestrap_header_override" action
-    if ( !has_action( 'shoestrap_header_override' ) )
-      get_template_part( 'templates/header' );
-    else
-      do_action( 'shoestrap_header_override' );
-  endif;
+  <?php if ( has_action( 'shoestrap_below_top_navbar' ) ) : ?>
+    <div class="before-main-wrapper">
+      <?php do_action('shoestrap_below_top_navbar'); ?>
+    </div>
+  <?php endif; ?>
 
-  // The "shoestrap_post_navbar" action
-  do_action( 'shoestrap_post_navbar' );
+  <?php do_action('shoestrap_pre_wrap'); ?>
 
-  // If the user has selected "boxed" style, close the container div that we opened earlier.
-  if ( shoestrap_getVariable( 'site_style' ) == 'boxed' )
-    echo '</div>';
+  <?php if ( has_action( 'shoestrap_breadcrumbs' ) ) : ?>
+    <?php do_action('shoestrap_breadcrumbs'); ?>
+  <?php endif; ?>
 
-  // If there is a "shoestrap_below_top_navbar" action, add it here.
-  // This is also used to render the extra header.
-  if ( has_action( 'shoestrap_below_top_navbar' ) ) :
-    echo '<div class="before-main-wrapper">';
-    do_action('shoestrap_below_top_navbar');
-    echo '</div>';
-  endif;
+  <?php do_action('shoestrap_header_media'); ?>
 
-  // The "shoestrap_pre_wrap" hook.
-  // This is also used to render the hero area.
-  do_action('shoestrap_pre_wrap');
+  <div class="wrap main-section <?php echo shoestrap_container_class(); ?>" role="document">
 
-  if ( has_action( 'shoestrap_breadcrumbs' ) ) :
-    do_action('shoestrap_breadcrumbs');
-  endif;
+    <?php do_action('shoestrap_pre_content'); ?>
 
-  do_action('shoestrap_header_media');
+    <div class="content">
+      <div class="row bg">
 
-  // Open the main-content wrapper
-  echo '<div class="wrap main-section ' . shoestrap_container_class() . '" role="document">';
-    // The "shoestrap_pre_content" hook
-    do_action('shoestrap_pre_content');
-    // Adding a div with the "row" class so that bootstrap can properly handle
-    // the main content and sidebars width classes
-    echo '<div class="content"><div class="row bg">';
-      // The "shoestrap_pre_main" hook
-      do_action('shoestrap_pre_main');
+        <?php do_action('shoestrap_pre_main'); ?>
 
-      // If the layout requires an extra wrapping element, add it here.
-      if ( shoestrap_section_class( 'wrap' ) ) {
-        echo '<div class="mp_wrap ' . shoestrap_section_class( 'wrapper' ) . '"><div class="row">';
-      }
+        <?php if ( shoestrap_section_class( 'wrap' ) ) : ?>
+          <div class="mp_wrap <?php echo shoestrap_section_class( 'wrapper' ); ?>">
+            <div class="row">
+        <?php endif; ?>
 
-      // This is where we load the extra template files for the main content.
-      echo '<main class="main ' . shoestrap_section_class( 'main' ) . '" role="main">';
-        include roots_template_path();
-      echo '</main><!-- /.main -->';
+        <main class="main <?php echo shoestrap_section_class( 'main' ); ?>" role="main">
+          <?php include roots_template_path(); ?>
+        </main><!-- /.main -->
 
-      do_action('shoestrap_after_main');
+        <?php do_action('shoestrap_after_main'); ?>
 
-      // Add the PRIMARY sidebar when applicable.
-      if ( ( shoestrap_getLayout() != 0 && ( roots_display_sidebar() ) ) || ( is_front_page() && shoestrap_getVariable( 'layout_sidebar_on_front' ) == 1 ) ) :
-        if ( !is_front_page() || ( is_front_page() && shoestrap_getVariable( 'layout_sidebar_on_front' ) == 1 ) ) :
-          echo '<aside class="sidebar ' . shoestrap_section_class( 'primary' ) . '" role="complementary">';
-            if ( !has_action( 'shoestrap_sidebar_override' ) )
-              include roots_sidebar_path();
-            else
-              do_action( 'shoestrap_sidebar_override' );
-          echo '</aside><!-- /.sidebar -->';
-        endif;
-      endif;
+        <?php if ( ( shoestrap_getLayout() != 0 && ( roots_display_sidebar() ) ) || ( is_front_page() && shoestrap_getVariable( 'layout_sidebar_on_front' ) == 1 ) ) : ?>
+          <?php if ( !is_front_page() || ( is_front_page() && shoestrap_getVariable( 'layout_sidebar_on_front' ) == 1 ) ) : ?>
+            <aside class="sidebar <?php echo shoestrap_section_class( 'primary' ); ?>" role="complementary">
+              <?php if ( !has_action( 'shoestrap_sidebar_override' ) ) : ?>
+                <?php include roots_sidebar_path(); ?>
+              <?php else : ?>
+                <?php do_action( 'shoestrap_sidebar_override' ); ?>
+              <?php endif; ?>
+            </aside><!-- /.sidebar -->
+          <?php endif; ?>
+        <?php endif; ?>
 
-      // If the layout requires an extra wrapping element and we added it before, close it here.
-      if ( shoestrap_section_class( 'wrap' ) )
-        echo '</div></div>';
+        <?php if ( shoestrap_section_class( 'wrap' ) ) : ?>
+            </div>
+          </div>
+        <?php endif; ?>
 
-      // Add the SECONDARY sidebar when applicable.
-      if ( shoestrap_getLayout() >= 3 && is_active_sidebar( 'sidebar-secondary' ) ) :
-        if ( !is_front_page() || ( is_front_page() && shoestrap_getVariable( 'layout_sidebar_on_front' ) == 1 ) ) :
-          echo '<aside class="sidebar secondary ' . shoestrap_section_class( 'secondary' ) . '" role="complementary">';
-            dynamic_sidebar('sidebar-secondary');
-          echo '</aside><!-- /.sidebar -->';
-        endif;
-      endif;
+        <?php if ( shoestrap_getLayout() >= 3 && is_active_sidebar( 'sidebar-secondary' ) ) : ?>
+          <?php if ( !is_front_page() || ( is_front_page() && shoestrap_getVariable( 'layout_sidebar_on_front' ) == 1 ) ) : ?>
+            <aside class="sidebar secondary <?php echo shoestrap_section_class( 'secondary' ); ?>" role="complementary">
+              <?php dynamic_sidebar( 'sidebar-secondary' ); ?>
+            </aside><!-- /.sidebar -->
+          <?php endif; ?>
+        <?php endif; ?>
+      </div>
+    </div><!-- /.content -->';
+    <?php do_action('shoestrap_after_content'); ?>
+  </div><!-- /.wrap -->
+  <?php do_action('shoestrap_after_wrap'); ?>
 
-    // Close the content div
-    echo '</div></div><!-- /.content -->';
-    // The "shoestrap_after_content" hook
-    do_action('shoestrap_after_content');
-  // Close the wrapper div
-  echo '</div><!-- /.wrap -->';
-  // The "shoestrap_after_wrap" hook
-  do_action('shoestrap_after_wrap');
+  <?php if ( shoestrap_getVariable( 'site_style' ) == 'boxed' ) : ?>
+    <div class="container boxed-container">
+  <?php endif; ?>
 
-  // If the user has selected "boxed" style, add a container div to contain the footer.
-  if ( shoestrap_getVariable( 'site_style' ) == 'boxed' )
-    echo '<div class="container boxed-container">';
+  <?php do_action('shoestrap_pre_footer'); ?>
+  <?php if ( !has_action( 'shoestrap_footer_override' ) ) : ?>
+    <?php get_template_part('templates/footer'); ?>
+  <?php else : ?>
+    <?php do_action( 'shoestrap_footer_override' ); ?>
+  <?php endif; ?>
 
-  // The "shoestrap_pre_footer" hook
-  do_action('shoestrap_pre_footer');
+  <?php do_action('shoestrap_after_footer'); ?>
 
-  // Add the footer OR override the footer template if there is a "shoestrap_footer_override" action.
-  if ( !has_action( 'shoestrap_footer_override' ) )
-    get_template_part('templates/footer');
-  else
-    do_action( 'shoestrap_footer_override' );
+  <?php if ( shoestrap_getVariable( 'site_style' ) == 'boxed' ) : ?>
+    </div>
+  <?php endif; ?>
 
-  // The "shoestrap_after_footer" hook
-  do_action('shoestrap_after_footer');
-
-  // If the user has selected "boxed" style, close the container div that we opened earlier to contain the footer.
-  if ( shoestrap_getVariable( 'site_style' ) == 'boxed' )
-    echo '</div>';
-
-  // wp_footer required by WordPress.
-  wp_footer();
-  ?>
+  <?php wp_footer(); ?>
 
 </body>
 </html>
