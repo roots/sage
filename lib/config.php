8bcd2fb4e381f38d89e50d459182adb4c022ccc0
Tobias Møller Kjærsgaard <tobias.kjr@gmail.com>
2013-12-21 14:18:15 +0100

    Override variables from Bootstrap

--- lib/config.php
+++ lib/config.php
@@ -7,8 +7,9 @@ add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's top navbar
 add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails component on [gallery]
 add_theme_support('nice-search');           // Enable /?s= to /search/ redirect
 //add_theme_support('jquery-cdn');            // Enable to load jQuery from the Google CDN
-//add_theme_support( 'includes_bp_styles' );  // Dequeue loading of Buddypress related CSS-files
-//add_theme_support( 'includes_bp_script' );  // Dequeue loading of Buddypress related JS-files
+//add_theme_support( 'buddypress' );          // Tell BuddyPress not to load the theme compat layer
+//add_theme_support( 'override_bp_styles' );  // Dequeue loading of Buddypress related CSS-files
+//add_theme_support( 'override_bp_scripts' );  // Dequeue loading of Buddypress related JS-files
 
 /**
  * Configuration values
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '54a37b509e25732dbcac8dbe57e47083');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '658eb6a8e8ed141a59607408b046b6f7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
