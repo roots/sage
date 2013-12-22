581a82c8bb32f9232fd3cc9d333933010dbccd98
Tobias Møller Kjærsgaard <tobias.kjr@gmail.com>
2013-12-22 23:46:39 +0100

    Minor changes to Buddypress styles and post-form.php

--- buddypress/activity/post-form.php
+++ buddypress/activity/post-form.php
@@ -7,68 +7,89 @@
  * @subpackage bp-legacy
  */
 
+global $bp;
+
 ?>
 
 <form action="<?php bp_activity_post_form_action(); ?>" method="post" id="whats-new-form" name="whats-new-form" role="complementary">
 
 	<?php do_action( 'bp_before_activity_post_form' ); ?>
 
+	<?php if ( bp_is_group() ) {
+		$whats_new_text = sprintf( __( "What's new in %s, %s?", 'buddypress' ), bp_get_group_name(), bp_get_user_firstname() );
+	} else {
+		$whats_new_text = sprintf( __( "What's new, %s?", 'buddypress' ), bp_get_user_firstname() );
+	} ?>
+
 	<div id="whats-new-avatar">
 		<a href="<?php echo bp_loggedin_user_domain(); ?>">
 			<?php bp_loggedin_user_avatar( 'width=' . bp_core_avatar_thumb_width() . '&height=' . bp_core_avatar_thumb_height() ); ?>
 		</a>
 	</div>
-	
-	<p class="activity-greeting"><?php if ( bp_is_group() )
-		printf( __( "What's new in %s, %s?", 'buddypress' ), bp_get_group_name(), bp_get_user_firstname() );
-	else
-		printf( __( "What's new, %s?", 'buddypress' ), bp_get_user_firstname() );
-	?></p>
 
 	<div id="whats-new-content">
-		<div id="whats-new-textarea">
-			<textarea name="whats-new" id="whats-new" cols="50" rows="10"><?php if ( isset( $_GET['r'] ) ) : ?>@<?php echo esc_attr( $_GET['r'] ); ?> <?php endif; ?></textarea>
+
+		<div class="whats-new-row">
+			<div id="whats-new-textarea">
+				<div class="form-group">
+					<label for="whats-new"><?php print $whats_new_text ?></label>
+					<textarea name="whats-new" id="whats-new" cols="50" rows="10" placeholder="Use @username to mention a user…"><?php if ( isset( $_GET['r'] ) ) : ?>@<?php echo esc_attr( $_GET['r'] ); ?> <?php endif; ?></textarea>
+				</div>
+			</div>
 		</div>
 
-		<div id="whats-new-options">
+		<div class="whats-new-row" id="whats-new-options">
+
+			<?php if ( bp_is_active( 'groups' ) ) {
+				echo '<input type="hidden" id="whats-new-post-object" name="whats-new-post-object" value="groups" />';
+
+				$group_ids = groups_get_user_groups( get_current_user_id() );
+				$select_disabled = "";
+				$option_arr = array();				
+				
+				foreach ( $group_ids['groups'] as $group_id ) {
+					$group = groups_get_group( array( 'group_id' => $group_id ) );
+					$selected = ( bp_is_group_home() && $group->id == $bp->groups->current_group->id ) ? "selected" : "";
+					$option_arr[] = sprintf('<option value="%d" %s>%s</option>', $group->id, $selected, $group->name);
+				}
+
+				if ( bp_is_group_home() ) {
+					$select_disabled = "disabled";
+				}
+
+			} else {
+
+				$select_disabled = "disabled";
+				$option_arr = array();
+
+			} ?>
+
+			<div id="whats-new-post-in-box">
+
+				<div class="form-group">
+					<label for="whats-new-post-in"><?php _e('Post in') ?>:</label>
+					<select id="whats-new-post-in" name="whats-new-post-in" <?php echo $select_disabled ?>>
+						<option <?php echo ( bp_is_my_profile() ) ? "selected" : "" ?> value="0"><?php _e( 'My Profile', 'buddypress' ); ?></option>
+						<?php foreach ($option_arr as $option) { echo $option; } ?>
+					</select>
+
+				</div>
+
+			</div>
+
+
+
 			<div id="whats-new-submit">
 				<input type="submit" name="aw-whats-new-submit" id="aw-whats-new-submit" value="<?php _e( 'Post Update', 'buddypress' ); ?>" />
 			</div>
-
-			<?php if ( bp_is_active( 'groups' ) && !bp_is_my_profile() && !bp_is_group() ) : ?>
-
-				<div id="whats-new-post-in-box">
-
-					<?php _e( 'Post in', 'buddypress' ); ?>:
-
-					<select id="whats-new-post-in" name="whats-new-post-in">
-						<option selected="selected" value="0"><?php _e( 'My Profile', 'buddypress' ); ?></option>
-
-						<?php if ( bp_has_groups( 'user_id=' . bp_loggedin_user_id() . '&type=alphabetical&max=100&per_page=100&populate_extras=0' ) ) :
-							while ( bp_groups() ) : bp_the_group(); ?>
-
-								<option value="<?php bp_group_id(); ?>"><?php bp_group_name(); ?></option>
-
-							<?php endwhile;
-						endif; ?>
-
-					</select>
-				</div>
-				<input type="hidden" id="whats-new-post-object" name="whats-new-post-object" value="groups" />
-
-			<?php elseif ( bp_is_group_home() ) : ?>
-
-				<input type="hidden" id="whats-new-post-object" name="whats-new-post-object" value="groups" />
-				<input type="hidden" id="whats-new-post-in" name="whats-new-post-in" value="<?php bp_group_id(); ?>" />
-
-			<?php endif; ?>
-
+	
 			<?php do_action( 'bp_activity_post_form_options' ); ?>
-
+	
 		</div><!-- #whats-new-options -->
+
 	</div><!-- #whats-new-content -->
 
-	<?php wp_nonce_field( 'post_update', '_wpnonce_post_update' ); ?>
-	<?php do_action( 'bp_after_activity_post_form' ); ?>
+<?php wp_nonce_field( 'post_update', '_wpnonce_post_update' ); ?>
+<?php do_action( 'bp_after_activity_post_form' ); ?>
 
 </form><!-- #whats-new-form -->
--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,28 @@
 /**
  * Custom functions
  */
+
+/**
+ * Dump the global variable wp_filter to debug action and filter queues
+ * 
+ * @return void
+ */
+function dump_wp_filter() {
+	global $wp_filter;
+	global $wp_styles;
+
+		print "<pre>";
+		print_r ($wp_styles);
+		print "</pre>";	
+}
+add_shortcode( 'filter_dump', 'dump_wp_filter' );
+
+function override_buddypress_styles() {
+	if (current_theme_supports( 'override_bp_styles' )) {
+		wp_dequeue_style( 'bp-parent-css' );
+		wp_deregister_style( 'bp-parent-css' );
+		wp_dequeue_style( 'bp-legacy-css' );
+		wp_deregister_style( 'bp-legacy-css' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'override_buddypress_styles', 12 );
