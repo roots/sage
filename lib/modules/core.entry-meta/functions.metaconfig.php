32e21ae6d8bcb63a7fe8fcdc5b4d0949d686cff1
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-27 21:46:31 +0200

    Syntax improvements - entry-meta module.

--- lib/modules/core.entry-meta/functions.metaconfig.php
+++ lib/modules/core.entry-meta/functions.metaconfig.php
@@ -17,18 +17,17 @@ function shoestrap_meta_custom_render() {
   $metaconfig = shoestrap_getVariable( 'shoestrap_entry_meta_config' );
 
   // early return if no need to proceed
-  if ( !is_array( $metaconfig ) || !isset( $metaconfig['enabled'] ) || empty( $metaconfig ) ) :
+  if ( !is_array( $metaconfig ) || !isset( $metaconfig['enabled'] ) || empty( $metaconfig ) )
     return;
-  else :
-    $metaelements = $metaconfig['enabled'];
-    // remove the weird placebo element if it exists
-    unset( $metaelements['placebo'] );
-  endif;
+
+
+  $metaelements = $metaconfig['enabled'];
+  // remove the weird placebo element if it exists
+  unset( $metaelements['placebo'] );
 
   // check if anything is left after we remove the weird placebo element
-  if ( empty( $metaelements ) ) :
+  if ( empty( $metaelements ) )
     return;
-  endif;
 
   $meta_html = $metaelements;
 
@@ -44,72 +43,66 @@ function shoestrap_meta_custom_render() {
 
   // Distribute meta elements over 12 columns
   $elementscountplus = strlen( $elementscountplus );
-  if ( $elementscountplus >= 2 ) :
-    $col = round( ( 12 / ( $elementscountplus ) ), 0);
-  else :
-    $col = 12;
-  endif;
+
+  $col = ( $elementscountplus >= 2 ) ? round( ( 12 / ( $elementscountplus ) ), 0) : 12;
 
   $colclass = 'col-md-' . $col;
 
   // output sticky element
-  if ( isset( $metaelements['sticky'] ) ) :
-    if ( is_sticky() ) :
+  if ( isset( $metaelements['sticky'] ) ) {
+    if ( is_sticky() )
       $meta_html['sticky'] = '<span class="featured-post ' . $colclass . '"><i class="el-icon-flag icon"></i> ' . __( 'Sticky', 'shoestrap' ) . '</span>';
-    else :
+    else
       unset( $meta_html['sticky'] );
-    endif;
-  endif;
+  }
 
   // output date element
-  if ( !has_post_format( 'link' ) ) :
+  if ( !has_post_format( 'link' ) ) {
     $format_prefix = ( has_post_format( 'chat' ) || has_post_format( 'status' ) ) ? _x( '%1$s on %2$s', '1: post format name. 2: date', 'shoestrap' ): '%2$s';
 
-    if ( isset( $metaelements['date'] ) ) :
+    if ( isset( $metaelements['date'] ) ) {
       $meta_html['date'] = sprintf( '<span class="date ' . $colclass . '"><i class="el-icon-time icon"></i> <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a></span>',
         esc_url( get_permalink() ),
         esc_attr( sprintf( __( 'Permalink to %s', 'shoestrap' ), the_title_attribute( 'echo=0' ) ) ),
         esc_attr( get_the_date( 'c' ) ),
         esc_html( sprintf( $format_prefix, get_post_format_string( get_post_format() ), get_the_date() ) )
       );
-    endif;
-  endif;
+    }
+  }
 
   // output category element
-  if ( isset( $metaelements['category'] ) ) :
-    if ( $categories_list ) :
+  if ( isset( $metaelements['category'] ) ) {
+    if ( $categories_list )
       $meta_html['category'] = '<span class="categories-links ' . $colclass . '"><i class="el-icon-folder-open icon"></i> ' . $categories_list . '</span>';
-    else :
-      // unset category element if set but not being used
+    else
       unset( $meta_html['category'] );
-    endif;
-  endif;
+  }
 
   // output tag element
-  if ( isset( $metaelements['tags'] ) ) :
-    if ( $tag_list ) :
+  if ( isset( $metaelements['tags'] ) ) {
+    if ( $tag_list )
       $meta_html['tags'] = '<span class="tags-links ' . $colclass . '"><i class="el-icon-tags icon"></i> ' . $tag_list . '</span>';
-    else :
-      // unset tag element if set but not being used
+    else
       unset( $meta_html['tags'] );
-    endif;
-  endif;
+  }
 
   // output author element
-  if ( isset( $metaelements['author'] ) ) :
+  if ( isset( $metaelements['author'] ) ) {
     $meta_html['author'] = sprintf( '<span class="author vcard ' . $colclass . '"><i class="el-icon-user icon"></i> <a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
       esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
       esc_attr( sprintf( __( 'View all posts by %s', 'shoestrap' ), get_the_author() ) ),
       get_the_author()
     );
-  endif;
+  }
 
-  if ( !empty( $metaelements ) ) :
+  if ( !empty( $metaelements ) ) {
     echo '<div class="row row-meta">';
-    foreach ( $meta_html as $el => $html ) :
+
+    foreach ( $meta_html as $el => $html ) {
       echo $html;
-    endforeach;
+    }
+
     echo '</div>';
-  endif;
+  }
 }
 endif;
