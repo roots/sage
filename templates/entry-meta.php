18fcc6e21c9f4633aa04803f75d3f2fe1336ceca
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-28 10:11:25 +0200

    pulling roots files

---
+++ templates/entry-meta.php
@@ -0,0 +1,2 @@
+<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+<p class="byline author vcard"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
