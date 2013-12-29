c1815c9066aeecb626b8faabf840348c0dd8dfe3
Ben Word <ben@benword.com>
2013-12-29 14:13:23 -0600

    screen_icon has been deprecated with WP 3.8

--- lib/activation.php
+++ lib/activation.php
@@ -49,7 +49,6 @@ function roots_get_theme_activation_options() {
 
 function roots_theme_activation_options_render_page() { ?>
   <div class="wrap">
-    <?php screen_icon(); ?>
     <h2><?php printf(__('%s Theme Activation', 'roots'), wp_get_theme()); ?></h2>
     <?php settings_errors(); ?>
 
