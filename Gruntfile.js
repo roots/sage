8daed35764e125b045e3316ddfd8171c27d4b364
fovoc <fovoc.otinanai@gmail.com>
2013-12-30 20:00:21 +0200

    Done, without errors.
    now we can work more on this...

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
     version: {
       options: {
         file: 'lib/scripts.php',
-        css: 'assets/css/main.min.css',
+        css: 'assets/css/style.css',
         cssHandle: 'roots_main',
         js: 'assets/js/scripts.min.js',
         jsHandle: 'roots_scripts'
