c1331a8de566cf13452a275754d36c899f78b569
Kalen Johnson <info@kalenjohnson.com>
2013-12-31 11:02:42 -0800

    Updated with .bowerrc to install bower components to assets/components directory. Updated bootstrap.less and Gruntfile to point to new location

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,18 +33,18 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'assets/js/scripts.min.js': [
-            'bower_components/bootstrap/js/transition.js',
-						'bower_components/bootstrap/js/alert.js',
-						'bower_components/bootstrap/js/button.js',
-						'bower_components/bootstrap/js/carousel.js',
-						'bower_components/bootstrap/js/collapse.js',
-						'bower_components/bootstrap/js/dropdown.js',
-						'bower_components/bootstrap/js/modal.js',
-						'bower_components/bootstrap/js/tooltip.js',
-						'bower_components/bootstrap/js/popover.js',
-						'bower_components/bootstrap/js/scrollspy.js',
-						'bower_components/bootstrap/js/tab.js',
-						'bower_components/bootstrap/js/affix.js',
+            'assets/components/bootstrap/js/transition.js',
+						'assets/components/bootstrap/js/alert.js',
+						'assets/components/bootstrap/js/button.js',
+						'assets/components/bootstrap/js/carousel.js',
+						'assets/components/bootstrap/js/collapse.js',
+						'assets/components/bootstrap/js/dropdown.js',
+						'assets/components/bootstrap/js/modal.js',
+						'assets/components/bootstrap/js/tooltip.js',
+						'assets/components/bootstrap/js/popover.js',
+						'assets/components/bootstrap/js/scrollspy.js',
+						'assets/components/bootstrap/js/tab.js',
+						'assets/components/bootstrap/js/affix.js',
             'assets/js/plugins/*.js',
             'assets/js/_*.js'
           ]
