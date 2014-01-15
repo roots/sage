56cc1863f278a4415f9f53b785cb3ff17091e56c
Ben Word <ben@benword.com>
2014-01-15 15:20:00 -0600

    Use get_feed_link for RSS URL

--- templates/head.php
+++ templates/head.php
@@ -8,5 +8,5 @@
 
   <?php wp_head(); ?>
 
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
