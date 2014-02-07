d8d528c282decf58482ef87a6294311f22bbe895
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-07 21:05:48 +0200

    simplified the titles function

--- lib/titles.php
+++ lib/titles.php
@@ -12,22 +12,21 @@ function shoestrap_title() {
 	} elseif ( is_archive() ) {
 		$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 
-		if ( $term ) {
+		if ( $term )
 			return apply_filters( 'single_term_title', $term->name );
-		} elseif ( is_post_type_archive() ) {
+		elseif ( is_post_type_archive() )
 			return apply_filters( 'the_title', get_queried_object()->labels->name );
-		} elseif ( is_day() ) {
+		elseif ( is_day() )
 			return sprintf(__( 'Daily Archives: %s', 'shoestrap' ), get_the_date() );
-		} elseif ( is_month() ) {
+		elseif ( is_month() )
 			return sprintf(__( 'Monthly Archives: %s', 'shoestrap' ), get_the_date( 'F Y' ) );
-		} elseif ( is_year() ) {
+		elseif ( is_year() )
 			return sprintf(__( 'Yearly Archives: %s', 'shoestrap' ), get_the_date( 'Y' ) );
-		} elseif ( is_author() ) {
-			$author = get_queried_object();
-			return sprintf( __( 'Author Archives: %s', 'shoestrap' ), $author->display_name );
-		} else {
+		elseif ( is_author() )
+			return sprintf( __( 'Author Archives: %s', 'shoestrap' ), get_queried_object()->display_name );
+		else
 			return single_cat_title( '', false );
-		}
+
 	} elseif ( is_search() ) {
 		return sprintf( __( 'Search Results for %s', 'shoestrap' ), get_search_query() );
 	} elseif ( is_404() ) {
