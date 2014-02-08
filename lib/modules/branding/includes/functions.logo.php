2d30f740001ba4c23e3abf6be22cfc3e1ac549d5
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-08 12:37:12 +0200

    minimalizing logo functions

--- lib/modules/branding/includes/functions.logo.php
+++ lib/modules/branding/includes/functions.logo.php
@@ -9,23 +9,10 @@ function shoestrap_logo() {
 	$logo  = shoestrap_getVariable( 'logo' );
 
 	if ( !empty( $logo['url'] ) )
-		echo '<img id="site-logo" src="' . $logo['url'] . '" alt="' . get_bloginfo( 'name' ) . '">';
+		$branding = '<img id="site-logo" src="' . $logo['url'] . '" alt="' . get_bloginfo( 'name' ) . '">';
 	else
-		echo '<span class="sitename">' . bloginfo( 'name' ) . '</span>';
-}
-endif;
+		$branding = '<span class="sitename">' . get_bloginfo( 'name' ) . '</span>';
 
-if ( !function_exists( 'shoestrap_branding_class' ) ) :
-function shoestrap_branding_class( $echo = true ) {
-	$logo  = shoestrap_getVariable( 'logo' );
-
-	// apply the proper class
-	$class = ( !empty( $logo['url'] ) ) ? 'logo' : 'text';
-
-	// echo or return the value
-	if ( $echo )
-		echo $class;
-	else
-		return $class;
+	return $branding;
 }
 endif;
