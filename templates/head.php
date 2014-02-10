89312c7dbc608ccbe5dc3caac34e00f82cd51286
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-10 16:22:37 +0200

    convert identation to tabs on templates

--- templates/head.php
+++ templates/head.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
-  <?php wp_head(); ?>
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php wp_title('|', true, 'right'); ?></title>
+	<?php wp_head(); ?>
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
-  	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/html5shiv.js"></script>
-  	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/respond.min.js"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/html5shiv.js"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/respond.min.js"></script>
 	<![endif]-->
 
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+	<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
--- templates/searchform.php
+++ templates/searchform.php
@@ -2,12 +2,12 @@
 if ( !has_action( 'shoestrap_searchform_override' ) ) : ?>
 
 <form role="search" method="get" id="searchform" class="form-inline" action="<?php echo home_url('/'); ?>">
-  <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="form-control" placeholder="<?php _e('Search', 'shoestrap'); ?> <?php bloginfo('name'); ?>">
-  <label class="hide" for="s"><?php _e('Search for:', 'shoestrap'); ?></label>
-  <button type="submit" id="searchsubmit" class="btn btn-default"><i class="el-icon-search"></i></button>
+	<input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="form-control" placeholder="<?php _e('Search', 'shoestrap'); ?> <?php bloginfo('name'); ?>">
+	<label class="hide" for="s"><?php _e('Search for:', 'shoestrap'); ?></label>
+	<button type="submit" id="searchsubmit" class="btn btn-default"><i class="el-icon-search"></i></button>
 </form>
 
 <?php
 else:
-  do_action( 'shoestrap_searchform_override' );
+	do_action( 'shoestrap_searchform_override' );
 endif;
