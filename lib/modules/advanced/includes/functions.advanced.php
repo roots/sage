58859d4df9dfce355d58d0070988545b9a6222b8
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-13 19:55:47 +0200

    removed the pjax control and functionality. Will be built as a plugin.

--- lib/modules/advanced/includes/functions.advanced.php
+++ lib/modules/advanced/includes/functions.advanced.php
@@ -41,40 +41,6 @@ else
 endif;
 add_filter( 'show_admin_bar' , 'shoestrap_admin_bar' );
 
-// PJAX
-if ( shoestrap_getVariable( 'pjax' ) == 1 ) {
-	add_action( 'shoestrap_pre_wrap', 'shoestrap_pjax_open_container' );
-	add_action( 'shoestrap_pre_footer', 'shoestrap_pjax_close_container' );
-	add_action( 'wp_footer', 'shoestrap_pjax_trigger_script', 200 );
-}
-
-
-if ( !function_exists( 'shoestrap_pjax_open_container' ) ) :
-function shoestrap_pjax_open_container() { ?>
-	<div id="pjax-container">
-	<?php
-}
-endif;
-	
-
-if ( !function_exists( 'shoestrap_pjax_close_container' ) ) :
-function shoestrap_pjax_close_container() { ?>
-	</div>
-	<?php
-}
-endif;
-
-
-if ( !function_exists( 'shoestrap_pjax_trigger_script' ) ) :
-function shoestrap_pjax_trigger_script() { ?>
-	<script>
-	$(document).on('pjax:send', function() { $('.main').fadeToggle("fast", "linear") })
-	$(document).pjax('nav a, aside a, .breadcrumb a', '#pjax-container')
-	</script>
-	<?php
-}
-endif;
-
 
 if ( !function_exists( 'shoestrap_google_analytics' ) ) :
 /**
@@ -95,6 +61,7 @@ function shoestrap_google_analytics() {
 }
 endif;
 add_action( 'wp_footer', 'shoestrap_google_analytics', 20 );
+
 
 /**
  * Post Excerpt Length
--- lib/modules/advanced/module.php
+++ lib/modules/advanced/module.php
@@ -62,14 +62,6 @@ function shoestrap_module_advanced_options( $sections ) {
 	);
 
 	$fields[] = array( 
-		'title'     => __( 'PJAX', 'shoestrap' ),
-		'desc'      => __( 'Use <a href="https://github.com/defunkt/jquery-pjax" target="_blank">PJAX</a> in link tags inside NavBars, Sibebars & Breadcrumb. This cause a fast linear fadeToggle effect in main page. Default: OFF', 'shoestrap' ),
-		'id'        => 'pjax',
-		'default'   => 0,
-		'type'      => 'switch',
-	);
-
-	$fields[] = array( 
 		'title'     => __( 'Root Relative URLs', 'shoestrap' ),
 		'desc'      => __( 'Return URLs such as <em>/assets/css/style.css</em> instead of <em>http://example.com/assets/css/style.css</em>. Default: ON', 'shoestrap' ),
 		'id'        => 'root_relative_urls',
