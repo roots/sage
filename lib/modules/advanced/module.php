56f8ac901f9a0b92fea1a868277f14f8ea539123
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-14 11:08:47 +0200

    Advanced module is now a class

--- lib/modules/advanced/module.php
+++ lib/modules/advanced/module.php
@@ -1,160 +1,392 @@
 <?php
 
-if ( !function_exists( 'shoestrap_module_advanced_options' ) ) :
-/*
- * The advanced core options for the Shoestrap theme
- */
-function shoestrap_module_advanced_options( $sections ) {
-	// Advanced Settings
-	$section = array( 
-		'title'   => __( 'Advanced', 'shoestrap' ),
-		'icon'    => 'el-icon-cogs icon-large'
-	);  
 
-	$fields[] = array( 
-		'title'     => __( 'Enable Retina mode', 'shoestrap' ),
-		'desc'      => __( 'By enabling your site\'s featured images will be retina ready. Requires images to be uploaded at 2x the typical size desired. Default: On', 'shoestrap' ),
-		'id'        => 'retina_toggle',
-		'default'   => 1,
-		'type'      => 'switch',
-		'customizer'=> array(),
-	);
+if ( !class_exists( 'ShoestrapAdvanced' ) ) {
 
-	$fields[] = array( 
-		'title'     => __( 'Google Analytics ID', 'shoestrap' ),
-		'desc'      => __( 'Paste your Google Analytics ID here to enable analytics tracking. Only Universal Analytics properties. Your user ID should be in the form of UA-XXXXX-Y.', 'shoestrap' ),
-		'id'        => 'analytics_id',
-		'default'   => '',
-		'type'      => 'text',
-	);
+	/**
+	* The "Advanced" module
+	*/
+	class ShoestrapAdvanced {
 
-	$fields[] = array( 
-		'title'     => 'Border-Radius and Padding Base',
-		'id'        => 'help2',
-		'desc'      => __( 'The following settings affect various areas of your site, most notably buttons.', 'shoestrap' ),
-		'type'      => 'info',
-	);
+		function __construct() {
+			add_filter( 'redux/options/' . SHOESTRAP_OPT_NAME . '/sections', array( $this, 'options' ), 95 );
+			add_action( 'wp_enqueue_scripts', array( $this, 'user_css' ), 101 );
+			add_action( 'wp_footer', array( $this, 'user_js' ), 200 );
+			add_filter( 'show_admin_bar' , array( $this, 'admin_bar' ) );
+			add_action( 'wp_footer', array( $this, 'google_analytics' ), 20 );
+			add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ), 100 );
+			add_action( 'after_setup_theme', array( $this, 'jquery_cdn_toggler' ) );
+			add_filter( 'shoestrap_compiler', array( $this, 'variables_filter' ) );
 
-	$fields[] = array( 
-		'title'     => __( 'Border-Radius', 'shoestrap' ),
-		'desc'      => __( 'You can adjust the corner-radius of all elements in your site here. This will affect buttons, navbars, widgets and many more. Default: 4', 'shoestrap' ),
-		'id'        => 'general_border_radius',
-		'default'   => 4,
-		'min'       => 0,
-		'step'      => 1,
-		'max'       => 50,
-		'advanced'  => true,
-		'compiler'  => true,
-		'type'      => 'slider',
-	);
+			if ( shoestrap_getVariable( 'nice_search' ) == 1 )
+				add_action( 'template_redirect', array( $this, 'nice_search_redirect' ) );
 
-	$fields[] = array( 
-		'title'     => __( 'Padding Base', 'shoestrap' ),
-		'desc'      => __( 'You can adjust the padding base. This affects buttons size and lots of other cool stuff too! Default: 8', 'shoestrap' ),
-		'id'        => 'padding_base',
-		'default'   => 6,
-		'min'       => 0,
-		'step'      => 1,
-		'max'       => 20,
-		'advanced'  => true,
-		'compiler'  => true,
-		'type'      => 'slider',
-	);
+			/**
+			 * Post Excerpt Length
+			 * Length in words for excerpt_length filter (http://codex.wordpress.org/Plugin_API/Filter_Reference/excerpt_length)
+			 */
+			define( 'POST_EXCERPT_LENGTH', shoestrap_getVariable( 'post_excerpt_length' ) );
 
-	$fields[] = array( 
-		'title'     => __( 'Root Relative URLs', 'shoestrap' ),
-		'desc'      => __( 'Return URLs such as <em>/assets/css/style.css</em> instead of <em>http://example.com/assets/css/style.css</em>. Default: ON', 'shoestrap' ),
-		'id'        => 'root_relative_urls',
-		'default'   => 0,
-		'type'      => 'switch'
-	);
+			if ( self::enable_root_relative_urls() ) {
+				$root_rel_filters = array(
+					'bloginfo_url',
+					'the_permalink',
+					'wp_list_pages',
+					'wp_list_categories',
+					'shoestrap_wp_nav_menu_item',
+					'the_content_more_link',
+					'the_tags',
+					'get_pagenum_link',
+					'get_comment_link',
+					'month_link',
+					'day_link',
+					'year_link',
+					'tag_link',
+					'the_author_posts_link',
+					'script_loader_src',
+					'style_loader_src'
+				);
 
-	$fields[] = array( 
-		'title'     => __( 'Enable Nice Search', 'shoestrap' ),
-		'desc'      => __( 'Redirects /?s=query to /search/query/, convert %20 to +. Default: ON', 'shoestrap' ),
-		'id'        => 'nice_search',
-		'default'   => 1,
-		'type'      => 'switch'
-	);
+				add_filters( $root_rel_filters, array( $this, 'root_relative_url' ) );
+			}
+		}
 
-	$fields[] = array( 
-		'title'     => __( 'Custom CSS', 'shoestrap' ),
-		'desc'      => __( 'You can write your custom CSS here. This code will appear in a script tag appended in the header section of the page.', 'shoestrap' ),
-		'id'        => 'user_css',
-		'default'   => '',
-		'type'      => 'ace_editor',
-		'mode'      => 'css',
-		'theme'     => 'monokai',
-	);
+		/*
+		 * The advanced core options for the Shoestrap theme
+		 */
+		function options( $sections ) {
+			// Advanced Settings
+			$section = array( 
+				'title'   => __( 'Advanced', 'shoestrap' ),
+				'icon'    => 'el-icon-cogs icon-large'
+			);  
 
-	$fields[] = array( 
-		'title'     => __( 'Custom LESS', 'shoestrap' ),
-		'desc'      => __( 'You can write your custom LESS here. This code will be compiled with the other LESS files of the theme and be appended to the header.', 'shoestrap' ),
-		'id'        => 'user_less',
-		'default'   => '',
-		'type'      => 'ace_editor',
-		'mode'      => 'less',
-		'theme'     => 'monokai',
-		'compiler'  => true,
-	);  
+			$fields[] = array( 
+				'title'     => __( 'Enable Retina mode', 'shoestrap' ),
+				'desc'      => __( 'By enabling your site\'s featured images will be retina ready. Requires images to be uploaded at 2x the typical size desired. Default: On', 'shoestrap' ),
+				'id'        => 'retina_toggle',
+				'default'   => 1,
+				'type'      => 'switch',
+				'customizer'=> array(),
+			);
 
-	$fields[] = array( 
-		'title'     => __( 'Custom JS', 'shoestrap' ),
-		'desc'      => __( 'You can write your custom JavaScript/jQuery here. The code will be included in a script tag appended to the bottom of the page.', 'shoestrap' ),
-		'id'        => 'user_js',
-		'default'   => '',
-		'type'      => 'ace_editor',
-		'mode'      => 'javascript',
-		'theme'     => 'monokai',
-	);
+			$fields[] = array( 
+				'title'     => __( 'Google Analytics ID', 'shoestrap' ),
+				'desc'      => __( 'Paste your Google Analytics ID here to enable analytics tracking. Only Universal Analytics properties. Your user ID should be in the form of UA-XXXXX-Y.', 'shoestrap' ),
+				'id'        => 'analytics_id',
+				'default'   => '',
+				'type'      => 'text',
+			);
 
-	$fields[] = array( 
-		'title'     => __( 'Minimize CSS', 'shoestrap' ),
-		'desc'      => __( 'Minimize the genearated CSS. This should be ON for production sites. Default: OFF.', 'shoestrap' ),
-		'id'        => 'minimize_css',
-		'default'   => 1,
-		'compiler'  => true,
-		'customizer'=> array(),
-		'type'      => 'switch',
-	);
+			$fields[] = array( 
+				'title'     => 'Border-Radius and Padding Base',
+				'id'        => 'help2',
+				'desc'      => __( 'The following settings affect various areas of your site, most notably buttons.', 'shoestrap' ),
+				'type'      => 'info',
+			);
 
-	$fields[] = array(
-		'title'     => __( 'Toggle adminbar On/Off', 'shoestrap' ),
-		'desc'      => __( 'Turn the admin bar On or Off on the frontend. Default: Off.', 'shoestrap' ),
-		'id'        => 'advanced_wordpress_disable_admin_bar_toggle',
-		'default'   => 0,
-		'customizer'=> array(),
-		'type'      => 'switch',
-	);
+			$fields[] = array( 
+				'title'     => __( 'Border-Radius', 'shoestrap' ),
+				'desc'      => __( 'You can adjust the corner-radius of all elements in your site here. This will affect buttons, navbars, widgets and many more. Default: 4', 'shoestrap' ),
+				'id'        => 'general_border_radius',
+				'default'   => 4,
+				'min'       => 0,
+				'step'      => 1,
+				'max'       => 50,
+				'advanced'  => true,
+				'compiler'  => true,
+				'type'      => 'slider',
+			);
 
-	$fields[] = array(
-		'title'     => __( 'Use Google CDN for jQuery', 'shoestrap' ),
-		'desc'      => '',
-		'id'        => 'shoestrap_jquery_cdn',
-		'default'   => 0,
-		'customizer'=> array(),
-		'type'      => 'switch',
-	);
+			$fields[] = array( 
+				'title'     => __( 'Padding Base', 'shoestrap' ),
+				'desc'      => __( 'You can adjust the padding base. This affects buttons size and lots of other cool stuff too! Default: 8', 'shoestrap' ),
+				'id'        => 'padding_base',
+				'default'   => 6,
+				'min'       => 0,
+				'step'      => 1,
+				'max'       => 20,
+				'advanced'  => true,
+				'compiler'  => true,
+				'type'      => 'slider',
+			);
 
-	$section['fields'] = $fields;
+			$fields[] = array( 
+				'title'     => __( 'Root Relative URLs', 'shoestrap' ),
+				'desc'      => __( 'Return URLs such as <em>/assets/css/style.css</em> instead of <em>http://example.com/assets/css/style.css</em>. Default: ON', 'shoestrap' ),
+				'id'        => 'root_relative_urls',
+				'default'   => 0,
+				'type'      => 'switch'
+			);
 
-	$section = apply_filters( 'shoestrap_module_advanced_options_modifier', $section );
-	
-	$sections[] = $section;
-	
-	return $sections;
+			$fields[] = array( 
+				'title'     => __( 'Enable Nice Search', 'shoestrap' ),
+				'desc'      => __( 'Redirects /?s=query to /search/query/, convert %20 to +. Default: ON', 'shoestrap' ),
+				'id'        => 'nice_search',
+				'default'   => 1,
+				'type'      => 'switch'
+			);
 
+			$fields[] = array( 
+				'title'     => __( 'Custom CSS', 'shoestrap' ),
+				'desc'      => __( 'You can write your custom CSS here. This code will appear in a script tag appended in the header section of the page.', 'shoestrap' ),
+				'id'        => 'user_css',
+				'default'   => '',
+				'type'      => 'ace_editor',
+				'mode'      => 'css',
+				'theme'     => 'monokai',
+			);
+
+			$fields[] = array( 
+				'title'     => __( 'Custom LESS', 'shoestrap' ),
+				'desc'      => __( 'You can write your custom LESS here. This code will be compiled with the other LESS files of the theme and be appended to the header.', 'shoestrap' ),
+				'id'        => 'user_less',
+				'default'   => '',
+				'type'      => 'ace_editor',
+				'mode'      => 'less',
+				'theme'     => 'monokai',
+				'compiler'  => true,
+			);  
+
+			$fields[] = array( 
+				'title'     => __( 'Custom JS', 'shoestrap' ),
+				'desc'      => __( 'You can write your custom JavaScript/jQuery here. The code will be included in a script tag appended to the bottom of the page.', 'shoestrap' ),
+				'id'        => 'user_js',
+				'default'   => '',
+				'type'      => 'ace_editor',
+				'mode'      => 'javascript',
+				'theme'     => 'monokai',
+			);
+
+			$fields[] = array( 
+				'title'     => __( 'Minimize CSS', 'shoestrap' ),
+				'desc'      => __( 'Minimize the genearated CSS. This should be ON for production sites. Default: OFF.', 'shoestrap' ),
+				'id'        => 'minimize_css',
+				'default'   => 1,
+				'compiler'  => true,
+				'customizer'=> array(),
+				'type'      => 'switch',
+			);
+
+			$fields[] = array(
+				'title'     => __( 'Toggle adminbar On/Off', 'shoestrap' ),
+				'desc'      => __( 'Turn the admin bar On or Off on the frontend. Default: Off.', 'shoestrap' ),
+				'id'        => 'advanced_wordpress_disable_admin_bar_toggle',
+				'default'   => 0,
+				'customizer'=> array(),
+				'type'      => 'switch',
+			);
+
+			$fields[] = array(
+				'title'     => __( 'Use Google CDN for jQuery', 'shoestrap' ),
+				'desc'      => '',
+				'id'        => 'jquery_cdn_toggler',
+				'default'   => 0,
+				'customizer'=> array(),
+				'type'      => 'switch',
+			);
+
+			$section['fields'] = $fields;
+
+			$section = apply_filters( 'shoestrap_module_advanced_options_modifier', $section );
+			
+			$sections[] = $section;
+			
+			return $sections;
+
+		}
+
+		/*
+		 * echo any custom CSS the user has written to the <head> of the page
+		 */
+		function user_css() {
+			$header_scripts = shoestrap_getVariable( 'user_css' );
+			
+			if ( trim( $header_scripts ) != '' )
+				wp_add_inline_style( 'shoestrap_css', $header_scripts );
+		}
+
+		/*
+		 * echo any custom JS the user has written to the footer of the page
+		 */
+		function user_js() {
+			$footer_scripts = shoestrap_getVariable( 'user_js' );
+
+			if ( trim( $footer_scripts ) != '' )
+				echo '<script id="core.advanced-user-js">' . $footer_scripts . '</script>';
+		}
+
+		/**
+		 * Switch the adminbar On/Off
+		 */
+		function admin_bar() {
+			if ( shoestrap_getVariable( 'advanced_wordpress_disable_admin_bar_toggle' ) == 0 )
+				return false;
+			else
+				return true;
+		}
+
+		/**
+		 * The Google Analytics code
+		 */
+		function google_analytics() {
+			$analytics_id = shoestrap_getVariable( 'analytics_id' );
+
+			if ( !is_null( $analytics_id ) && !empty( $analytics_id ) )
+				echo "<script>
+			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+			e.src='//www.google-analytics.com/analytics.js';
+			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+			ga('create','" . $analytics_id . "');ga('send','pageview');
+			</script>";
+		}
+
+		/**
+		 * Redirects search results from /?s=query to /search/query/, converts %20 to +
+		 *
+		 * @link http://txfx.net/wordpress-plugins/nice-search/
+		 */
+		function nice_search_redirect() {
+			global $wp_rewrite;
+
+			if ( !isset( $wp_rewrite ) || !is_object( $wp_rewrite ) || !$wp_rewrite->using_permalinks() )
+				return;
+
+			$search_base = $wp_rewrite->search_base;
+			if ( is_search() && !is_admin() && strpos( $_SERVER['REQUEST_URI'], "/{$search_base}/" ) === false ) {
+				wp_redirect( home_url( "/{$search_base}/" . urlencode( get_query_var( 's' ) ) ) );
+				exit();
+			}
+		}
+
+		/**
+		 * Root relative URLs
+		 *
+		 * WordPress likes to use absolute URLs on everything - let's clean that up.
+		 * Inspired by http://www.456bereastreet.com/archive/201010/how_to_make_wordpress_urls_root_relative/
+		 *
+		 * @author Scott Walkinshaw <scott.walkinshaw@gmail.com>
+		 */
+		function root_relative_url( $input ) {
+			preg_match( '|https?://([^/]+)(/.*)|i', $input, $matches );
+
+			if ( !isset( $matches[1] ) || !isset( $matches[2] ) )
+				return $input;
+			elseif ( ( $matches[1] === $_SERVER['SERVER_NAME'] ) || $matches[1] === $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] )
+				return wp_make_link_relative($input);
+			else
+				return $input;
+		}
+
+		function enable_root_relative_urls() {
+			return !( is_admin() || in_array( $GLOBALS['pagenow'], array( 'wp-login.php', 'wp-register.php' ) ) );
+		}
+
+		/**
+		 * Enqueue some extra scripts
+		 */
+		function scripts() {
+			if ( shoestrap_getVariable( 'retina_toggle' ) == 1 ) {
+				wp_register_script( 'retinajs', SHOESTRAP_ASSETS_URL . '/js/vendor/retina.js', false, null, true );
+				wp_enqueue_script( 'retinajs' );
+			}
+		}
+
+		/**
+		 * Use a CDN for jQuery
+		 */
+		function jquery_cdn() {
+
+			// jQuery is loaded using the same method from HTML5 Boilerplate:
+			// Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
+			// It's kept in the header instead of footer to avoid conflicts with plugins.
+			if ( !is_admin() ) {
+				wp_deregister_script( 'jquery' );
+				wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, false );
+				add_filter( 'script_loader_src', 'jquery_local_fallback', 10, 2 );
+			}
+		}
+
+		/**
+		 * http://wordpress.stackexchange.com/a/12450
+		 */
+		function jquery_local_fallback( $src, $handle = null ) {
+			static $add_jquery_fallback = false;
+
+			if ( $add_jquery_fallback ) {
+				echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery-1.11.0.min.js"><\/script>\')</script>' . "\n";
+				$add_jquery_fallback = false;
+			}
+
+			if ( $handle === 'jquery' )
+				$add_jquery_fallback = true;
+
+			return $src;
+		}
+
+
+		/**
+		 * Toggle activation of the jQuery CDN
+		 */
+		function jquery_cdn_toggler() {
+			if ( shoestrap_getVariable( 'jquery_cdn' ) == 1 ) {
+				add_action( 'wp_enqueue_scripts', 'jquery_cdn', 100 );
+				add_action( 'wp_head', 'jquery_local_fallback' );
+			}
+		}
+
+		/**
+		 * Variables to use for the compiler.
+		 * These override the default Bootstrap Variables.
+		 */
+		public static function variables() {
+			$padding_base  = intval( shoestrap_getVariable( 'padding_base', true ) );
+			$border_radius = filter_var( shoestrap_getVariable( 'general_border_radius', true ), FILTER_SANITIZE_NUMBER_INT );
+			$border_radius = ( strlen( $border_radius ) < 1 ) ? 0 : $border_radius;
+
+			$variables = '';
+
+			$variables .= '@padding-base-vertical:    ' . round( $padding_base * 6 / 6 ) . 'px;';
+			$variables .= '@padding-base-horizontal:  ' . round( $padding_base * 12 / 6 ) . 'px;';
+
+			$variables .= '@padding-large-vertical:   ' . round( $padding_base * 10 / 6 ) . 'px;';
+			$variables .= '@padding-large-horizontal: ' . round( $padding_base * 16 / 6 ) . 'px;';
+
+			$variables .= '@padding-small-vertical:   ' . round( $padding_base * 5 / 6 ) . 'px;';
+			$variables .= '@padding-small-horizontal: @padding-large-vertical;';
+
+			$variables .= '@padding-xs-vertical:      ' . round( $padding_base * 1 / 6 ) . 'px;';
+			$variables .= '@padding-xs-horizontal:    @padding-small-vertical;';
+
+			$variables .= '@border-radius-base:  ' . round( $border_radius * 4 / 4 ) . 'px;';
+			$variables .= '@border-radius-large: ' . round( $border_radius * 6 / 4 ) . 'px;';
+			$variables .= '@border-radius-small: ' . round( $border_radius * 3 / 4 ) . 'px;';
+
+			$variables .= '@pager-border-radius: ' . round( $border_radius * 15 / 4 ) . 'px;';
+
+			$variables .= '@tooltip-arrow-width: @padding-small-vertical;';
+			$variables .= '@popover-arrow-width: (@tooltip-arrow-width * 2);';
+
+			$variables .= '@thumbnail-padding:         ' . round( $padding_base * 4 / 6 ) . 'px;';
+			$variables .= '@thumbnail-caption-padding: ' . round( $padding_base * 9 / 6 ) . 'px;';
+
+			$variables .= '@badge-border-radius: ' . round( $border_radius * 10 / 4 ) . 'px;';
+
+			$variables .= '@breadcrumb-padding-vertical:   ' . round( $padding_base * 8 / 6 ) . 'px;';
+			$variables .= '@breadcrumb-padding-horizontal: ' . round( $padding_base * 15 / 6 ) . 'px;';
+
+			return $variables;
+		}
+
+		/**
+		 *
+		 */
+		function variables_filter( $variables ) {
+			return $variables . self::variables();
+		}
+	}
 }
-endif;
-add_filter( 'redux/options/' . SHOESTRAP_OPT_NAME . '/sections', 'shoestrap_module_advanced_options', 95 );
 
-
-include_once( dirname( __FILE__ ) . '/includes/functions.advanced.php' );
-include_once( dirname( __FILE__ ) . '/includes/variables.php' );
-include_once( dirname( __FILE__ ) . '/includes/scripts.php' );
-
-function shoestrap_relative_urls_including() {
-	if ( shoestrap_getVariable( 'root_relative_urls' ) == 1  )
-		include_once( dirname( __FILE__ ) . '/includes/relative-urls.php' );
-}
-add_action( 'after_setup_theme', 'shoestrap_relative_urls_including' );
+$advanced = new ShoestrapAdvanced();
