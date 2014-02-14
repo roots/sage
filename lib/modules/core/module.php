df4cbcbc8bc3addedf2887edef724033ef6a1bd0
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-14 18:51:02 +0200

    classified the blog module & separated the ShoestrapImage class

--- lib/modules/core/module.php
+++ lib/modules/core/module.php
@@ -5,7 +5,8 @@ define( 'themeFOLDER', get_template() );
 define( 'themePATH', get_theme_root() );
 define( 'themeNAME', wp_get_theme() );
 
-include_once( dirname( __FILE__ ) . '/includes/functions.color.php' );
+include_once( dirname( __FILE__ ) . '/includes/class.ShoestrapColor.php' );
+include_once( dirname( __FILE__ ) . '/includes/class.ShoestrapImage.php' );
 
 
 if ( !function_exists( 'shoestrap_getVariable' ) ) :
