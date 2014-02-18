7aeeccd56cf69a1fdc87f15ed5680313556bf62b
Giacomo Zinetti <giacomo.zinetti@bryan.it>
2014-02-18 18:31:50 +0100

    theme compatible with grunt-wordpress-version 0.2.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'assets/js/*.js',
+        '!assets/js/scripts.min.*.js',
         '!assets/js/scripts.min.js'
       ]
     },
