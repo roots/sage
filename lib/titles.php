952b3a07a722ee7622e5fd31d14a76bf0f1ddd6e
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-18 19:35:37 +0200

    bugfix

--- lib/titles.php
+++ lib/titles.php
@@ -44,7 +44,8 @@ function shoestrap_title() {
  */
 function shoestrap_title_section( $header = true, $element = 'h1', $link = false, $class = 'entry-title' ) {
 	$content  = $header ? '<header>' : '';
-	$content .= '<title><' . $element . ' class="' . $class . '"></title>';
+	$content .= '<title>' . get_the_title() . '</title>';
+	$content .= '<' . $element . ' class="' . $class . '">';
 	$content .= $link ? '<a href="' . get_permalink() . '">' : '';
 	$content .= is_singular() ? shoestrap_title() : apply_filters( 'shoestrap_title', get_the_title() );
 	$content .= $link ? '</a>' : '';
