9850d144bcfb247df3d4a00349caf111f3486ecf
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-25 18:38:54 +0200

    using framework class for footer template & 404

--- 404.php
+++ 404.php
@@ -1,13 +1,16 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'shoestrap'); ?>
-</div>
+global $ss_framework;
 
-<p><?php _e('It looks like this was the result of either:', 'shoestrap'); ?></p>
+get_template_part('templates/page', 'header');
+
+$alert_message = __( 'Sorry, but the page you were trying to view does not exist.', 'shoestrap' );
+echo $ss_framework->alert( $type = 'warning', $alert_message );
+?>
+
+<p><?php _e( 'It looks like this was the result of either:', 'shoestrap' ); ?></p>
 <ul>
-  <li><?php _e('a mistyped address', 'shoestrap'); ?></li>
-  <li><?php _e('an out-of-date link', 'shoestrap'); ?></li>
+	<li><?php _e( 'a mistyped address', 'shoestrap' ); ?></li>
+	<li><?php _e( 'an out-of-date link', 'shoestrap' ); ?></li>
 </ul>
-
 <?php get_search_form();
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
+<?php global $ss_framework; ?>
 <footer id="page-footer" class="content-info" role="contentinfo">
 	<div class="<?php echo apply_filters( 'shoestrap_container_class', 'container' ); ?>">
-		<div class="row">
+		<?php echo $ss_framework->makerow( 'div' ); ?>
 			<?php shoestrap_footer_content(); ?>
 		</div>
 	</div>
