a4fbc44049869a2cf73e2bbd73b3740d67f98662
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-25 19:48:39 +0200

    bugfix

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <?php global $ss_framework; ?>
 <footer id="page-footer" class="content-info" role="contentinfo">
 	<div class="<?php echo apply_filters( 'shoestrap_container_class', 'container' ); ?>">
-		<?php echo $ss_framework->makerow( 'div' ); ?>
+		<?php echo $ss_framework->make_row( 'div' ); ?>
 			<?php shoestrap_footer_content(); ?>
 		</div>
 	</div>
