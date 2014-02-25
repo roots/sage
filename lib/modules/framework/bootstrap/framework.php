1a7b99933c0ac44b501b667d246e93a9a0a468ee
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-25 19:53:16 +0200

    moving gallery modifications inside the bootstrap framework

--- lib/modules/framework/bootstrap/framework.php
+++ lib/modules/framework/bootstrap/framework.php
@@ -2,5 +2,6 @@
 
 include_once( dirname( __FILE__ ) . '/class-Shoestrap_Bootstrap.php' );            // Framework class.
 include_once( dirname( __FILE__ ) . '/menus/nav.php' );                            // NavWalker
+include_once( dirname( __FILE__ ) . '/gallery.php' );                              // Custom [gallery] modifications
 include_once( dirname( __FILE__ ) . '/menus/class-Shoestrap_Menus.php' );          // The menus module
 include_once( dirname( __FILE__ ) . '/jumbotron/class-Shoestrap_Jumbotron.php' );  // The Jumbotron module
