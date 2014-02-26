de1b01f1c346e8d938896878d50ec337f3877e6a
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-26 22:46:23 +0200

    added modules to foundation as well

--- framework/foundation/framework.php
+++ framework/foundation/framework.php
@@ -6,6 +6,13 @@ if ( $ss_settings['framework'] == 'foundation' ) {
 	define( 'SS_FRAMEWORK_PATH', dirname( __FILE__ ) );
 
 	include_once( dirname( __FILE__ ) . '/class-Shoestrap_Foundation.php' );               // Framework class.
+
+	include_once( dirname( __FILE__ ) . '/modules/class-Shoestrap_Branding.php' );     // Branding
+	include_once( dirname( __FILE__ ) . '/modules/class-Shoestrap_Typography.php' );   // Typography
+	include_once( dirname( __FILE__ ) . '/modules/class-Shoestrap_Social.php' );       // Social
+	include_once( dirname( __FILE__ ) . '/modules/class-Shoestrap_Layout.php' );       // layout
+	include_once( dirname( __FILE__ ) . '/modules/widgets.php' );                      // Widgets
+
 	include_once( dirname( __FILE__ ) . '/menus/class-Shoestrap_Foundation_Menus.php' );   // The menus module
 	include_once( dirname( __FILE__ ) . '/menus/nav-foundation.php' );                     // Specific classes for navbar
 }
