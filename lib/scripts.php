328801482e7728b799486600fca52d1f65d75189
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2014-02-26 15:19:48 +0100

    added styles for friendship buttons

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '32f3d046b837fa5e890c34d23f2ab623');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '977858ac323cbaf54fd35f7566814887');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
