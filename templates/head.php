a0a071c2c7332bb8f1a4397bccaa82ddeb666b51
Jackie Lord <j4ckielord@gmail.com>
2014-03-04 09:58:13 +0700

    Change meta charset from UTF-8 to Charset setting

--- templates/head.php
+++ templates/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
