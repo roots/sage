3689b727871037a5023090effd8575fce7ad3e34
Aristeides Stathopoulos <aristath@gmail.com>
2014-03-04 16:35:15 +0200

    removed obsolete scripts and references

--- lib/scripts.php
+++ lib/scripts.php
@@ -10,13 +10,11 @@ function shoestrap_scripts() {
 	wp_enqueue_style( 'shoestrap_css', $stylesheet_url, false, $stylesheet_ver );
 
 	wp_register_script( 'modernizr',         SHOESTRAP_ASSETS_URL . '/js/vendor/modernizr-2.7.0.min.js', false, null, false );
-	wp_register_script( 'shoestrap_main',    SHOESTRAP_ASSETS_URL . '/js/main.js',                       false, null, true  );
 	wp_register_script( 'fitvids',           SHOESTRAP_ASSETS_URL . '/js/vendor/jquery.fitvids.js',      false, null, true  );
 
 	wp_enqueue_script( 'jquery'            );
 
 	wp_enqueue_script( 'modernizr'         );
-	wp_enqueue_script( 'shoestrap_main'    );
 	wp_enqueue_script( 'fitvids' );
 
 	if ( is_single() && comments_open() && get_option( 'thread_comments' ) ) {
