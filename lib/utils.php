dede0bd8515018c6abd41a241b57d06ab696f63c
Ben Word <ben@benword.com>
2014-03-11 23:34:17 -0500

    Close #992 - Move some Roots features into Soil

--- lib/utils.php
+++ lib/utils.php
@@ -2,13 +2,15 @@
 /**
  * Utility functions
  */
-function add_filters($tags, $function) {
-  foreach($tags as $tag) {
-    add_filter($tag, $function);
-  }
-}
-
 function is_element_empty($element) {
   $element = trim($element);
   return empty($element) ? false : true;
 }
+
+// Tell WordPress to use searchform.php from the templates/ directory
+function roots_get_search_form($form) {
+  $form = '';
+  locate_template('/templates/searchform.php', true, false);
+  return $form;
+}
+add_filter('get_search_form', 'roots_get_search_form');
