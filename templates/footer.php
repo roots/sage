289ea01fc291af5f1b31634f3022d1cfc9264a13
Ben Word <ben@benword.com>
2014-03-12 00:04:56 -0500

    Separate containers

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,7 @@
-<footer class="content-info container" role="contentinfo">
-  <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    </div>
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   </div>
 </footer>
 
