cf68ee7616cca745e9dc4ca527d0303e505bf5dd
Artem Belov <artbelov@live.ru>
2014-03-13 20:53:58 +0400

    Fix Search Form label.
    Move Search label outside ".input-group" and fix POT-file.

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url('/'); ?>">
+  <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
   <div class="input-group">
     <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
-    <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'roots'); ?></button>
     </span>
