eb8fcb1e8f64ad534fe447dc7a8245240ee9287c
Russell Fair <rfair404@gmail.com>
2014-03-21 17:37:21 +0100

    add hooks for footer, before, top, bottom, after and body bottom

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,12 @@
+<?php roots_footer_before(); ?>
 <footer class="content-info" role="contentinfo">
   <div class="container">
+    <?php tha_footer_top(); ?>
     <?php dynamic_sidebar('sidebar-footer'); ?>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    <?php tha_footer_bottom(); ?>
   </div>
 </footer>
-
+<?php roots_footer_after(); ?>
+<?php roots_body_bottom(); ?>
 <?php wp_footer(); ?>
