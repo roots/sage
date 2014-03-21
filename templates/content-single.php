1f56dc920fac2d0f9c09efd7d1e146454efbf9a1
Russell Fair <rfair404@gmail.com>
2014-03-21 17:40:47 +0100

    add the entry hooks

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,5 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
+  <?php roots_entry_before(); ?>
   <article <?php post_class(); ?>>
+    <?php roots_entry_top(); ?>
     <header>
       <?php get_template_part('templates/breadcrumb'); ?>
       <h1 class="entry-title"><?php the_title(); ?></h1>
@@ -12,5 +14,7 @@
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
+    <?php tha_entry_bottom(); ?>
   </article>
+  <?php roots_entry_after(); ?>
 <?php endwhile; ?>
