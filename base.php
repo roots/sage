447e92493c96770faec171459f003a43a624655d
Russell Fair <rfair404@gmail.com>
2014-03-21 17:29:48 +0100

    add tha_body_top

--- base.php
+++ base.php
@@ -1,6 +1,6 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
-
+  <?php roots_body_top(); ?>
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
@@ -31,6 +31,5 @@
   </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
-
 </body>
 </html>
