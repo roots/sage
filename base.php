b7460b7704eac4b1e97212dfebd5a6a5dfdf0754
Aristeides Stathopoulos <aristath@gmail.com>
2014-04-02 17:44:22 +0300

    spacing

--- base.php
+++ base.php
@@ -1,10 +1,10 @@
-<?php ss_get_template_part('templates/head'); ?>
+<?php ss_get_template_part( 'templates/head' ); ?>
 <body <?php body_class(); ?>>
 <a href="#content" class="sr-only"><?php _e( 'Skip to main content', 'shoestrap' ); ?></a>
 <?php global $ss_framework; ?>
 
 	<!--[if lt IE 8]>
-		<?php echo $ss_framework->alert( 'warning', __('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'shoestrap') ); ?>
+		<?php echo $ss_framework->alert( 'warning', __(' You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'shoestrap' ) ); ?>
 	<![endif]-->
 
 	<?php do_action( 'get_header' ); ?>
@@ -17,7 +17,7 @@
 
 	<?php echo $ss_framework->open_container( 'div', 'wrap-main-section', 'wrap main-section' ); ?>
 
-		<?php do_action('shoestrap_pre_content'); ?>
+		<?php do_action( 'shoestrap_pre_content' ); ?>
 
 		<div id="content" class="content">
 			<?php echo $ss_framework->open_row( 'div', null, 'bg' ); ?>
@@ -28,7 +28,7 @@
 					<?php include shoestrap_template_path(); ?>
 				</main><!-- /.main -->
 
-				<?php do_action('shoestrap_after_main'); ?>
+				<?php do_action( 'shoestrap_after_main' ); ?>
 
 				<?php if ( shoestrap_display_primary_sidebar() ) : ?>
 					<aside id="sidebar-primary" class="sidebar <?php shoestrap_section_class( 'primary', true ); ?>" role="complementary">
@@ -49,11 +49,11 @@
 				<?php endif; ?>
 			<?php echo $ss_framework->close_row( 'div' ); ?>
 		</div><!-- /.content -->
-		<?php do_action('shoestrap_after_content'); ?>
+		<?php do_action( 'shoestrap_after_content' ); ?>
 	<?php echo $ss_framework->close_container( 'div' ); ?><!-- /.wrap -->
 	<?php
 
-	do_action('shoestrap_pre_footer');
+	do_action( 'shoestrap_pre_footer' );
 
 	if ( ! has_action( 'shoestrap_footer_override' ) ) {
 		ss_get_template_part( 'templates/footer' );
