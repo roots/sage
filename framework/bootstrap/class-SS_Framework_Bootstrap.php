37fece8dc48952b9e4c8a2091e80c68323052755
Aristeides Stathopoulos <aristath@gmail.com>
2014-04-02 17:40:57 +0300

    Adding styles for CF7

--- framework/bootstrap/class-SS_Framework_Bootstrap.php
+++ framework/bootstrap/class-SS_Framework_Bootstrap.php
@@ -946,6 +946,11 @@ if ( ! class_exists( 'SS_Framework_Bootstrap' ) ) {
 			$variables .= '@import "' . dirname( __FILE__ ) . '/assets/less/widgets.less";';
 			$variables .= '@import "' . dirname( __FILE__ ) . '/assets/less/footer.less";';
 
+			// Include styles for Contact Form 7.
+			if ( defined( 'WPCF7_PLUGIN_DIR' ) && ! empty( WPCF7_PLUGIN_DIR ) ) {
+				$variables .= '@import "' . dirname( __FILE__ ) . '/assets/less/cf7.less";';
+			}
+
 			return $variables;
 		}
 
