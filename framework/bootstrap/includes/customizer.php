dea3fffb7fdd2c48f04f898acaa199f904735086
Aristeides Stathopoulos <aristath@gmail.com>
2014-04-04 11:40:42 +0300

    Added typography colors to the customizer

--- framework/bootstrap/includes/customizer.php
+++ framework/bootstrap/includes/customizer.php
@@ -22,10 +22,22 @@ function shoestrap_customizer_fields() {
 				),
 			),
 		),
-		'branding_section' => array(
-			'slug'   => 'branding',
-			'title'  => __( 'Branding', 'shoestrap' ),
+		'colors_section' => array(
+			'slug'   => 'colors',
+			'title'  => __( 'Colors', 'shoestrap' ),
 			'fields' => array(
+				'font_base' => array(
+					'label' => __( 'Base Font', 'shoestrap' ),
+					'type'  => 'typography',
+					'style' => 'body',
+					'priority' => 1,
+				),
+				'font_h' => array(
+					'label' => __( 'Headers', 'shoestrap' ),
+					'type'  => 'typography',
+					'style' => 'h1, h2, h3, h4, h5, h6',
+					'priority' => 2,
+				),
 				'color_brand_primary' => array(
 					'label' => __( 'Brand Colors: Primary', 'shoestrap' ),
 					'type'  => 'color',
@@ -94,6 +106,7 @@ function shoestrap_customizer( $wp_customize ) {
 					'label'    => $args['label'],
 					'section'  => $section['slug'],
 					'settings' => $field,
+					'priority' => isset( $args['priority'] ) ? $args['priority'] : null,
 				) ) );
 
 			} elseif ( 'color' == $args['type'] ) { // Color setting
@@ -112,7 +125,34 @@ function shoestrap_customizer( $wp_customize ) {
 					'label'    => $args['label'],
 					'section'  => $section['slug'],
 					'settings' => $field,
+					'priority' => isset( $args['priority'] ) ? $args['priority'] : null,
 				) ) );
+
+			} elseif ( 'typography' == $args['type'] ) { // Typography setting
+
+				if ( 'font_h' == $field ) {
+					$default = $ss_settings['font_h1']['color'];
+				} else {
+					$default = $ss_settings[$field]['color'];
+				}
+
+				// Add settings
+				$wp_customize->add_setting( $field,
+					array(
+						'default'    => $default,
+						'type'       => 'theme_mod',
+						'capability' => 'edit_theme_options'
+					)
+				);
+
+				// Add control
+				$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, $field, array(
+					'label'    => $args['label'],
+					'section'  => $section['slug'],
+					'settings' => $field,
+					'priority' => isset( $args['priority'] ) ? $args['priority'] : null,
+				) ) );
+
 			}
 		}
 	}
@@ -133,21 +173,23 @@ function shoestrap_background_css() {
 
 		foreach ( $fields as $field => $args ) {
 
+			$value = get_theme_mod( $field );
+
 			if ( 'background' == $args['type'] ) {
 
 				// Generic style for all "background" settings
-				echo $args['style'] . ' { background: ' . get_theme_mod( $field ) . '; }';
+				echo $args['style'] . ' { background: ' . $value . '; }';
 
 				// Additional styles for the content background setting
 				if ( 'body_bg' == $field ) {
-					$bg_brightness = Shoestrap_Color::get_brightness( get_theme_mod( $field ) );
+					$bg_brightness = Shoestrap_Color::get_brightness( $value );
 					// Set an "accent" color depending on the background's brightness
 					if ( $bg_brightness > 50 ) {
-						$accent = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -20 );
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -30 );
+						$accent = Shoestrap_Color::adjust_brightness( $value, -20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, -30 );
 					} else {
-						$accent = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 20 );
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 30 );
+						$accent = Shoestrap_Color::adjust_brightness( $value, 20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, 30 );
 					}
 
 					echo '.well { background: ' . $accent . '; border-color: ' . $border . ' }';
@@ -156,95 +198,99 @@ function shoestrap_background_css() {
 			} elseif ( 'color' == $args['type'] ) {
 
 				// Generic style for all "color" settings
-				echo $args['style'] . ' { color: ' . get_theme_mod( $field ) . '; }';
+				echo $args['style'] . ' { color: ' . $value . '; }';
 
 				// Additional styles per setting
 				if ( 'color_brand_primary' == $field ) {
 
-					$brightness = Shoestrap_Color::get_brightness( get_theme_mod( $field ) );
+					$brightness = Shoestrap_Color::get_brightness( $value );
 					if ( $brightness < 195 ) {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, -20 );
 						$text_c = '#fff';
 					} else {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, 20 );
 						$text_c = '#333';
 					}
 
 					echo '.btn.btn-primary {
-						background-color: ' . get_theme_mod( $field ) . ';
+						background-color: ' . $value . ';
 						border-color: ' . $border . ';
 						color: ' . $text_c . ';
 					}';
 
 				} elseif ( 'color_brand_success' == $field ) {
 
-					$brightness = Shoestrap_Color::get_brightness( get_theme_mod( $field ) );
+					$brightness = Shoestrap_Color::get_brightness( $value );
 					if ( $brightness < 195 ) {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, -20 );
 						$text_c = '#fff';
 					} else {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, 20 );
 						$text_c = '#333';
 					}
 
 					echo '.btn.btn-success {
-						background-color: ' . get_theme_mod( $field ) . ';
+						background-color: ' . $value . ';
 						border-color: ' . $border . ';
 						color: ' . $text_c . ';
 					}';
 
 				} elseif ( 'color_brand_warning' == $field ) {
 
-					$brightness = Shoestrap_Color::get_brightness( get_theme_mod( $field ) );
+					$brightness = Shoestrap_Color::get_brightness( $value );
 					if ( $brightness < 195 ) {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, -20 );
 						$text_c = '#fff';
 					} else {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, 20 );
 						$text_c = '#333';
 					}
 
 					echo '.btn.btn-warning {
-						background-color: ' . get_theme_mod( $field ) . ';
+						background-color: ' . $value . ';
 						border-color: ' . $border . ';
 						color: ' . $text_c . ';
 					}';
 
 				} elseif ( 'color_brand_danger' == $field ) {
 
-					$brightness = Shoestrap_Color::get_brightness( get_theme_mod( $field ) );
+					$brightness = Shoestrap_Color::get_brightness( $value );
 					if ( $brightness < 195 ) {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, -20 );
 						$text_c = '#fff';
 					} else {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, 20 );
 						$text_c = '#333';
 					}
 
 					echo '.btn.btn-danger {
-						background-color: ' . get_theme_mod( $field ) . ';
+						background-color: ' . $value . ';
 						border-color: ' . $border . ';
 						color: ' . $text_c . ';
 					}';
 
 				} elseif ( 'color_brand_info' == $field ) {
 
-					$brightness = Shoestrap_Color::get_brightness( get_theme_mod( $field ) );
+					$brightness = Shoestrap_Color::get_brightness( $value );
 					if ( $brightness < 195 ) {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), -20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, -20 );
 						$text_c = '#fff';
 					} else {
-						$border = Shoestrap_Color::adjust_brightness( get_theme_mod( $field ), 20 );
+						$border = Shoestrap_Color::adjust_brightness( $value, 20 );
 						$text_c = '#333';
 					}
 
 					echo '.btn.btn-info {
-						background-color: ' . get_theme_mod( $field ) . ';
+						background-color: ' . $value . ';
 						border-color: ' . $border . ';
 						color: ' . $text_c . ';
 					}';
-
 				}
+
+			} elseif ( 'typography' == $args['type'] ) {
+
+				// Generic style for all "typography" settings
+				echo $args['style'] . ' { color: ' . $value . '; }';
 
 			}
 
@@ -271,15 +317,30 @@ function shoestrap_customizer_copy_options() {
 
 		foreach ( $fields as $field => $args ) {
 
+			$value = get_theme_mod( $field );
+
 			// Backgrounds are an array of options, so we have to include each one of them separately
 			if ( 'background' == $args['type'] ) {
+
 				// Copy the theme_mod to our settings array
-				$ss_settings[$field]['background-color'] = get_theme_mod( $field );
+				$ss_settings[$field]['background-color'] = $value;
 				// Clean up theme mods
 				remove_theme_mod( $field );
+
+			} elseif ( 'typography' == $args['type'] ) {
+
+				if ( 'font_h' == $field ) {
+					$ss_settings['font_h1']['color'] = $value;
+					$ss_settings['font_h2']['color'] = $value;
+					$ss_settings['font_h3']['color'] = $value;
+					$ss_settings['font_h4']['color'] = $value;
+					$ss_settings['font_h5']['color'] = $value;
+					$ss_settings['font_h6']['color'] = $value;
+				}
+
 			} else {
 				// Copy the theme_mod to our settings array
-				$ss_settings[$field] = get_theme_mod( $field );
+				$ss_settings[$field] = $value;
 				// Clean up theme mods
 				remove_theme_mod( $field );
 			}
