92f4b3ef29dfe5e375fa710ea1ef76dd321a7e6e
Tibor Paulsch <4tibor@gmail.com>
2014-04-06 12:34:58 +0200

    Responsive image tag from Sass replaced with CSS

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '39590a8f4e003ed77a38f73338819732');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a9fed573bd798e4e53f639b5ebc3064c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
