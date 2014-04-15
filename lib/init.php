e7ac4ebe5b23fbb3d80c3119c50297acb71feae4
Aristeides Stathopoulos <aristath@gmail.com>
2014-04-15 19:05:51 +0300

    Add suport for HTML5 Galleries & Captions
    See http://make.wordpress.org/core/2014/04/15/html5-galleries-captions-
    in-wordpress-3-9/ for mor details

--- lib/init.php
+++ lib/init.php
@@ -7,7 +7,7 @@ function shoestrap_setup() {
 	load_theme_textdomain( 'shoestrap', get_template_directory() . '/lang' );
 
 	// Register wp_nav_menu() menus ( http://codex.wordpress.org/Function_Reference/register_nav_menus )
-	register_nav_menus( array( 
+	register_nav_menus( array(
 		'primary_navigation'   => __( 'Primary Navigation', 'shoestrap' ),
 		'secondary_navigation' => __( 'Secondary Navigation', 'shoestrap' ),
 	 ) );
@@ -19,6 +19,8 @@ function shoestrap_setup() {
 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 
 	add_theme_support( 'automatic-feed-links' );
+
+	add_theme_support( 'html5', array( 'gallery', 'caption' ) );
 
 	// Tell the TinyMCE editor to use a custom stylesheet
 	add_editor_style( '/assets/css/editor-style.css' );
