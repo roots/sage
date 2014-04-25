e594059812d8310ed8bc4e1a4effe3ce5d374190
Aristeides Stathopoulos <aristath@gmail.com>
2014-04-25 17:48:37 +0300

    cleanup - fixed "headers already sent" issue

--- framework/bootstrap/includes/class-Shoestrap_Options.php
+++ framework/bootstrap/includes/class-Shoestrap_Options.php
@@ -48,50 +48,6 @@ if ( ! class_exists( 'Shoestrap_Options' ) ) {
 
 			$settings = get_option( SHOESTRAP_OPT_NAME );
 
-			$ct              = wp_get_theme();
-			$this->theme     = $ct;
-			$item_name       = $this->theme->get('Name');
-			$tags            = $this->theme->Tags;
-			$screenshot      = $this->theme->get_screenshot();
-			$class           = $screenshot ? 'has-screenshot' : '';
-
-			$customize_title = sprintf( __( 'Customize &#8220;%s&#8221;', 'shoestrap'), $this->theme->display('Name'));
-
-			?>
-			<div id="current-theme" class="<?php echo esc_attr($class); ?>">
-				<?php if ( $screenshot ) : ?>
-					<?php if ( current_user_can( 'edit_theme_options' ) ) : ?>
-						<a href="<?php echo wp_customize_url(); ?>" class="load-customize hide-if-no-customize" title="<?php echo esc_attr( $customize_title ); ?>">
-							<img src="<?php echo esc_url( $screenshot ); ?>" alt="<?php esc_attr_e( 'Current theme preview' ); ?>" />
-						</a>
-					<?php endif; ?>
-					<img class="hide-if-customize" src="<?php echo esc_url( $screenshot ); ?>" alt="<?php esc_attr_e( 'Current theme preview' ); ?>" />
-				<?php endif; ?>
-
-				<h4><?php echo $this->theme->display( 'Name' ); ?></h4>
-
-				<div>
-					<ul class="theme-info">
-						<li><?php printf( __( 'By %s', 'shoestrap' ), $this->theme->display( 'Author' ) ); ?></li>
-						<li><?php printf( __( 'Version %s', 'shoestrap' ), $this->theme->display( 'Version' ) ); ?></li>
-						<li><?php echo '<strong>' . __( 'Tags', 'shoestrap' ) . ':</strong> '; ?><?php printf( $this->theme->display( 'Tags' ) ); ?></li>
-					</ul>
-					<p class="theme-description"><?php echo $this->theme->display( 'Description' ); ?></p>
-
-					<?php
-					if ( $this->theme->parent() ) {
-						printf('<p class="howto">' . __( 'This <a href="%1$s">child theme</a> requires its parent theme, %2$s.' ) . '</p>', __( 'http://codex.wordpress.org/Child_Themes', 'shoestrap' ), $this->theme->parent()->display( 'Name' ) );
-					}
-					?>
-				</div>
-			</div>
-
-			<?php
-
-			$item_info = ob_get_contents();
-
-			ob_end_clean();
-
 			// General Settings
 			$this->sections[] = array(
 				'title' => __( 'General', 'shoestrap' ),
