507a81445b35272b115250405b8ac3e7f6bb00d9
Michael Silber <michael@cfxdesign.com>
2014-05-26 13:54:46 -0400

    Add "collapsed" default class to navbar collapse button

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
