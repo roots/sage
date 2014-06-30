17ee6e42d881b7e26e548ed123b922efa2096e7e
Ben Word <ben@benword.com>
2014-06-30 00:12:37 -0500

    Update lib/init.php

--- lib/init.php
+++ lib/init.php
@@ -7,18 +7,21 @@ function roots_setup() {
   // Community translations can be found at https://github.com/roots/roots-translations
   load_theme_textdomain('roots', get_template_directory() . '/lang');
 
-  // Register wp_nav_menu() menus (http://codex.wordpress.org/Function_Reference/register_nav_menus)
+  // Register wp_nav_menu() menus
+  // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus(array(
-    'primary_navigation' => __('Primary Navigation', 'roots'),
+    'primary_navigation' => __('Primary Navigation', 'roots')
   ));
 
-  // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
+  // Add post thumbnails
+  // http://codex.wordpress.org/Post_Thumbnails
+  // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+  // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
-  // set_post_thumbnail_size(150, 150, false);
-  // add_image_size('category-thumb', 300, 9999); // 300px wide (and unlimited height)
 
-  // Add post formats (http://codex.wordpress.org/Post_Formats)
-  // add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+  // Add post formats
+  // http://codex.wordpress.org/Post_Formats
+  add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio'));
 
   // Tell the TinyMCE editor to use a custom stylesheet
   add_editor_style('/assets/css/editor-style.css');
