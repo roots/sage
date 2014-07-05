a94aa1650b8c4adf92f210067ced6844d8e72c34
Dave Bowker <dave@davebowker.com>
2014-07-05 13:53:13 +0100

    merged

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,187 +2,36 @@
  * Create grunt tasks in the /tasks directory
  * Define task options in the /tasks/options directory
  */
-
 'use strict';
 
-module.exports = function(grunt) {
-  // Load all tasks
-  require('load-grunt-tasks')(grunt);
-  // Show elapsed time
-  require('time-grunt')(grunt);
+module.exports = function (grunt) {
+	// Load all tasks
+	require('load-grunt-tasks')(grunt);
+	// Show elapsed time
+	require('time-grunt')(grunt);
 
-  var jsFileList = [
-    'assets/vendor/bootstrap/js/transition.js',
-    'assets/vendor/bootstrap/js/alert.js',
-    'assets/vendor/bootstrap/js/button.js',
-    'assets/vendor/bootstrap/js/carousel.js',
-    'assets/vendor/bootstrap/js/collapse.js',
-    'assets/vendor/bootstrap/js/dropdown.js',
-    'assets/vendor/bootstrap/js/modal.js',
-    'assets/vendor/bootstrap/js/tooltip.js',
-    'assets/vendor/bootstrap/js/popover.js',
-    'assets/vendor/bootstrap/js/scrollspy.js',
-    'assets/vendor/bootstrap/js/tab.js',
-    'assets/vendor/bootstrap/js/affix.js',
-    'assets/js/plugins/*.js',
-    'assets/js/_*.js'
-  ];
+	var jsFileList = [
+		'assets/vendor/bootstrap/js/transition.js',
+		'assets/vendor/bootstrap/js/alert.js',
+		'assets/vendor/bootstrap/js/button.js',
+		'assets/vendor/bootstrap/js/carousel.js',
+		'assets/vendor/bootstrap/js/collapse.js',
+		'assets/vendor/bootstrap/js/dropdown.js',
+		'assets/vendor/bootstrap/js/modal.js',
+		'assets/vendor/bootstrap/js/tooltip.js',
+		'assets/vendor/bootstrap/js/popover.js',
+		'assets/vendor/bootstrap/js/scrollspy.js',
+		'assets/vendor/bootstrap/js/tab.js',
+		'assets/vendor/bootstrap/js/affix.js',
+		'assets/js/plugins/*.js',
+		'assets/js/_*.js'
+	];
 
-<<<<<<< HEAD
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'assets/js/*.js',
-        '!assets/js/scripts.js',
-        '!assets/**/*.min.*'
-      ]
-    },
-    less: {
-      dev: {
-        files: {
-          'assets/css/main.css': [
-            'assets/less/main.less'
-          ]
-        },
-        options: {
-          compress: false,
-          // LESS source map
-          // To enable, set sourceMap to true and update sourceMapRootpath based on your install
-          sourceMap: true,
-          sourceMapFilename: 'assets/css/main.css.map',
-          sourceMapRootpath: '/app/themes/roots/'
-        }
-      },
-      build: {
-        files: {
-          'assets/css/main.min.css': [
-            'assets/less/main.less'
-          ]
-        },
-        options: {
-          compress: true
-        }
-      }
-    },
-    concat: {
-      options: {
-        separator: ';',
-      },
-      dist: {
-        src: [jsFileList],
-        dest: 'assets/js/scripts.js',
-      },
-    },
-    uglify: {
-      dist: {
-        files: {
-          'assets/js/scripts.min.js': [jsFileList]
-        }
-      }
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
-      },
-      dev: {
-        options: {
-          map: 'assets/css/'
-        },
-        src: 'assets/css/main.css'
-      },
-      build: {
-        src: 'assets/css/main.min.css'
-      }
-    },
-    modernizr: {
-      build: {
-        devFile: 'assets/vendor/modernizr/modernizr.js',
-        outputFile: 'assets/js/vendor/modernizr.min.js',
-        files: {
-          'src': [
-            ['assets/js/scripts.min.js'],
-            ['assets/css/main.min.css']
-          ]
-        },
-        uglify: true,
-        parseFiles: true
-      }
-    },
-    version: {
-      default: {
-        options: {
-          format: true,
-          length: 32,
-          manifest: 'assets/manifest.json',
-          querystring: {
-            style: 'roots_css',
-            script: 'roots_js'
-          }
-        },
-        files: {
-          'lib/scripts.php': 'assets/{css,js}/{main,scripts}.min.{css,js}'
-        }
-      }
-    },
-    watch: {
-      less: {
-        files: [
-          'assets/less/*.less',
-          'assets/less/**/*.less'
-        ],
-        tasks: ['less:dev', 'autoprefixer:dev']
-      },
-      js: {
-        files: [
-          jsFileList,
-          '<%= jshint.all %>'
-        ],
-        tasks: ['jshint', 'concat']
-      },
-      livereload: {
-        // Browser live reloading
-        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-        options: {
-          livereload: false
-        },
-        files: [
-          'assets/css/main.css',
-          'assets/js/scripts.js',
-          'templates/*.php',
-          '*.php'
-        ]
-      }
-    }
-  });
-
-  // Register tasks
-  grunt.registerTask('default', [
-    'dev'
-  ]);
-  grunt.registerTask('dev', [
-    'jshint',
-    'less:dev',
-    'autoprefixer:dev',
-    'concat'
-  ]);
-  grunt.registerTask('build', [
-    'jshint',
-    'less:build',
-    'autoprefixer:build',
-    'uglify',
-    'modernizr',
-    'version'
-  ]);
-=======
 	// Set and then load configurable variables we can use in our grunt file
 	var config = {
-		pkg : grunt.file.readJSON('package.json'),
-		path : {
-			assets : "./assets"
+		pkg: grunt.file.readJSON('package.json'),
+		path: {
+			assets: "./assets"
 		}
 	};
 	grunt.initConfig(config);
@@ -194,11 +43,12 @@ module.exports = function(grunt) {
 		var key;
 
 		glob.sync('*', {
-			cwd : path
-		}).forEach(function(option) {
-			key = option.replace(/\.js$/, '');
-			object[key] = require(path + option);
-		});
+				cwd: path
+			})
+			.forEach(function (option) {
+				key = option.replace(/\.js$/, '');
+				object[key] = require(path + option);
+			});
 
 		return object;
 	}
@@ -206,13 +56,13 @@ module.exports = function(grunt) {
 
 	// The default grunt task builds all files ready for development
 	// There are more tasks defined in /tasks
-	grunt.registerTask('default', function() {
+	grunt.registerTask('default', function () {
 		grunt.log.subhead("Running grunt and building project files");
 		grunt.task.run([
-		    'clean',
-		    'less',
-		    'uglify',
-		    'version'
+			'jshint',
+			'less:dev',
+			'autoprefixer:dev',
+			'concat'
 		]);
 	});
 
@@ -222,8 +72,6 @@ module.exports = function(grunt) {
 	// Load all packages starting with 'grunt-'
 	// Packages that don't start with 'grunt-' should be defined below
 	require('load-grunt-tasks')(grunt, {
-		pattern : 'grunt-*'
+		pattern: 'grunt-*'
 	});
-	
->>>>>>> 215f0d004411337ce6ca6456b4c8588a3abd04fc
 };
