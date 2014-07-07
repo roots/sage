1994666b4401141b823caf0ac467573517606126
Phil Nelson <phil@fullyint.com>
2014-07-07 06:47:31 -0400

    Initiate the loop for template-custom.php
    Just as #1056 added a loop-start to page.php, this adds a
    loop-start to template-custom.php.

--- template-custom.php
+++ template-custom.php
@@ -4,5 +4,7 @@ Template Name: Custom Template
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
