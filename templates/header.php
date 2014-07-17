d511c3396323f5068cd780240eaa016d615dfa7d
Michael Silber <michael@cfxdesign.com>
2014-07-16 20:27:48 -0400

    Remove extra forward slash after navbar home_url()

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>/"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
