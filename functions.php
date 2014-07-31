8f88131b98a1b77c5d19ecaac9088edc9f616ac9
linoA <keyframerlino@gmail.com>
2014-07-31 09:23:42 +0800

    delete codes on functions.php

--- functions.php
+++ functions.php
@@ -70,13 +70,3 @@ if ( ! class_exists( 'ReduxFramework' ) ) {
 if ( file_exists( locate_template( '/lib/updater/updater.php' ) ) ) {
 	require_once locate_template( '/lib/updater/updater.php' );
 }
-
-if ( class_exists( 'bbPress' ) ) {
-	require_once locate_template( '/lib/bbpress.php' );
-}
-
-function theme_name_scripts() {
-	wp_enqueue_style( 'theme-style', get_stylesheet_uri());
-}
-
-add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
