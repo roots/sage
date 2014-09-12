45a00cec0e96e8546239ad9e39fb84438830e204
Austin Pray <austin@austinpray.com>
2014-09-11 22:15:45 -0500

    Use gulp-load-plugins
    https://www.npmjs.org/package/gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,6 @@
-var gulp = require('gulp'),
-  less = require('gulp-less'),
-  autoprefix = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps'),
-  rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
-  minifyCSS = require('gulp-minify-css'),
-  jshint = require('gulp-jshint'),
-  uglify = require('gulp-uglify'),
-  livereload = require('gulp-livereload'),
-  stylish = require('jshint-stylish'),
-  rev = require('gulp-rev');
-  modernizr = require('gulp-modernizr');
+/*global $:true*/
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 var paths = {
   scripts: [
@@ -48,34 +38,35 @@ var destination = {
 
 gulp.task('less', function() {
   return gulp.src(paths.less)
-    .pipe(sourcemaps.init())
-      .pipe(less()).on('error', function(err) {
+    .pipe($.sourcemaps.init())
+      .pipe($.less()).on('error', function(err) {
         console.warn(err.message);
       })
-      .pipe(autoprefix('last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12'))
-      .pipe(rename('./main.css'))
-    .pipe(sourcemaps.write())
+      .pipe($.autoprefixer('last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12'))
+      .pipe($.rename('./main.css'))
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest(destination.css))
-    .pipe(minifyCSS())
-    .pipe(rename('./main.min.css'))
+    .pipe($.minifyCss())
+    .pipe($.rename('./main.min.css'))
     .pipe(gulp.dest(destination.css))
-    .pipe(livereload({ auto: false }));
+    .pipe($.livereload({ auto: false }));
 });
 
 gulp.task('jshint', function() {
   return gulp.src(paths.jshint)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('js', ['jshint'], function() {
   return gulp.src(paths.scripts)
-    .pipe(concat('./scripts.js'))
+    .pipe($.concat('./scripts.js'))
     .pipe(gulp.dest(destination.scripts))
-    .pipe(uglify())
-    .pipe(rename('./scripts.min.js'))
+    .pipe($.uglify())
+    .pipe($.rename('./scripts.min.js'))
     .pipe(gulp.dest(destination.scripts))
-    .pipe(livereload({ auto: false }));
+    .pipe($.livereload({ auto: false }));
 });
 
 gulp.task('modernizr', function() {
@@ -83,27 +74,27 @@ gulp.task('modernizr', function() {
     ['assets/js/scripts.min.js'],
     ['assets/css/main.min.css']
   )
-    .pipe(modernizr())
+    .pipe($.modernizr())
     .pipe(gulp.dest(destination.modernizr))
-    .pipe(uglify())
-    .pipe(rename('./modernizr.min.js'))
+    .pipe($.uglify())
+    .pipe($.rename('./modernizr.min.js'))
     .pipe(gulp.dest(destination.vendor));
 });
 
 gulp.task('version', function() {
   return gulp.src(['assets/css/main.min.css', 'assets/js/scripts.min.js'], { base: 'assets' })
-    .pipe(rev())
+    .pipe($.rev())
     .pipe(gulp.dest('assets'))
-    .pipe(rev.manifest())
+    .pipe($.rev.manifest())
     .pipe(gulp.dest('assets'));
 });
 
 gulp.task('watch', function() {
-  livereload.listen();
+  $.livereload.listen();
   gulp.watch('assets/less/**/*.less', ['less']);
   gulp.watch('assets/js/**/*.js', ['jshint', 'js']);
   gulp.watch('**/*.php').on('change', function(file) {
-    livereload.changed(file.path);
+    $.livereload.changed(file.path);
   });
 });
 
