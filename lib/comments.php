610543eb6ecf32e8611833981338be9e65ebdcf7
Eric Wafford <ewafford@tidalwaveagency.com>
2014-10-04 00:00:58 -0500

    Remove roots_get_avatar function and create LESS styles semantically to the .avatar class, scoped to .comment

--- lib/comments.php
+++ lib/comments.php
@@ -42,11 +42,3 @@ class Roots_Walker_Comment extends Walker_Comment {
     echo "</div></li>\n";
   }
 }
-
-function roots_get_avatar($avatar, $type) {
-  if (!is_object($type)) { return $avatar; }
-
-  $avatar = str_replace("class='avatar", "class='avatar pull-left media-object", $avatar);
-  return $avatar;
-}
-add_filter('get_avatar', 'roots_get_avatar', 10, 2);
