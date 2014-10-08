4358853f88514f7da15d6b98193899d79290e02a
fovoc <kalliris.d@gmail.com>
2014-10-08 20:56:15 +0300

    remove breadcrumbs if Woo Child is activated

--- framework/bootstrap/class-SS_Framework_Bootstrap.php
+++ framework/bootstrap/class-SS_Framework_Bootstrap.php
@@ -154,7 +154,10 @@ if ( ! class_exists( 'SS_Framework_Bootstrap' ) ) {
 				add_filter( 'nav_menu_css_class', array( $this, 'nav_menu_css_class' ), 10, 2 );
 				add_filter( 'nav_menu_item_id',   '__return_null' );
 			}
-			add_action( 'shoestrap_pre_wrap', array( $this, 'breadcrumbs' ), 99 );
+
+			if ( ! class_exists( 'WooCommerce' ) && ! function_exists( 'shoestrap_woo_include_files' ) && ! is_woocommerce() ) {
+				add_action( 'shoestrap_pre_wrap', array( $this, 'breadcrumbs' ), 99 );
+			}
 			add_filter( 'wp_nav_menu_args',   array( $this, 'nav_menu_args' ) );
 		}
 
