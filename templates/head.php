1d05b41197f5b4ee96ba26cb7288e7c529c93dae
Michael Silber <michael@cfxdesign.com>
2014-11-13 19:41:45 -0500

    Move wp_head() to be the last markup before </head>

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,7 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <?php wp_head(); ?>
-
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+
+  <?php wp_head(); ?>
 </head>
