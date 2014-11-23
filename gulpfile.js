3a4ae77b1e1c10e7678ba4bc8889fc4099cbfe05
Austin Pray <austin@austinpray.com>
2014-11-22 19:28:38 -0600

    Reorganizes asset file structure
    Separates assets into
    - Source assets: `assets`
    - Compiled assets: `dist`
    
    Edits assets.php to reflect changes
    
    Removes '.min' from filenames
    
    Fixes:
    - removes 'clean' from build step.

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,26 @@ var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')();
 
+var build = {
+  src: 'assets/',
+  dist: 'dist/'
+};
+
 var paths = {
   scripts: [
-    'assets/src/js/**/*'
+    build.src + 'scripts/**/*'
   ],
   jshint: [
     'bower.json',
     'gulpfile.js',
-    'assets/src/js/**/*'
+    build.src + 'scripts/**/*'
   ],
-  less: 'assets/src/less/main.less',
-  editorStyle: 'assets/src/less/editor-style.less'
+  styles: build.src + 'styles/main.less',
+  editorStyle: build.src + 'styles/editor-style.less'
 };
 
-gulp.task('less:dev', function() {
-  return gulp.src(paths.less)
+gulp.task('styles:dev', function() {
+  return gulp.src(paths.styles)
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
       .pipe($.less()).on('error', function(err) {
@@ -25,32 +30,32 @@ gulp.task('less:dev', function() {
       })
       .pipe($.autoprefixer('last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12'))
     .pipe($.sourcemaps.write())
-    .pipe($.rename('./main.css'))
-    .pipe(gulp.dest('assets/dist/css'))
+    .pipe($.rename('main.css'))
+    .pipe(gulp.dest(build.dist + 'styles'))
     .pipe($.livereload({ auto: false }));
 });
 
-gulp.task('less:build', function() {
-  return gulp.src(paths.less)
+gulp.task('styles:build', function() {
+  return gulp.src(paths.styles)
     .pipe($.plumber())
       .pipe($.less()).on('error', function(err) {
         console.warn(err.message);
       })
       .pipe($.autoprefixer('last 2 versions', 'ie 9', 'android 2.3', 'android 4', 'opera 12'))
-      .pipe($.rename('./main.min.css'))
+      .pipe($.rename('main.css'))
     .pipe($.minifyCss())
-    .pipe(gulp.dest('assets/dist/css'));
+    .pipe(gulp.dest(build.dist + 'styles'));
 });
 
-gulp.task('less:editorStyle', function() {
+gulp.task('styles:editorStyle', function() {
   return gulp.src(paths.editorStyle)
     .pipe($.plumber())
     .pipe($.less()).on('error', function(err) {
       console.warn(err.message);
     })
     .pipe($.autoprefixer('last 2 versions', 'ie 9', 'android 2.3', 'android 4', 'opera 12'))
-    .pipe($.rename('./editor-style.css'))
-    .pipe(gulp.dest('assets/dist/css'));
+    .pipe($.rename('editor-style.css'))
+    .pipe(gulp.dest(build.dist + 'styles'));
 });
 
 gulp.task('jshint', function() {
@@ -60,73 +65,73 @@ gulp.task('jshint', function() {
     .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('js:dev', ['jshint'], function() {
+gulp.task('scripts:dev', ['jshint'], function() {
   return gulp.src(require('main-bower-files')().concat(paths.scripts))
     .pipe($.filter('**/*.js'))
-    .pipe($.concat('./scripts.js'))
-    .pipe(gulp.dest('assets/dist/js'))
+    .pipe($.concat('scripts.js'))
+    .pipe(gulp.dest(build.dist + 'scripts'))
     .pipe($.livereload({ auto: false }));
 });
 
-gulp.task('js:build', ['jshint'], function() {
+gulp.task('scripts:build', ['jshint'], function() {
   return gulp.src(require('main-bower-files')().concat(paths.scripts))
     .pipe($.filter('**/*.js'))
-    .pipe($.concat('./scripts.min.js'))
+    .pipe($.concat('scripts.js'))
     .pipe($.uglify())
-    .pipe(gulp.dest('assets/dist/js'));
+    .pipe(gulp.dest(build.dist + 'scripts'));
 });
 
 gulp.task('copy:fonts', function() {
-  return gulp.src(require('main-bower-files')().concat('asset/src/fonts/**/*'))
+  return gulp.src(require('main-bower-files')().concat(build.src + 'fonts/**/*'))
     .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-    .pipe(gulp.dest('assets/dist/fonts'));
+    .pipe(gulp.dest(build.dist + 'fonts'));
 });
 
 gulp.task('copy:jquery', function() {
-  return gulp.src(['bower_components/jquery/dist/jquery.min.js'])
-    .pipe($.rename('jquery-1.11.1.min.js'))
-    .pipe(gulp.dest('assets/dist/js'));
+  return gulp.src(['bower_components/jquery/dist/jquery.js'])
+    .pipe($.rename('jquery.js'))
+    .pipe(gulp.dest(build.dist + 'scripts'));
 });
 
 gulp.task('copy:modernizr', function() {
   return gulp.src(['bower_components/modernizr/modernizr.js'])
     .pipe($.uglify())
-    .pipe($.rename('modernizr.min.js'))
-    .pipe(gulp.dest('assets/dist/js'));
+    .pipe($.rename('modernizr.js'))
+    .pipe(gulp.dest(build.dist + 'scripts'));
 });
 
 gulp.task('images', function() {
-  return gulp.src('assets/src/img/**/*')
+  return gulp.src(build.src + 'src/images/**/*')
     .pipe($.imagemin({
       progressive: true,
       interlaced: true
     }))
-    .pipe(gulp.dest('assets/dist/img'));
+    .pipe(gulp.dest(build.dist + 'images'));
 });
 
 gulp.task('version', function() {
-  return gulp.src(['assets/dist/css/main.min.css', 'assets/dist/js/scripts.min.js'], { base: 'assets/dist' })
-    .pipe(gulp.dest('assets/dist'))
+  return gulp.src([build.dist + 'styles/main.css', build.dist + 'js/scripts.js'], { base: build.dist })
+    .pipe(gulp.dest(build.dist))
     .pipe($.rev())
-    .pipe(gulp.dest('assets/dist'))
+    .pipe(gulp.dest(build.dist))
     .pipe($.rev.manifest())
-    .pipe(gulp.dest('assets/dist'));
+    .pipe(gulp.dest(build.dist));
 });
 
 gulp.task('clean', function() {
-  return gulp.src(['assets/dist/css/main.min*', 'assets/dist/js/scripts.min*'], { read: false })
+  return gulp.src(build.dist, { read: false })
     .pipe($.clean());
 });
 
 gulp.task('watch', function() {
   $.livereload.listen();
-  gulp.watch(['assets/src/less/**/*', 'bower.json'], ['less:dev']);
-  gulp.watch(['assets/src/js/**/*', 'bower.json'], ['jshint', 'js:dev']);
+  gulp.watch([build.src + 'styles/**/*', 'bower.json'], ['styles:dev']);
+  gulp.watch([build.src + 'scripts/**/*', 'bower.json'], ['jshint', 'scripts:dev']);
   gulp.watch('**/*.php').on('change', function(file) {
     $.livereload.changed(file.path);
   });
 });
 
-gulp.task('default', ['less:dev', 'less:editorStyle', 'jshint', 'js:dev', 'copy:fonts', 'images']);
+gulp.task('default', ['styles:dev', 'styles:editorStyle', 'jshint', 'scripts:dev', 'copy:fonts', 'images']);
 gulp.task('dev', ['default']);
-gulp.task('build', ['clean', 'less:build', 'less:editorStyle', 'js:build', 'copy:fonts', 'copy:jquery', 'copy:modernizr', 'images', 'version']);
+gulp.task('build', ['styles:build', 'styles:editorStyle', 'scripts:build', 'copy:fonts', 'copy:jquery', 'copy:modernizr', 'images', 'version']);
