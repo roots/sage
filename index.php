b67539812a09066ac5163f13fc08bb95e7a4197a
Alkymst <webalkymst@gmail.com>
2015-01-10 13:40:22 -0700

    ref:#1033 load post_type than format on index.php

--- index.php
+++ index.php
@@ -2,32 +2,27 @@
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
-	<?php _e('Sorry, no results were found.', 'roots'); ?>
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php
-	// load post_type if it exists
-	if( get_post_type() != 'post' ) {
-	  if( get_post_format() )
-		$p_template = get_post_type() . '/' . get_post_format();
-	  else
-		$p_template = get_post_type();
-	}
-	else {
-	  $p_template = get_post_format();
-	}
-	get_template_part('templates/content', apply_filters( 'roots_post_template', $p_template) );
-  ?>
+
+  <?php if(get_post_type() != 'post') : ?>
+    <?php if(get_post_format()) get_template_part('templates/content', trailingslashit(get_post_type()) . get_post_format()); ?>
+    <?php else get_template_part('templates/content', get_post_type()); ?>
+  <?php else : ?>
+    <?php get_template_part('templates/content', get_post_format()); ?>
+  <?php endif; ?>
+
 <?php endwhile; ?>
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
   <nav class="post-nav">
-	<ul class="pager">
-	  <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-	  <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
-	</ul>
+    <ul class="pager">
+      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+    </ul>
   </nav>
 <?php endif; ?>
