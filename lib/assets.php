67f3811a6db5decee1290fcebba7968834d22497
Ben Word <ben@benword.com>
2015-01-18 17:30:49 -0600

    app.js -> main.js

--- lib/assets.php
+++ lib/assets.php
@@ -11,7 +11,7 @@ namespace Roots\Sage\Assets;
  * Enqueue scripts in the following order:
  * 1. Latest jQuery via Google CDN (if enabled in config.php)
  * 2. /theme/dist/scripts/modernizr.js
- * 3. /theme/dist/scripts/app.js
+ * 3. /theme/dist/scripts/main.js
  *
  * Google Analytics is loaded after enqueued scripts if:
  * - An ID has been defined in config.php
@@ -58,7 +58,7 @@ function assets() {
 
   wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
   wp_enqueue_script('jquery');
-  wp_enqueue_script('sage_js', asset_path('scripts/app.js'), [], null, true);
+  wp_enqueue_script('sage_js', asset_path('scripts/main.js'), [], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
