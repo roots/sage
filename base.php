611462ee124eb5291b638c5ae69c628c6d088041
Leonardo Giacone <leonardo.giacone@gmail.com>
2015-01-19 01:16:07 +0100

    Remove Bootstrap classes from base.php
    Remove Bootstrap classes from base.php and create equivalent LESS

--- base.php
+++ base.php
@@ -19,8 +19,8 @@ use Roots\Sage\Wrapper;
     get_template_part('templates/header');
   ?>
 
-  <div class="wrap container" role="document">
-    <div class="content row">
+  <div class="wrap" role="document">
+    <div class="content">
       <main class="main" role="main">
         <?php include Wrapper\template_path(); ?>
       </main><!-- /.main -->
