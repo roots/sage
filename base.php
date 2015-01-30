0fd9686147dd88823ac2228ce484a11b386c0346
Ben Word <ben@benword.com>
2015-01-29 23:20:07 -0600

    Show outdated browser alert for <IE9

--- base.php
+++ base.php
@@ -10,7 +10,7 @@ use Roots\Sage\Wrapper;
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
 
-  <!--[if lt IE 8]>
+  <!--[if lt IE 9]>
     <div class="alert alert-warning">
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
     </div>
