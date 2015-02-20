80b821575abbf568856d1d7f88de59cb472414c3
Ben Word <ben@benword.com>
2015-02-20 15:16:19 -0600

    Simplify 404 page
    Related: https://github.com/h5bp/html5-boilerplate/commit/782aefab7ea7ae57d30b237be5823298c053136d

--- 404.php
+++ 404.php
@@ -4,10 +4,4 @@
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
 </div>
 
-<p><?php _e('It looks like this was the result of either:', 'sage'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'sage'); ?></li>
-  <li><?php _e('an out-of-date link', 'sage'); ?></li>
-</ul>
-
 <?php get_search_form(); ?>
