65382410b52cdc8217c2f58b3a7596833c45a7d1
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2015-02-27 22:07:43 -0600

    Restore template-custom to sidebar config

--- lib/config.php
+++ lib/config.php
@@ -57,7 +57,8 @@ function display_sidebar() {
        */
       [
         'is_404',
-        'is_front_page'
+        'is_front_page',
+        ['is_page_template', ['template-custom.php']]
       ]
     );
 
