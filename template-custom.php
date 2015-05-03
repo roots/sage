55be6ddc6532f6f0a15ae7161ecd39a6a465f7d8
leopuleo <leonardo.giacone@gmail.com>
2015-05-03 21:54:41 +0200

    Page template re-organization

--- template-custom.php
+++ template-custom.php
@@ -6,5 +6,6 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 <?php endwhile; ?>
