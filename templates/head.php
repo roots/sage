70720ca59b69e3fc567b7f4caf050ef081b1323d
carl alberto <cralberto11@gmail.com>
2015-06-07 11:15:37 +0800

    Added the sage favicon.ico, user may need to update their own favicon image

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,9 @@
+<?php use Roots\Sage\Assets; ?>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="<?= Assets\asset_path('images/favicon.ico') ?>" />
   <?php wp_head(); ?>
 </head>
