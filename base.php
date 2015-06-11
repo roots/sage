aab62c66245a3db086c8843aee486e68f98cbda4
Viktor <viktor@futurniture.se>
2015-06-11 10:18:34 +0200

    Including content-svg.php in base.php

--- base.php
+++ base.php
@@ -16,6 +16,7 @@ use Roots\Sage\Wrapper;
     <![endif]-->
     <?php
       do_action('get_header');
+      get_template_part('templates/content-svg.php');
       get_template_part('templates/header');
     ?>
     <div class="wrap container" role="document">
