79c9a19f15af915a946b098dcac5dfaa1084b42b
hinnerk <hinnerk@gnutzmann.info>
2015-07-01 13:56:27 +0200

    Removing PHP short tags as per WP theme review standards
    PHP short tags, such as <?=$variable?>, are disabled by many web hosts and therefore discouraged according to the WP theme review standards (https://make.wordpress.org/themes/handbook/review/required/theme-check-plugin/).
    
    Removing all instances of short tags being used in favor of more verbose alternative <? echo $variable ?>

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,2 @@
-<time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time>
-<p class="byline author vcard"><?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></p>
+<time class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+<p class="byline author vcard"><?php echo __('By', 'sage'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
