f971603025321c822469398f68248e381fe6a476
kivlov van leeuwen <kivlov02@gmail.com>
2015-07-06 16:15:51 +0200

    Added additional viewport setting, user-scalable, ua-compatibility

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,6 @@
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable= no">
   <?php wp_head(); ?>
 </head>
