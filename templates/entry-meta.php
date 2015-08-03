e5ec0703e9f0e8d70abc82628ef60e3d88dd909c
Valentino Cossar <valentinocoss@gmail.com>
2015-08-04 01:09:44 +0200

    fix timezone offset in ISO 8601 date in byline

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,2 @@
-<time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time>
+<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
 <p class="byline author vcard"><?= __('By', 'shaba'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></p>
