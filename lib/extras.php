c4c59ca8f1e7a4308fa1eb0256afbcd72fb4159a
Valentino Cossar <valentinocossar@gmail.com>
2015-08-19 16:30:36 +0200

    Fixed body_class function error with url GET parameters

--- lib/extras.php
+++ lib/extras.php
@@ -11,7 +11,10 @@ function body_class($classes) {
   // Add page slug if it doesn't exist
   if (is_single() || is_page() && !is_front_page()) {
     if (!in_array(basename(get_permalink()), $classes)) {
-      $classes[] = basename(get_permalink());
+      $url = get_permalink();
+      $path = explode("?",$url);
+      $slug = basename($path[0]);
+      $classes[] = $slug;
     }
   }
 
