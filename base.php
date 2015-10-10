28a25c430f736439b557ce6e30b1d9d9f09233bd
Micah <micah@twolanedesign.com>
2015-10-10 12:13:29 -0700

    Removing redundancy with WAI-ARIA in HTML

--- base.php
+++ base.php
@@ -20,11 +20,11 @@ use Roots\Sage\Wrapper;
     ?>
     <div class="wrap container" role="document">
       <div class="content row">
-        <main class="main" role="main">
+        <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
         <?php if (Config\display_sidebar()) : ?>
-          <aside class="sidebar" role="complementary">
+          <aside class="sidebar">
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
         <?php endif; ?>
