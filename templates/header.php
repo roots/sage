70ac97292bdd382cd2eda5222a6f1939a98318fa
Micah <asuh@users.noreply.github.com>
2015-10-09 17:56:12 -0700

    Updating nav class to primary-nav

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="site-nav">
+    <nav class="primary-nav">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
