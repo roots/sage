a88a4db344b4dafad36b21db493b762ca6b73114
Ben Word <ben@benword.com>
2015-10-10 12:28:19 -0500

    Remove references to WP_ENV

--- lib/config.php
+++ lib/config.php
@@ -16,12 +16,6 @@ add_theme_support('soil-jquery-cdn');       // Enable to load jQuery from the Go
 /**
  * Configuration values
  */
-if (!defined('WP_ENV')) {
-  // Fallback if WP_ENV isn't defined in your WordPress config
-  // Used in lib/assets.php to check for 'development' or 'production'
-  define('WP_ENV', 'production');
-}
-
 if (!defined('DIST_DIR')) {
   // Path to the build directory for front-end assets
   define('DIST_DIR', '/dist/');
