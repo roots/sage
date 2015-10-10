fe7ab2764b116308429e000225497851cf25672b
Micah <micah@twolanedesign.com>
2015-10-09 17:25:49 -0700

    Removing redundancy with WAI-ARIA in HTML

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
-<header class="banner" role="banner">
+<header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
+    <nav class="site-nav">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
--- templates/searchform.php
+++ templates/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form form-inline" action="<?= esc_url(home_url('/')); ?>">
+<form method="get" class="search-form form-inline" action="<?= esc_url(home_url('/')); ?>">
   <label class="sr-only"><?php _e('Search for:', 'sage'); ?></label>
   <div class="input-group">
     <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'sage'); ?> <?php bloginfo('name'); ?>" required>
