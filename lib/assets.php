4072bb0d76bd509126814d79b1cf8a07e4766b81
Ben Word <ben@benword.com>
2015-10-10 22:58:16 -0500

    Move CSS/JS enqueues to setup.php

--- lib/assets.php
+++ lib/assets.php
@@ -3,9 +3,8 @@
 namespace Roots\Sage\Assets;
 
 /**
- * Scripts and stylesheets
+ * Get paths for assets
  */
-
 class JsonManifest {
   private $manifest;
 
@@ -57,14 +56,3 @@ function asset_path($filename) {
     return $dist_path . $directory . $file;
   }
 }
-
-function assets() {
-  wp_enqueue_style('sage/css', asset_path('styles/main.css'), false, null);
-
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
-
-  wp_enqueue_script('sage/js', asset_path('scripts/main.js'), ['jquery'], null, true);
-}
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
--- lib/setup.php
+++ lib/setup.php
@@ -89,3 +89,17 @@ function display_sidebar() {
 
   return apply_filters('sage/display_sidebar', $display);
 }
+
+/**
+ * Theme assets
+ */
+function assets() {
+  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+
+  if (is_single() && comments_open() && get_option('thread_comments')) {
+    wp_enqueue_script('comment-reply');
+  }
+
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+}
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
