24ca079c4240a10a525d52ab6a31b2582597f28a
QWp6t <hi@qwp6t.me>
2015-11-05 12:50:22 -0800

    Drop support for older browsers

--- base.php
+++ base.php
@@ -9,7 +9,7 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-    <!--[if lt IE 9]>
+    <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
