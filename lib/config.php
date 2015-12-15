c915e58ff5ec9c0a81e2e72c55fc0360cbaf21b5
Valentino Cossar <valentinocossar@gmail.com>
2015-12-15 13:23:52 +0100

    Remove references to WP_ENV but not in README.md

--- lib/config.php
+++ lib/config.php
@@ -25,12 +25,6 @@ add_theme_support('rankz-admin-login', 'ensoul.it', '#E41B44');   // Customize a
 /**
  * Configuration values
  */
-if (!defined('WP_ENV')) {
-  // Fallback if WP_ENV isn't defined in your WordPress config
-  // Used in lib/assets.php to check for 'development' or 'production'
-  define('WP_ENV', 'production');
-}
-
 if (!defined('DIST_DIR')) {
   // Path to the build directory for front-end assets
   define('DIST_DIR', '/dist/');
