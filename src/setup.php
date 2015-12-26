fa0e51faa47d6d4769b4e8e2d2a08845a27ecf4e
Ben Word <ben@benword.com>
2015-12-26 17:13:00 -0600

    Update asset handles

--- src/setup.php
+++ src/setup.php
@@ -86,6 +86,6 @@ add_action('widgets_init', function () {
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_style('sage/css', asset_path('styles/main.css'), false, null);
-  wp_enqueue_script('sage/js', asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+  wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }, 100);
