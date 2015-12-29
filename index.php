f598ca9a0bc1c000f0de5fccb9109378a8203926
QWp6t <hi@qwp6t.me>
2015-12-29 14:27:17 -0800

    Only use root editorconfig, allow warnings in travis

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 if (defined('ABSPATH')) {
-  update_option('template', get_option('template') . '/templates');
+    update_option('template', get_option('template') . '/templates');
 }
 die("Kind Regards,\nRoots");
