b48711ee80d2b03635ee035ace55271671fc2b28
Tom Brown <tbrown@aagplc.com>
2016-02-09 09:49:42 +0000

    Various

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,10 @@
 <header class="banner">
   <div class="container">
+  	<div class="row">
+  	<div class="branding col-xs-12 col-sm-6">
+    	<h1>Accident Advice Helpline</h1>
+    </div>
+  	<div class="menu col-xs-12 col-sm-6 col-md-4 col-lg-2">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav class="nav-primary">
       <?php
@@ -8,5 +13,7 @@
       endif;
       ?>
     </nav>
+    </div><!-- .menu -->
+    </div><!-- .row -->
   </div>
 </header>
