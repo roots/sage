ff1ca9079a583a6cae7ad1a36c5b207da6025ac1
Michael Vieth <mavieth@gmail.com>
2016-02-24 17:04:30 -0500

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,24 @@
-<header class="banner">
+<?php
+  // This file assumes that you have included the nav walker from https://github.com/twittem/wp-bootstrap-navwalker
+  // somewhere in your theme.
+?>
+
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
