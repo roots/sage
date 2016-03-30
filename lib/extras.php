c860648a8376e36518ecd29e195f13ed2439952d
Paul <peiche@gmail.com>
2016-03-30 16:25:36 -0500

    Custom logo function for WP < 4.5

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,9 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+function sage_the_custom_logo() {
+   if ( function_exists( 'the_custom_logo' ) ) {
+      the_custom_logo();
+   }
+}
