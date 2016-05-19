f761cada56e6fdd3061baa2c0801e42bfc07936a
Yub! <lewisyubu@gmail.com>
2016-05-18 22:46:48 -0400

    Added Bootstrap Navwalker for main menu
    Added wp-bootstrap-navwalker http://twittem.github.io/wp-bootstrap-navwalker/ "A custom WordPress nav walker class to fully implement the Bootstrap 3.0+ navigation style in a custom theme using the WordPress built in menu manager."

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,36 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+<nav class="navbar navbar-default navbar-static-top nav-primary" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <a class="navbar-brand" href="<?php bloginfo('url'); ?>">
+          <?php bloginfo('name'); ?>
+        </a>
+      </div>
+
       <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
+      if (has_nav_menu('primary_navigation')){
+        wp_nav_menu( array(
+          'menu'              => 'primary_navigation',
+          'theme_location'    => 'primary_navigation',
+          'depth'             => 2,
+          'container'         => 'div',
+          'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+          'menu_class'        => 'nav navbar-nav',
+          'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+          'walker'            => new wp_bootstrap_navwalker())
+        );
+      }
       ?>
-    </nav>
+    </div>
+  </nav>
+  <div class="container">
   </div>
 </header>
