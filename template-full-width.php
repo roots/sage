d0bfa4c66a4c514f3f3ab538b99457c2a33ef7ac
Michael Vieth <mavieth@gmail.com>
2016-07-08 14:04:07 -0600

    Full Width Page Template.

---
+++ template-full-width.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
