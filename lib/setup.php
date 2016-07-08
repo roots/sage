d3122656d595c892420fed77bba7670c6707ddb9
Michael Vieth <mavieth@gmail.com>
2016-07-08 14:04:21 -0600

    Full Width Page Template.

--- lib/setup.php
+++ lib/setup.php
@@ -86,6 +86,7 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
+    is_page_template('template-full-width.php'),
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
