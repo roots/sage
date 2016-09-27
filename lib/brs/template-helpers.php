eea6c6df5ff0ee595e9a5fa71c92059f71233821
WilliamSWoodruff <williamswoodruff@gmail.com>
2016-09-27 11:42:01 -0400

    Add closing bracket, usage example

--- lib/brs/template-helpers.php
+++ lib/brs/template-helpers.php
@@ -2,6 +2,19 @@
 
 // Component files need to live inside 'components/cmp-name/cmp-name'
 // And they need to look like <?php return function($arg1, $arg2=null, $arg3=null) {print($arg1)} ?\> (sry the escape needed to be there)
+
+/* Example usage
+
+    <div>
+        echo getComponent(
+            'page-header',
+            $title,
+            $subTitle,
+            $arg3
+        );
+    </div>
+*/
+
 function getComponent() {
 
     $args = func_get_args();
@@ -9,7 +22,7 @@ function getComponent() {
 
     $args = array_slice($args, 1);
 
-    return '<div class="'.$componentName.'">'.call_user_func_array(include(locate_template('components/'.$componentName.'/'.$componentName.'.php')), $args).'</div';
+    return '<div class="'.$componentName.'">'.call_user_func_array(include(locate_template('components/'.$componentName.'/'.$componentName.'.php')), $args).'</div>';
 }
 
 // This lets you pass a template name and save it to a variable, need to have 'templates/tmpl-name' like normal
