c959a470b0f9454033c819680f125845a6ab13e5
William Woodruff <williamswoodruff@gmail.com>
2016-10-05 10:13:30 -0400

    Overload getComponent to optionally pass in paths

--- lib/brs/template-helpers.php
+++ lib/brs/template-helpers.php
@@ -2,8 +2,6 @@
 
 // Component files need to live inside 'components/cmp-name/cmp-name'
 
-// Checkout the 2 examples in the components folder.
-
 /* How to get a component
 
     <div>
@@ -64,21 +62,44 @@ function initComponent($args, $vars) {
     return $localVarDefs;
 }
 
-
 function getComponent() {
 
     $args = func_get_args();
     $componentName = $args[0];
 
     $args = array_slice($args, 1);
+    $path = null;
+
+    if(file_exists(get_stylesheet_directory() . '/components/'.$componentName.'/'.$componentName.'.php')) {
+        $path = 'components/'.$componentName.'/'.$componentName.'.php';
+    }
+
+
+    if(!$path) {
+
+        // If not a top-level component, just assume a path was passed in
+        $componentPath = $componentName;
+        $componentName = explode('/', $componentPath);
+        $componentName = end($componentName);
+
+        if(file_exists(get_stylesheet_directory() . '/components/'.$componentPath.'.php')) {
+
+            $path = 'components/'.$componentPath.'.php';
+        } else if(file_exists(get_stylesheet_directory() . '/components/'.$componentPath.'/'.$componentName.'.php')) {
+
+            $path = 'components/'.$componentPath.'/'.$componentName.'.php';
+        } else if(file_exists(get_stylesheet_directory() . '/components/'.$componentPath.'/index.php')) {
+
+            $path = 'components/'.$componentPath.'/index.php';
+        }
+    }
 
     ob_start();
-    include(locate_template('components/'.$componentName.'/'.$componentName.'.php'));
+    include(locate_template($path));
     $component = ob_get_clean();
 
     return '<div class="'.$componentName.'">'.$component.'</div>';
 }
-
 
 // This lets you pass a template name and save it to a variable, need to have 'templates/tmpl-name' like normal
 function getTemplate($tmplName) {
