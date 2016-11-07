c1bb2b3a27bd97feb4a30db3969cfc1156ee4138
QWp6t <hi@qwp6t.me>
2016-11-06 20:32:47 -0800

    Switch assets manifest plugin

--- assets/build/webpack.config.production.js
+++ assets/build/webpack.config.production.js
@@ -1,18 +1,50 @@
-const AssetsPlugin = require('assets-webpack-plugin');
+'use strict'; // eslint-disable-line
+
+const WebpackAssetsManifest = require('webpack-assets-manifest');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const cssnano = require('cssnano');
+const path = require('path');
 
-const processOutput = require('./util/assetsPluginProcessOutput');
 const config = require('./config');
 
 module.exports = {
   plugins: [
-    new AssetsPlugin({
-      path: config.paths.dist,
-      filename: 'assets.json',
-      fullPath: false,
-      processOutput(assets) {
-        return JSON.stringify(Object.assign(processOutput(assets), config.manifest));
+    new WebpackAssetsManifest({
+      output: 'assets.json',
+      space: 2,
+      writeToDisk: false,
+      assets: config.manifest,
+      replacer(key, value) {
+        if (typeof value === 'string') {
+          return value;
+        }
+        const manifest = value;
+        /**
+         * Hack to prepend scripts/ or styles/ to manifest keys
+         *
+         * This might need to be reworked at some point.
+         *
+         * Before:
+         *   {
+         *     "main.js": "scripts/main_abcdef.js"
+         *     "main.css": "styles/main_abcdef.css"
+         *   }
+         * After:
+         *   {
+         *     "scripts/main.js": "scripts/main_abcdef.js"
+         *     "styles/main.css": "styles/main_abcdef.css"
+         *   }
+         */
+        Object.keys(manifest).forEach((src) => {
+          const sourcePath = path.basename(path.dirname(src));
+          const targetPath = path.basename(path.dirname(manifest[src]));
+          if (sourcePath === targetPath) {
+            return;
+          }
+          manifest[`${targetPath}/${src}`] = manifest[src];
+          delete manifest[src];
+        });
+        return manifest;
       },
     }),
     new OptimizeCssAssetsPlugin({
