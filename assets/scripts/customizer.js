61c97535527166bc13e4a92b681ebda7bb31749d
Andy Dayton <amdayton@gmail.com>
2016-12-14 22:24:06 -0500

    Began updating with new gulpfile

--- assets/scripts/customizer.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  // Site title
-  wp.customize('blogname', function(value) {
-    value.bind(function(to) {
-      $('.brand').text(to);
-    });
-  });
-})(jQuery);
--- lib/setup.php
+++ lib/setup.php
@@ -101,6 +101,6 @@ function assets() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/index.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
