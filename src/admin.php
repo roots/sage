725ee8446ba44e46edd24ca0456d14a41bfbb2c0
Ben Word <ben@benword.com>
2016-12-25 15:48:18 -0700

    Enable selective refresh for widgets in customizer (#1778)

--- src/admin.php
+++ src/admin.php
@@ -8,6 +8,12 @@ namespace App;
 add_action('customize_register', function (\WP_Customize_Manager $wp_customize) {
     // Add postMessage support
     $wp_customize->get_setting('blogname')->transport = 'postMessage';
+    $wp_customize->selective_refresh->add_partial('blogname', [
+        'selector' => '.brand',
+        'render_callback' => function () {
+            bloginfo('name');
+        }
+    ]);
 });
 
 /**
