1279552a6ab92349f0b56822564c9d2b2e39fb6c
Ben Word <ben@benword.com>
2017-01-11 16:08:54 -0700

    Use NoEmitOnErrorsPlugin, thanks @eshimischi

--- assets/build/webpack.config.js
+++ assets/build/webpack.config.js
@@ -175,7 +175,7 @@ if (config.enabled.optimize) {
 }
 
 if (config.env.production) {
-  webpackConfig.plugins.push(new webpack.NoErrorsPlugin());
+  webpackConfig.plugins.push(new webpack.NoEmitOnErrorsPlugin());
 }
 
 if (config.enabled.cacheBusting) {
--- assets/build/webpack.config.watch.js
+++ assets/build/webpack.config.watch.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new BrowserSyncPlugin({
       target: config.devUrl,
       publicPath: config.publicPath,
