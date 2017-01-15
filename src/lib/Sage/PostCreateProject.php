612810b1005be595b1c640b812d5cb35c6060de0
Ben Word <ben@benword.com>
2017-01-14 19:38:14 -0700

    Default to keeping Bootstrap

--- src/lib/Sage/PostCreateProject.php
+++ src/lib/Sage/PostCreateProject.php
@@ -9,21 +9,18 @@ class PostCreateProject
     public static function removeBootstrap(Event $event)
     {
         $io = $event->getIO();
+
         if ($io->isInteractive()) {
-            $remove_bootstrap = $io->askConfirmation('<info>Remove Bootstrap?</info> [<comment>Y,n</comment>]? ', true);
+            if ($io->askConfirmation('<info>Remove Bootstrap?</info> [<comment>y,N</comment>]? ', false)) {
+                file_put_contents('package.json', str_replace('    "bootstrap": "^4.0.0-alpha.6",' . "\n", '', file_get_contents('package.json')));
+                file_put_contents('assets/styles/main.scss', str_replace('// Import npm dependencies' . "\n", '', file_get_contents('assets/styles/main.scss')));
+                file_put_contents('assets/styles/main.scss', str_replace('@import "~bootstrap/scss/bootstrap";' . "\n", '', file_get_contents('assets/styles/main.scss')));
+                file_put_contents('assets/scripts/main.js', str_replace('import \'bootstrap/dist/js/bootstrap\';' . "\n", '', file_get_contents('assets/scripts/main.js')));
+                file_put_contents('assets/styles/components/_comments.scss', '');
+                file_put_contents('assets/styles/components/_forms.scss', '');
+                file_put_contents('assets/styles/components/_wp-classes.scss', '');
+                file_put_contents('assets/styles/layouts/_header.scss', '');
+            }
         }
-
-        if (!$remove_bootstrap) {
-            return 1;
-        }
-
-        file_put_contents('package.json', str_replace('    "bootstrap": "^4.0.0-alpha.6",' . "\n", '', file_get_contents('package.json')));
-        file_put_contents('assets/styles/main.scss', str_replace('// Import npm dependencies' . "\n", '', file_get_contents('assets/styles/main.scss')));
-        file_put_contents('assets/styles/main.scss', str_replace('@import "~bootstrap/scss/bootstrap";' . "\n", '', file_get_contents('assets/styles/main.scss')));
-        file_put_contents('assets/scripts/main.js', str_replace('import \'bootstrap/dist/js/bootstrap\';' . "\n", '', file_get_contents('assets/scripts/main.js')));
-        file_put_contents('assets/styles/components/_comments.scss', '');
-        file_put_contents('assets/styles/components/_forms.scss', '');
-        file_put_contents('assets/styles/components/_wp-classes.scss', '');
-        file_put_contents('assets/styles/layouts/_header.scss', '');
     }
 }
