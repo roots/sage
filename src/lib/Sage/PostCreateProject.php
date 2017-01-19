360dfc91c523a1b2d67dcf988729574456eaf93b
Ben Word <ben@benword.com>
2017-01-19 09:01:38 -0700

    Fix Bootstrap JS removal/replacement

--- src/lib/Sage/PostCreateProject.php
+++ src/lib/Sage/PostCreateProject.php
@@ -62,7 +62,7 @@ class PostCreateProject
                 case 1:
                     file_put_contents('package.json', preg_replace("/{$default_framework_pattern}/", '"foundation-sites": "6.3.0"', file_get_contents('package.json')));
                     file_put_contents('assets/styles/main.scss', str_replace('@import "~bootstrap/scss/bootstrap";' . "\n", '@import "~foundation-sites/scss/foundation";' . "\n" . '@include foundation-everything;' . "\n", file_get_contents('assets/styles/main.scss')));
-                    file_put_contents('assets/scripts/main.js', str_replace("import 'bootstrap/dist/js/bootstrap'\n", "import 'foundation-sites/dist/js/foundation';\n", file_get_contents('assets/scripts/main.js')));
+                    file_put_contents('assets/scripts/main.js', str_replace("import 'bootstrap';\n", "import 'foundation-sites/dist/js/foundation';\n", file_get_contents('assets/scripts/main.js')));
                     foreach($files_to_clear as $file) {
                         file_put_contents($file, '');
                     }
@@ -70,7 +70,7 @@ class PostCreateProject
                 case 2:
                     file_put_contents('package.json', preg_replace("/\s+{$default_framework_pattern},/", '', file_get_contents('package.json')));
                     file_put_contents('assets/styles/main.scss', str_replace('@import "~bootstrap/scss/bootstrap";' . "\n", '', file_get_contents('assets/styles/main.scss')));
-                    file_put_contents('assets/scripts/main.js', str_replace("import 'bootstrap/dist/js/bootstrap';\n", '', file_get_contents('assets/scripts/main.js')));
+                    file_put_contents('assets/scripts/main.js', str_replace("import 'bootstrap';\n", '', file_get_contents('assets/scripts/main.js')));
                     foreach($files_to_clear as $file) {
                         file_put_contents($file, '');
                     }
