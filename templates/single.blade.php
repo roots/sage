d032756f625c2332965ac82f3508952ef3241cc0
Corbin Hesse <hesse.corbin@gmail.com>
2017-03-06 15:40:06 -0600

    Add Aside Post Format & Single Partial
    - Include aside post format in `src/setup.php`
    - Loop single template through `post_type` then `post_format`
    - Create `content-single-aside` partial

--- templates/single.blade.php
+++ templates/single.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials/content-single-'.get_post_type())
+    @include('partials/content-single-'.(get_post_type() != 'post' ? get_post_type() : get_post_format()))
   @endwhile
 @endsection
-
