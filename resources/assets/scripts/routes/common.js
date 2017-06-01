a2603457483907f44e638a440acae5851e255cf5
Greg <greg@aorweb.ca>
2017-06-01 16:58:11 -0300

    Add Foundation init for all pages

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,11 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    /*
+	*	INITALIZE FOUNDATION
+	********************************************************************************/
+	$(document).foundation();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
