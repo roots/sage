442349f0a6413314ea00e4d06ed01559929fb00f
Greg <greg@aorweb.ca>
2017-06-01 15:01:28 -0300

    Add Advanced Custom fields vendor loader and settings file

---
+++ app/acf_settings.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App;
+
+/**
+ * Display warning if ACF is not installed
+ */
+add_action('admin_notices', function() {
+	if(!class_exists('acf')) {
+		$class = 'notice notice-error';
+		$message = __( 'Could not locate Advanced Custom Fields Pro. This theme may not function correctly until these files are added.', 'sage' );
+
+		printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), esc_html($message));
+	}
+});
+
+
+/**
+ * Create a Theme Options page for ACF fields
+ */
+add_action('init', function() {
+	if(function_exists('acf_add_options_page')) {
+		$parent = acf_add_options_page([
+			'page_title' => __('Theme Options', 'sage'),
+			'menu_title' => __('Theme Options', 'sage'),
+			'menu_slug' => 'theme_options',
+			'capability' => 'manage_options',
+			'post_id' => 'theme_options',
+		]);
+	}
+});
+
+
+/**
+ * Add Google API Key here
+ */
+add_action('acf/init', function() {
+	// TODO: Load API key from envar or const
+	acf_update_setting('google_api_key', '');
+});
+
+
+/**
+ * Add Post Category Ancestor rule for evaluating on Posts with a common 
+ * Parent Category
+ */
+add_filter('acf/location/rule_types', function ($choices) {
+	if (!isset($choices['Post']['post_category_ancestor'])) {
+		$choices['Post']['post_category_ancestor'] = 'Post Category Ancestor';
+	}
+	return $choices;
+});
+add_filter('acf/location/rule_values/post_category_ancestor', function ($choices) {
+	// copied from acf rules values for post_category
+	$terms = acf_get_taxonomy_terms('category');
+	if(!empty($terms)) {
+		$choices = array_pop($terms);
+	}
+	return $choices;
+});
+add_filter('acf/location/rule_match/post_category_ancestor', function ($match, $rule, $options) {
+	// most of this copied directly from acf post category rule
+	$terms = $options['post_taxonomy'];
+	$data = acf_decode_taxonomy_term($rule['value']);
+	$term = get_term_by('slug', $data['term'], $data['taxonomy']);
+	if (!$term && is_numeric($data['term'])) {
+		$term = get_term_by('id', $data['term'], $data['taxonomy']);
+	}
+	// this is where it's different than ACf
+	// get terms so we can look at the parents
+	if (is_array($terms)) {
+		foreach ($terms as $index => $term_id) {
+			$terms[$index] = get_term_by('id', intval($term_id), $term->taxonomy);
+		}
+	}
+	if (!is_array($terms) && $options['post_id']) {
+		$terms = wp_get_post_terms(intval($options['post_id']), $term->taxonomy);
+	}
+	if (!is_array($terms)) {
+		$terms = array($terms);
+	}
+	$terms = array_filter($terms);
+	$match = false;
+	// collect a list of ancestors
+	$ancestors = array();
+	if (count($terms)) {
+		foreach ($terms as $term_to_check) {
+			$ancestors = array_merge(get_ancestors($term_to_check->term_id, $term->taxonomy));
+		} // end foreach terms
+	} // end if
+	// see if the rule matches any term ancetor
+	if ($term && in_array($term->term_id, $ancestors)) {
+		$match = true;
+	}
+	
+	if ($rule['operator'] == '!=') {
+		// reverse the result
+		$match = !$match;
+	}
+	return $match;
+}, 10, 3);
--- resources/functions.php
+++ resources/functions.php
@@ -11,37 +11,37 @@
  * @param string $title
  */
 $sage_error = function ($message, $subtitle = '', $title = '') {
-    $title = $title ?: __('Sage &rsaquo; Error', 'sage');
-    $footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
-    $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
-    wp_die($message, $title);
+	$title = $title ?: __('Sage &rsaquo; Error', 'sage');
+	$footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
+	$message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
+	wp_die($message, $title);
 };
 
 /**
  * Ensure compatible version of PHP is used
  */
 if (version_compare('5.6.4', phpversion(), '>=')) {
-    $sage_error(__('You must be using PHP 5.6.4 or greater.', 'sage'), __('Invalid PHP version', 'sage'));
+	$sage_error(__('You must be using PHP 5.6.4 or greater.', 'sage'), __('Invalid PHP version', 'sage'));
 }
 
 /**
  * Ensure compatible version of WordPress is used
  */
 if (version_compare('4.7.0', get_bloginfo('version'), '>=')) {
-    $sage_error(__('You must be using WordPress 4.7.0 or greater.', 'sage'), __('Invalid WordPress version', 'sage'));
+	$sage_error(__('You must be using WordPress 4.7.0 or greater.', 'sage'), __('Invalid WordPress version', 'sage'));
 }
 
 /**
  * Ensure dependencies are loaded
  */
 if (!class_exists('Roots\\Sage\\Container')) {
-    if (!file_exists($composer = __DIR__.'/../vendor/autoload.php')) {
-        $sage_error(
-            __('You must run <code>composer install</code> from the Sage directory.', 'sage'),
-            __('Autoloader not found.', 'sage')
-        );
-    }
-    require_once $composer;
+	if (!file_exists($composer = __DIR__.'/../vendor/autoload.php')) {
+		$sage_error(
+			__('You must run <code>composer install</code> from the Sage directory.', 'sage'),
+			__('Autoloader not found.', 'sage')
+		);
+	}
+	require_once $composer;
 }
 
 /**
@@ -51,16 +51,17 @@ if (!class_exists('Roots\\Sage\\Container')) {
  * Add or remove files to the array as needed. Supports child theme overrides.
  */
 array_map(function ($file) use ($sage_error) {
-    $file = "../app/{$file}.php";
-    if (!locate_template($file, true, true)) {
-        $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
-    }
+	$file = "../app/{$file}.php";
+	if (!locate_template($file, true, true)) {
+		$sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
+	}
 }, [
-    'helpers',
-    'setup',
-    'filters',
-    'admin',
-    'cleanup',
+	'helpers',
+	'setup',
+	'filters',
+	'admin',
+	'cleanup',
+	'acf_settings',
 ]);
 
 /**
@@ -81,20 +82,47 @@ array_map(function ($file) use ($sage_error) {
  * └── TEMPLATEPATH           -> /srv/www/example.com/current/web/app/themes/sage/resources
  */
 if (is_customize_preview() && isset($_GET['theme'])) {
-    $sage_error(__('Theme must be activated prior to using the customizer.', 'sage'));
+	$sage_error(__('Theme must be activated prior to using the customizer.', 'sage'));
 }
 $sage_views = basename(dirname(__DIR__)).'/'.basename(__DIR__).'/views';
 add_filter('stylesheet', function () use ($sage_views) {
-    return dirname($sage_views);
+	return dirname($sage_views);
 });
 add_filter('stylesheet_directory_uri', function ($uri) {
-    return dirname($uri);
+	return dirname($uri);
 });
 if ($sage_views !== get_option('stylesheet')) {
-    update_option('stylesheet', $sage_views);
-    if (php_sapi_name() === 'cli') {
-        return;
-    }
-    wp_redirect($_SERVER['REQUEST_URI']);
-    exit();
+	update_option('stylesheet', $sage_views);
+	if (php_sapi_name() === 'cli') {
+		return;
+	}
+	wp_redirect($_SERVER['REQUEST_URI']);
+	exit();
+}
+
+
+/**
+ * Check for and include ACF Pro files
+ */
+if(!class_exists('acf') &&  file_exists(get_stylesheet_directory() . '/vendor/advanced-custom-fields-pro/acf.php')) {
+
+	include_once(get_stylesheet_directory() . '/vendor/advanced-custom-fields-pro/acf.php');
+
+	// Set ACF path
+	add_filter('acf/settings/path', function ($path) {
+		$path = get_stylesheet_directory() . '/vendor/advanced-custom-fields-pro/';
+		return $path; 
+	});
+
+	// Set ACF dir
+	add_filter('acf/settings/dir', function ($dir) {
+		$dir = get_stylesheet_directory_uri() . '/vendor/advanced-custom-fields-pro/';
+		return $dir; 
+	});
+
+	// Hide ACF menu item
+	// TODO: Update this conditional to use defined constant from wp_config.php
+	if(false) {
+		add_filter('acf/settings/show_admin', '__return_false');
+	}
 }
