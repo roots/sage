963b1d0701b738fc1ce2602229faee37d3edaa4c
Greg <greg@aorweb.ca>
2017-06-01 16:49:44 -0300

    Add helper function to check if a post is in a decendent category of a target category

--- app/helpers.php
+++ app/helpers.php
@@ -174,3 +174,22 @@ function the_post_pagination($before = '', $after = '') {
     }
     echo '</ul></nav>'.$after."";
 }
+
+/**
+ * Tests if any of a post's assigned categories are descendants of target categories
+ * 
+ * @param int|array $cats The target categories. Integer ID or array of integer IDs
+ * @param int|object $_post The post. Omit to test the current post in the Loop or main query
+ * @return bool True if at least 1 of the post's categories is a descendant of any of the target categories
+ * @link http://codex.wordpress.org/Function_Reference/in_category#Testing_if_a_post_is_in_a_descendant_category
+ */
+
+function in_descendant_category( $cats, $_post = null ) {
+    foreach ((array) $cats as $cat) {
+        // get_term_children() accepts integer ID only
+        $descendants = get_term_children((int) $cat, 'category');
+        if ($descendants && in_category($descendants, $_post))
+                return true;
+    }
+    return false;
+}
