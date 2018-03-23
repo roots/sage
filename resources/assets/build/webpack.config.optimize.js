963e7a4a6502b8ff02d09eef07cbc991f3c3c455
hambos22 <hambos22@gmail.com>
2018-03-23 12:10:09 +0200

    Fix default SVG optimisation configuration

--- resources/assets/build/webpack.config.optimize.js
+++ resources/assets/build/webpack.config.optimize.js
@@ -11,7 +11,9 @@ module.exports = {
       optipng: { optimizationLevel: 7 },
       gifsicle: { optimizationLevel: 3 },
       pngquant: { quality: '65-90', speed: 4 },
-      svgo: { removeUnknownsAndDefaults: false, cleanupIDs: false },
+      svgo: {
+        plugins: [{ removeUnknownsAndDefaults: false }, { cleanupIDs: false }],
+      },
       plugins: [imageminMozjpeg({ quality: 75 })],
       disable: (config.enabled.watcher),
     }),
