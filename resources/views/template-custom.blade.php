be1b8887f44e40b85d9e3ab226fe0a38d869bb12
Charalambos Poulikidis <hambos22@gmail.com>
2018-03-28 01:38:28 +0300

    changed inline @php directive to full form (#2042)
    closes #2012

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -5,7 +5,7 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
+  @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
   @endwhile
