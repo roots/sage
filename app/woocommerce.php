043aff759600e0d8c4eed0169ffd652d5acb23a8
Charalambos Poulikidis <hambos22@gmail.com>
2018-04-13 14:17:37 +0300

    Removed dummy function

--- app/woocommerce.php
+++ app/woocommerce.php
@@ -5,7 +5,3 @@ namespace App;
 if (class_exists('woocommerce')  && !is_admin()) {
 	add_action('woocommerce_init', ['App\SageWoo\Bootstrap', 'get_instance']);
 }
-
-function test(){
-	echo 'true';
-}
