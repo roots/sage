31337ee3234d0c2b67aa16c9f9c013b8c864fc1d
Diego Betto <diego-betto@users.noreply.github.com>
2018-04-18 13:00:39 +0200

    Add version to main.js and main.css

--- app/setup.php
+++ app/setup.php
@@ -11,8 +11,19 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    wp_enqueue_style(
+        'sage/main.css', 
+        asset_path('styles/main.css'), 
+        false, 
+        filemtime(get_template_directory().'/../dist/styles/main.css') 
+    );
+    wp_enqueue_script(
+        'sage/main.js', 
+        asset_path('scripts/main.js'), 
+        ['jquery'], 
+        filemtime(get_template_directory().'/../dist/scripts/main.js'), 
+        true
+    );
 }, 100);
 
 /**
