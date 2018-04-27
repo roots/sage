b08db80c7b27169c2219c35484c1f6fbe5f4f10e
Valentino Cossar <valentinocossar@gmail.com>
2018-04-27 15:50:26 +0200

    Remove useless whitespace

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,7 +5,7 @@
 
   @if (!have_posts())
     <div class="alert alert-warning">
-      {{  __('Sorry, no results were found.', 'sage') }}
+      {{ __('Sorry, no results were found.', 'sage') }}
     </div>
     {!! get_search_form(false) !!}
   @endif
