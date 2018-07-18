ca66c0c43d4645b2bcf946f6b38f0fbfbe587597
Daniel Roe <daniel@concision.co.uk>
2018-07-18 20:52:39 +0100

    Fix missing comment-reply JS

--- app/setup.php
+++ app/setup.php
@@ -13,6 +13,10 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+
+    if (is_single() && comments_open() && get_option('thread_comments')) {
+      wp_enqueue_script('comment-reply');
+    }
 }, 100);
 
 /**
