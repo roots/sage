90de08f986d61507e3214fb7f4428ede02243137
Daniel Mejta <daniel@mejta.net>
2018-07-30 13:45:27 +0200

    Refactoring

--- app/woocommerce.php
+++ app/woocommerce.php
@@ -8,19 +8,13 @@ if (defined('WC_ABSPATH')) {
     });
 
     add_filter('template_include', function ($template) {
-        if (strpos($template, WC_ABSPATH) === 0) {
-            $check_template = 'woocommerce/' . str_replace(WC_ABSPATH . 'templates/', '', $template);
-            $template = locate_template($check_template) ?: $template;
-        }
-        return $template;
+        return strpos($template, WC_ABSPATH) === -1
+            ? $template
+            : locate_template('woocommerce/' . str_replace(WC_ABSPATH . 'templates/', '', $template)) ?: $template;
     }, 100, 1);
 
     add_filter('wc_get_template_part', function ($template, $slug, $name) {
-        $theme_template = locate_template([
-            'woocommerce/' . str_replace(WC_ABSPATH . 'templates/', '', $template),
-            'woocommerce/' . $slug . '-' . $name,
-            'woocommerce/' . $name,
-        ]);
+        $theme_template = locate_template('woocommerce/' . str_replace(WC_ABSPATH . 'templates/', '', $template));
         return $theme_template ? template_path($theme_template) : $template;
     }, 100, 3);
 
