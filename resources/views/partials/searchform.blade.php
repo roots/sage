b41047f5a1e611cda4586a7cc6c0d654b9ca1b5e
MWDelaney <michael.delaney@gmail.com>
2018-08-05 21:15:08 -0400

    Add text domain

--- resources/views/partials/searchform.blade.php
+++ resources/views/partials/searchform.blade.php
@@ -1,7 +1,7 @@
-<form role="search" method="get" class="search-form" action="{{ esc_url(home_url( '/') ) }}">
+<form role="search" method="get" class="search-form" action="{{ esc_url(home_url('/')) }}">
   <label>
-    <span class="screen-reader-text">{{ _x('Search for:', 'label') }}</span>
-    <input type="search" class="search-field" placeholder="{!! esc_attr_x('Search &hellip;', 'placeholder') !!}" value="{{ get_search_query() }}" name="s">
+    <span class="screen-reader-text">{{ _x('Search for:', 'label', 'sage') }}</span>
+    <input type="search" class="search-field" placeholder="{!! esc_attr_x('Search &hellip;', 'placeholder', 'sage') !!}" value="{{ get_search_query() }}" name="s">
   </label>
-  <input type="submit" class="search-submit" value="{{ esc_attr_x('Search', 'submit button') }}">
+  <input type="submit" class="search-submit" value="{{ esc_attr_x('Search', 'submit button', 'sage') }}">
 </form>
