57f2ba22ee80c65c51d505771523c78e912a20db
ztvMark <10190162+ztvMark@users.noreply.github.com>
2018-08-31 21:58:34 -0500

    best design
    an utf-8 icon is added,
    a display block is added,
    id #searchform for easy editing with css,
    placeholder is added with translation option

--- resources/views/partials/searchform.blade.php
+++ resources/views/partials/searchform.blade.php
@@ -1,7 +1,12 @@
-<form role="search" method="get" class="search-form" action="{{ $sf_action }}">
-  <label>
-    <span class="screen-reader-text">{{ $sf_screen_reader_text }}</span>
-    <input type="search" class="search-field" placeholder="{!! $sf_placeholder !!}" value="{{ $sf_current_query }}" name="s">
-  </label>
-  <input type="submit" class="search-submit" value="{{ $sf_submit_text }}">
+<form method="get" id="searchform" action="{{ $sf_action }}" role="search">
+	<label class="screen-reader-text" for="s">{{ $sf_screen_reader_text }}</label>
+	<div class="input-group">
+		<input class="field form-control" id="s" name="s" type="search"
+			placeholder="<?php esc_attr_e( 'Search &hellip;', 'sage' ); ?>" style="height: 2.6rem;">
+		<span class="input-group-btn">
+			<button class="btn btn-primary m-0 text-white rounded-0" type="submit" style="height: 2.6rem;">
+				➤
+			</button>
+		</span>
+	</div>
 </form>
