7773e7db41535418162cc6632cd88ea88223211f
Ben Word <ben@benword.com>
2018-11-16 11:39:07 -0700

    Double quotes for styles

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   'extends': 'stylelint-config-standard',
   'rules': {
     'no-empty-source': null,
-    'string-quotes': 'single',
+    'string-quotes': 'double',
     'at-rule-no-unknown': [
       true,
       {
