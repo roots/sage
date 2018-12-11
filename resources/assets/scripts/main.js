aa5ec758c94ba3cf29d4bc7ba112f0ba48a44c0e
Joi Glifberg <joi.glifberg@gmail.com>
2018-12-11 11:00:33 +0100

    Remove about us and home js routes.

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -7,17 +7,11 @@ import './autoload/**/*'
 // import local dependencies
 import Router from './util/Router';
 import common from './routes/common';
-import home from './routes/home';
-import aboutUs from './routes/about';
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
   // All pages
   common,
-  // Home page
-  home,
-  // About Us page, note the change from about-us to aboutUs.
-  aboutUs,
 });
 
 // Load Events
