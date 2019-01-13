db6aeee010884a906090cb6776fb53525aff1165
Kelly Mears <code@kellymears.me>
2019-01-12 17:59:08 -0800

    Gutenberg setup.php configurables

--- app/setup.php
+++ app/setup.php
@@ -34,6 +34,88 @@ add_action('after_setup_theme', function () {
     add_theme_support('soil-relative-urls');
 
     /**
+     * Add theme support for Wide Alignment
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#wide-alignment
+     */
+    add_theme_support( 'align-wide' );
+
+    /**
+     * Add editor styles
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#editor-styles
+     */
+    add_theme_support('editor-styles');
+    // add_theme_support( 'dark-editor-styles');
+    // add_editor_style( 'style-editor.css' );
+
+    /**
+     * Enable responsive embeds
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#responsive-embedded-content
+     */
+    add_theme_support( 'responsive-embeds' );
+
+    /**
+     * Enable Editor color palette support
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#block-color-palettes
+     */
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => __( 'strong magenta', 'sage' ),
+            'slug' => 'strong-magenta',
+            'color' => '#a156b4',
+        ),
+        array(
+            'name' => __( 'light grayish magenta', 'sage' ),
+            'slug' => 'light-grayish-magenta',
+            'color' => '#d0a5db',
+        ),
+        array(
+            'name' => __( 'very light gray', 'sage' ),
+            'slug' => 'very-light-gray',
+            'color' => '#eee',
+        ),
+        array(
+            'name' => __( 'very dark gray', 'sage' ),
+            'slug' => 'very-dark-gray',
+            'color' => '#444',
+        ),
+    ));
+
+    /**
+     * Enable Editor font size support
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#block-color-palettes
+     */
+    add_theme_support( 'editor-font-sizes', array(
+        array(
+            'name' => __( 'Small', 'sage' ),
+            'size' => 12,
+            'slug' => 'small'
+        ),
+        array(
+            'name' => __( 'Normal', 'sage' ),
+            'size' => 16,
+            'slug' => 'normal'
+        ),
+        array(
+            'name' => __( 'Large', 'sage' ),
+            'size' => 36,
+            'slug' => 'large'
+        ),
+        array(
+            'name' => __( 'Huge', 'sage' ),
+            'size' => 50,
+            'slug' => 'huge'
+        )
+    ));
+
+    /**
+     * Dequeue Gutenberg CSS
+     * @link https://wordpress.org/gutenberg/?s=dequeue (404)
+     */
+    add_action( 'wp_enqueue_scripts', function() {
+        wp_dequeue_style('wp-block-library');
+    }, 100);
+
+    /**
      * Enable plugins to manage the document title
      * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
      */
