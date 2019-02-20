3aa6a50a56d8088e61066425439173bbce5db65c
Kelly Mears <developers@tinypixel.io>
2019-02-19 20:07:20 -0800

    MODIFY: rest of gutenberg styles. ADDED: wp-block-media support.

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,4 +1,6 @@
-<div class="gutenberg">
-  @php the_content() @endphp
+<div class="wp-blocks">
+  <div class="entry-content">
+    @php the_content() @endphp
+  </div>
   {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
 </div>
