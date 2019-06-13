29e389f43803119d554160f7377ba17fab8255ff
Nathan Knowler <nathan@knowler.me>
2019-06-13 02:38:52 -0600

    Just use standard arrays + null coalesce ⛳

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,3 @@
-<div class="my-4 px-3 py-2 border rounded-sm {{ collect(['success' => 'bg-green-200 text-green-800 border-green-500', 'warning' => 'bg-yellow-200 text-yellow-800 border-yellow-500', 'error' => 'bg-red-200 text-red-700 border-red-500'])->get($type, 'bg-indigo-200 text-indigo-800 border-indigo-500') }}">
+<div class="my-4 px-3 py-2 border rounded-sm {{ ['success' => 'bg-green-200 text-green-800 border-green-500', 'warning' => 'bg-yellow-200 text-yellow-800 border-yellow-500', 'error' => 'bg-red-200 text-red-700 border-red-500'][$type] ?? 'bg-indigo-200 text-indigo-800 border-indigo-500' }}">
   <p>{{ $slot }}</p>
 </div>
