3c5e0e6d202e2caf8b4b3c972ddae187d6571922
Brandon <brandon@tendency.me>
2019-07-22 17:53:48 -0500

    Add timezone, globals, and locale app config [ci skip]

--- config/app.php
+++ config/app.php
@@ -42,6 +42,19 @@ return [
     */
 
     'debug' => env('WP_DEBUG', false),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
+    |
+    */
+    
+    'timezone' => get_option('timezone_string', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,6 +68,45 @@ return [
     */
 
     'preflight' => env('WP_ENV', 'production') !== 'production',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Global Helpers
+    |--------------------------------------------------------------------------
+    |
+    | This value enables the usage of various Acorn helpers without the need
+    | to specify a namespace. This defaults to false as to not pollute the
+    | global namespace.
+    |
+    */
+
+    'globals' => false,
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
+    
+    'locale' => get_locale(),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Fallback Locale
+    |--------------------------------------------------------------------------
+    |
+    | The fallback locale determines the locale to use when the current one
+    | is not available. You may change the value to correspond to any of
+    | the language folders that are provided through your application.
+    |
+    */
+    
+    'fallback_locale' => 'en',
 
     /*
     |--------------------------------------------------------------------------
