45f4884c68687391513285bc403828d65e09849c
Brandon <brandon@tendency.me>
2019-08-03 05:54:46 -0500

    Set `wp_nav_menu` echo to `false` (#2278)

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
 
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
       @endif
     </nav>
   </div>
