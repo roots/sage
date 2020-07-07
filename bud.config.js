3409717aa7c3af967f7dad6df10645133c0097be
Kelly Mears <developers@tinypixel.dev>
2020-07-07 14:18:42 -0700

    formatting. move enabled checks to fn params. v0.0.20 branch of @roots/bud-support

--- bud.config.js
+++ bud.config.js
@@ -1,36 +1,17 @@
 /**
- * Bud - asset compilation framework.
- *
- * @const {Object.<Bud>} bud
+ * Build theme assets.
  */
 
 const bud = require('@roots/budpack');
 
 /**
- * Set source assets directory.
- *
- * @param {string} directory - project source dir.
+ * Set source directory.
  */
 
 bud.srcPath('resources/assets');
 
 /**
- * Set compiled assets directory.
- *
- * @param {string} directory - project dist dir.
- */
-
-bud.distPath('dist');
-
-/**
- * Resolve modules through webpack aliases. Useful for situations
- * that may otherwise require brittle relative paths.
- *
- * @example
- *  `import {myScript} from '../../../components/myScript'`
- *  `import {myScript} from '@scripts/components/myScript'`
- *
- * @param {Object.<string, array>}
+ * Set webpack aliases.
  */
 
 bud.alias({
@@ -41,9 +22,7 @@ bud.alias({
 });
 
 /**
- * Automatically load modules instead of needing to explicitly import them.
- *
- * @param {Object.<string, array>}
+ * Autoload modules.
  */
 
 bud.auto({
@@ -51,104 +30,82 @@ bud.auto({
 });
 
 /**
- * Configure live reloading.
- *
- * @property {string}  [host='localhost']
- * @property {number}  [port=3000]
- * @property {boolean} [enabled=!bud.inProduction]
- * @property {string}  [proxy='']
+ * Configure live reload.
  */
 
 bud.sync({
+  enabled: !bud.inProduction,
   proxy: 'http://bud-sandbox.valet',
 });
 
 /**
- * Enable or disable filename hashing on compiled assets.
- *
- * @param {boolean} enabled - filename hashing enabled.
+ * Filename hashing.
  */
 
 bud.hash(false);
 
 /**
- * WordPress dependency manifest
- *
+ * Generate a WordPress dependency manifest.
+ * @see  @wordpress/dependency-manifest-webpack-plugin
  * @todo splitChunks breaks this
  */
-bud.wpManifest(false);
+
+bud.dependencyManifest()
+
+/**
+ * Optimize performance by inlining code split from bundles.
+ */
+
+bud.inlineManifest({
+  enabled: true,
+  name: 'runtime',
+});
 
 /**
  * Compile application JavaScript.
- *
- * @param  {string} name    - output name.
- * @param  {array}  entries - assets to include in the bundle.
  */
 
 bud
-  .bundle('scripts/editor', ['scripts/editor.js'])
-  .bundle('scripts/app', ['scripts/app.js'])
-  .bundle('scripts/customizer', ['scripts/customizer.js'])
+  .bundle('scripts/editor', [bud.src('scripts/editor.js')])
+  .bundle('scripts/app', [bud.src('scripts/app.js')])
+  .bundle('scripts/customizer', [bud.src('scripts/customizer.js')])
 
 /**
  * Compile application SCSS.
- *
- * @param  {string} name    - output name.
- * @param  {array}  entries - assets to include in the bundle.
  */
 
 bud
-  .bundle('styles/editor', ['styles/editor.scss'])
-  .bundle('styles/app', ['styles/app.scss'])
+  .bundle('styles/editor', [bud.src('styles/editor.scss')])
+  .bundle('styles/app', [bud.src('styles/app.scss')])
 
 /**
  * Copy static assets.
- *
- * @param {string} src  - copy from src relative dir
- * @param {string} dist - copy to dist relative dir
  */
 
 bud
-  .copyAll('images', 'images')
-  .copyAll('fonts', 'fonts');
+  .copyAll(bud.src('images'), bud.dist('images'))
+  .copyAll(bud.src('fonts'), bud.dist('fonts'));
 
 /**
  * Configure Babel and PostCSS.
- *
- * You may utilize a standard babel.config.js and/or postcss.config.js
- * file located in the project root, either alongside or in lieue of
- * these configurations.
- *
- * Conflicts between config sources will be resolved in favor of
- * the ones located in this file.
- *
- * @see https://babeljs.io/docs/en/configuration
- * @see https://github.com/postcss/postcss#options
  */
 
 bud
   .babel(bud.preset('babel/preset-wp'))
-  .postcss(bud.preset('postcss'));
+  .postCss(bud.preset('postcss'));
 
 /**
  * Translate strings from JS source assets.
- *
- * If you are already using `yarn translate` then there is no
- * reason to run this in addition to that. You may remove it.
- *
- * @param {string} potfile - project relative path to translation file
  */
 
 bud.translate('resources/languages/sage.pot');
 
 /**
  * Purge unused CSS from production builds.
- *
- * @see https://purgecss.com/guides/wordpress.html
- * @see https://purgecss.com/configuration.html
  */
 
-bud.inProduction && bud.purge({
+bud.purge({
+  enabled: bud.inProduction,
   content: [bud.project('resources/views/**/*.blade.php')],
   allow: require('purgecss-with-wordpress').whitelist,
   allowPatterns: require('purgecss-with-wordpress').whitelistPatterns,
@@ -156,7 +113,6 @@ bud.inProduction && bud.purge({
 
 /**
  * Export finalized configuration.
- * @exports {Object.<Bud>}
  */
 
 module.exports = bud;
