9f51c7df5bded072da12e2decea1b3b10c3307c1
Brandon <brandon@tendency.me>
2020-08-20 09:49:57 -0500

    chore(mix): Improve webpack.mix.js formatting

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,31 +14,31 @@ require('laravel-mix-copy-watched');
  |
  */
 
-mix.setPublicPath('./dist')
-   .browserSync('sage.test');
+mix
+  .setPublicPath('./dist')
+  .browserSync('sage.test');
 
-mix.sass('resources/assets/styles/app.scss', 'styles')
-   .sass('resources/assets/styles/editor.scss', 'styles')
-   .purgeCss({
-     whitelist: require('purgecss-with-wordpress').whitelist,
-     whitelistPatterns: require('purgecss-with-wordpress').whitelistPatterns,
-   });
+mix
+  .sass('resources/assets/styles/app.scss', 'styles')
+  .sass('resources/assets/styles/editor.scss', 'styles')
+  .purgeCss({
+    extend: { content: [path.join(__dirname, 'index.php')] },
+    whitelist: require('purgecss-with-wordpress').whitelist,
+    whitelistPatterns: require('purgecss-with-wordpress').whitelistPatterns,
+  });
 
-mix.js('resources/assets/scripts/app.js', 'scripts')
-   .js('resources/assets/scripts/customizer.js', 'scripts')
-   .blocks('resources/assets/scripts/editor.js', 'scripts')
-   .extract();
+mix
+  .js('resources/assets/scripts/app.js', 'scripts')
+  .js('resources/assets/scripts/customizer.js', 'scripts')
+  .blocks('resources/assets/scripts/editor.js', 'scripts')
+  .extract();
 
-mix.copyWatched('resources/assets/images/**', 'dist/images')
-   .copyWatched('resources/assets/fonts/**', 'dist/fonts');
+mix
+  .copyWatched('resources/assets/images/**', 'dist/images')
+  .copyWatched('resources/assets/fonts/**', 'dist/fonts');
 
-mix.autoload({
-  jquery: ['$', 'window.jQuery'],
-});
-
-mix.options({
-  processCssUrls: false,
-});
-
-mix.sourceMaps(false, 'source-map')
-   .version();
+mix
+  .autoload({ jquery: ['$', 'window.jQuery'] })
+  .options({ processCssUrls: false })
+  .sourceMaps(false, 'source-map')
+  .version();
