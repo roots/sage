b87418958bdb72f33102446d0ad6d0c8ba7ed969
Ben Word <ben@benword.com>
2020-08-30 09:57:28 -0600

    Soil: Activation updates for latest version

--- app/setup.php
+++ app/setup.php
@@ -52,10 +52,12 @@ add_action('after_setup_theme', function () {
      * Enable features from Soil when plugin is activated
      * @link https://roots.io/plugins/soil/
      */
-    add_theme_support('soil-clean-up');
-    add_theme_support('soil-nav-walker');
-    add_theme_support('soil-nice-search');
-    add_theme_support('soil-relative-urls');
+    add_theme_support('soil', [
+        'clean-up',
+        'nav-walker',
+        'nice-search',
+        'relative-urls'
+    ]);
 
     /**
      * Enable plugins to manage the document title
