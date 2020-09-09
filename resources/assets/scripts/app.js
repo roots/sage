879f12529e1a847309a3d27be7df6911a01a44e9
Kelly Mears <developers@tinypixel.dev>
2020-09-09 14:05:01 -0700

    Update build to next.13
    Also adds some about 4 lines of helpful hot reload boilerplate.

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -7,3 +7,15 @@ import 'bootstrap';
 $(document).ready(() => {
   // console.log('Hello world');
 });
+
+/**
+ * Accept module updates
+ *
+ * @see https://webpack.js.org/api/hot-module-replacement
+ * @see https://webpack.js.org/api/hot-module-replacement/#accept-self
+ */
+if (module.hot) {
+  module.hot.accept(err =>
+    console.err(err)
+  );
+}
