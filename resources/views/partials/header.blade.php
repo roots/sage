867e6ae7bc296911003834c2425c5cf18335bab9
Brandon <brandon@tendency.me>
2021-01-14 14:18:05 -0600

    chore(view): Remove redundant selector

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,11 @@
 <header class="banner">
-  <div class="max-w-3xl mx-auto">
-    <a class="brand" href="{{ home_url('/') }}">
-      {{ $siteName }}
-    </a>
+  <a class="brand" href="{{ home_url('/') }}">
+    {{ $siteName }}
+  </a>
 
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-      @endif
-    </nav>
-  </div>
+  <nav class="nav-primary">
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+    @endif
+  </nav>
 </header>
