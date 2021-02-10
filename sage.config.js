84f97174616e3d1c4a26ba604c6a9a2826bb611f
Kelly Mears <developers@tinypixel.dev>
2021-02-09 17:56:49 -0800

    [@roots/sage] add ts support for development (hmr)

--- sage.config.js
+++ sage.config.js
@@ -2,7 +2,7 @@
 const Sage = require('@roots/sage');
 
 Sage.bootstrap()
-  .entry('app', ['styles/app.scss', 'scripts/app.js'])
-  .entry('editor', ['styles/editor.scss', 'scripts/editor.js'])
-  .entry('customizer', ['scripts/customizer.js'])
+  .entry('app', ['@styles/app.scss', '@scripts/app.js'])
+  .entry('editor', ['@styles/editor.scss', '@scripts/editor.js'])
+  .entry('customizer', ['@scripts/customizer.js'])
   .run();
