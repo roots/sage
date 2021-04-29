b4f654aa7a14e7a47723fc46957ead7e7aa52e4a
Joshua Fredrickson <joshua@orangepineapple.com>
2021-04-29 12:59:59 -0500

    (enhance)theme: Disable full-site editing support

--- app/setup.php
+++ app/setup.php
@@ -60,6 +60,13 @@ add_action('after_setup_theme', function () {
     ]);
 
     /**
+     * Disable full-site editing support.
+     *
+     * @link https://wptavern.com/gutenberg-10-5-embeds-pdfs-adds-verse-block-color-options-and-introduces-new-patterns
+     */
+    remove_theme_support('block-templates');
+
+    /**
      * Register the navigation menus.
      * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
      */
