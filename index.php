b843074e312ff272e5783c1ed82c2d39b67ef9a2
Nathan Knowler <nathan@impress.org>
2021-06-16 09:25:27 -0500

    feat: remove x-ua-compatible tag
    This is redundant in IE 11.

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head(); ?>
   </head>
