6d40a61bbbaf447998bba9026f6d30c5c4b868e9
strarsis <strarsis@gmail.com>
2021-08-27 18:01:19 +0200

    Use Soil 4.x theme support, as in Sage 10. Improve comments.

--- app/setup.php
+++ app/setup.php
@@ -23,15 +23,19 @@ add_action('wp_enqueue_scripts', function () {
  * Theme setup
  */
 add_action('after_setup_theme', function () {
-    /**
-     * Enable features from Soil when plugin is activated
+
+	/**
+     * Enable features from the Soil plugin if activated.
+	 * for Soil 4.x
      * @link https://roots.io/plugins/soil/
      */
-    add_theme_support('soil-clean-up');
-    add_theme_support('soil-jquery-cdn');
-    add_theme_support('soil-nav-walker');
-    add_theme_support('soil-nice-search');
-    add_theme_support('soil-relative-urls');
+    add_theme_support('soil', [
+        'clean-up',
+        'nav-walker',
+        'nice-search',
+        'relative-urls'
+    ]);
+
 
     /**
      * Enable plugins to manage the document title
