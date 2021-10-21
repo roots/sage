385452469a9aac44b686f05515c613738629ee43
Brandon <brandon@tendency.me>
2021-10-21 15:19:13 -0500

    chore(views): nitpick blade syntax

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
   </a>
 
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{wp_get_nav_menu_name('primary_navigation')}}">
+    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     </nav>
   @endif
