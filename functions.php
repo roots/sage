14d60c9d531390828b3a2792316a27394c52a060
QWp6t <hi@qwp6t.me>
2021-11-18 04:09:36 -0800

    feat(zeroconfig): Add support for zero-config

--- functions.php
+++ functions.php
@@ -19,13 +19,50 @@ require $composer;
 
 /*
 |--------------------------------------------------------------------------
-| Run The Theme
+| Register The Bootloader
 |--------------------------------------------------------------------------
 |
-| Once we have the theme booted, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| The first thing we will do is schedule a new Acorn application container
+| to boot when WordPress is finished loading the theme. The application
+| serves as the "glue" for all the components of Laravel and is
+| the IoC container for the system binding all of the various parts.
 |
 */
 
-require_once __DIR__ . '/bootstrap/app.php';
+\Roots\bootloader();
+
+/*
+|--------------------------------------------------------------------------
+| Register Sage Theme Files
+|--------------------------------------------------------------------------
+|
+| Out of the box, Sage ships with categorically named theme files
+| containing common functionality and setup to be bootstrapped with your
+| theme. Simply add (or remove) files from the array below to change what
+| is registered alongside Sage.
+|
+*/
+
+collect(['helpers', 'setup', 'filters', 'admin'])
+    ->each(function ($file) {
+        if (! locate_template($file = "app/{$file}.php", true, true)) {
+            wp_die(
+                /* translators: %s is replaced with the relative file path */
+                sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file)
+            );
+        }
+    });
+
+/*
+|--------------------------------------------------------------------------
+| Enable Sage Theme Support
+|--------------------------------------------------------------------------
+|
+| Once our theme files are registered and available for use, we are almost
+| ready to boot our application. But first, we need to signal to Acorn
+| that we will need to initialize the necessary service providers built in
+| for Sage when booting.
+|
+*/
+
+add_theme_support('sage');
