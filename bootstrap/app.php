09234d21f07072872cbae06e0c4893b5a5fc0443
Ben Word <ben@benword.com>
2021-12-19 09:19:09 -0600

    Remove customizer starter (#2904)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@
 |
 */
 
-collect(['helpers', 'setup', 'filters', 'admin'])
+collect(['helpers', 'setup', 'filters'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
