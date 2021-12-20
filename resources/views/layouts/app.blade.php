358ae364d7a838b1712b652b1dae71ce929a3241
Ben Word <ben@benword.com>
2021-12-20 08:32:50 -0600

    Remove all base styles followup (#2900)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,17 @@
-<div class="max-w-3xl mx-auto">
+<a class="sr-only focus:not-sr-only" href="#main">
+  {{ __('Skip to content') }}
+</a>
 
-  <a class="sr-only focus:not-sr-only" href="#main">
-    {{ __('Skip to content') }}
-  </a>
+@include('partials.header')
 
-  @include('partials.header')
+  <main id="main" class="main">
+    @yield('content')
+  </main>
 
-    <main id="main" class="py-8 prose main">
-      @yield('content')
-    </main>
+  @hasSection('sidebar')
+    <aside class="sidebar">
+      @yield('sidebar')
+    </aside>
+  @endif
 
-    @hasSection('sidebar')
-      <aside class="sidebar">
-        @yield('sidebar')
-      </aside>
-    @endif
-
-  @include('partials.footer')
-</div>
+@include('partials.footer')
