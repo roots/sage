7f70c1611478659862318a5fb03cc2dd947c86cc
Kelly Mears <developers@tinypixel.dev>
2022-01-26 18:50:35 -0800

    actually 5.2.0 works well enough

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     require.resolve('@roots/sage/eslint-config'),
-    require.resolve('@roots/bud-typescript/eslint-config'),
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
