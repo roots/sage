7de60b1f7542e93848e412c1df363fe95ec9e57d
Kelly Mears <developers@tinypixel.dev>
2022-02-22 16:17:38 -0800

    explicitly specify dev server

--- bud.config.js
+++ bud.config.js
@@ -3,7 +3,7 @@
  *
  * @param {bud} app
  */
-module.exports = (app) =>
+module.exports = async (app) => {
   app
     /**
      * Application entrypoints
@@ -19,7 +19,7 @@ module.exports = (app) =>
      * These files should be processed as part of the build
      * even if they are not explicitly imported in application assets.
      */
-    .assets('@images')
+    .assets('images')
 
     /**
      * These files will trigger a full page reload
@@ -36,4 +36,10 @@ module.exports = (app) =>
      *
      * This is your local dev server.
      */
-    .proxy('http://tinypixel.sabo');
+    .proxy('http://example.test')
+
+    /**
+     * Development URL
+     */
+    .serve('http://example.test:3000');
+};
