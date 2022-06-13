da203125c633763db9959b92c5b37b5453c111d1
Przemysław Hernik <przemek.hernik@coditive.com>
2022-06-13 16:59:56 +0200

    Change proxy URL

--- bud.config.js
+++ bud.config.js
@@ -32,7 +32,7 @@ module.exports = async (app) => {
      *
      * This should be the URL you use to visit your local development server.
      */
-    .proxy('http://example.test')
+    .proxy('http://firestarter.test')
 
     /**
      * Development URL to be used in the browser.
