16d99623a90eb4d48fc97bbc8b2121f64c12d9f9
Ben Word <ben@benword.com>
2022-06-20 14:31:26 -0500

    🔧 bud config: set the public path by default (#3064)

--- bud.config.js
+++ bud.config.js
@@ -37,5 +37,10 @@ module.exports = async (app) => {
     /**
      * Development URL to be used in the browser.
      */
-    .serve('http://0.0.0.0:3000');
+    .serve('http://0.0.0.0:3000')
+
+    /**
+     * Relative path to the public directory.
+     */
+    .setPublicPath('/app/themes/sage/public/');
 };
