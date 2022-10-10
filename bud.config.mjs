89b1e6ee583d130f20af542203c1875211a70f22
Rob Voss <rob.voss@digerati.io>
2022-10-10 09:56:43 -0400

    C

--- bud.config.mjs
+++ bud.config.mjs
@@ -47,33 +47,96 @@ export default async (app) => {
      * @note This overwrites `theme.json` on every build.
      */
     .wpjson
-      .settings({
-        color: {
-          custom: false,
-          customGradient: false,
-          defaultPalette: false,
-          defaultGradients: false,
-        },
-        custom: {
-          spacing: {},
-          typography: {
-            'font-size': {},
-            'line-height': {},
+    .settings({
+      blocks: {
+        'core/button': {
+          border: {
+            radius: false
+          }
+        }
+      },
+      color: {
+        palette: [
+          {
+            slug: 'red',
+            color: '#D11141',
+            name: 'Red'
           },
-        },
-        spacing: {
-          padding: true,
-          units: ['px', '%', 'em', 'rem', 'vw', 'vh'],
-        },
+          {
+            slug: 'black',
+            color: '#111111',
+            name: 'Black'
+          }
+        ],
+        custom: false,
+        customGradient: false,
+        defaultPalette: false,
+        defaultGradients: false,
+      },
+      custom: {
+        spacing: {},
         typography: {
-          customFontSize: false,
+          'font-size': {},
+          'line-height': {},
         },
-      })
-      .enable()
+      },
+      spacing: {
+        padding: true,
+        units: ['px', '%', 'em', 'rem', 'vw', 'vh'],
+      },
+      typography: {
+        customFontSize: false,
+        fontSizes: [
+          {
+            name: 'Heading 1',
+            size: '56px',
+            slug: 'heading-1'
+          },
+          {
+            name: 'Heading 2',
+            size: '34px',
+            slug: 'heading-2'
+          },
+          {
+            name: 'Heading 3',
+            size: '24px',
+            slug: 'heading-3'
+          },
+          {
+            name: 'Normal',
+            size: '1rem',
+            slug: 'normal-font-size'
+          },
+          {
+            name: 'Primary Navigation',
+            size: '1rem',
+            slug: 'primary-nav-font-size'
+          },
+          {
+            name: 'Utility Navigation',
+            size: '0.75rem',
+            slug: 'utility-nav-font-size'
+          },
+          {
+            name: 'CTA',
+            size: '0.875rem',
+            slug: 'cta-font-size'
+          }
+        ],
+        fontFamilies: [
+          {
+            fontFamily: 'Noto Sans',
+            slug: 'noto-sans',
+            name: 'Noto Sans'
+          }
+        ]
+      },
+    })
+    .enable()
 
-    app.sass
-      .importGlobal([
-        '@src/styles/config',
-      ])
+  app.sass
+    .importGlobal([
+      '@src/styles/config',
+    ])
 
 };
