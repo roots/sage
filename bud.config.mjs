d52de9fcaa930f6d3a17b855659af2e03fb9715b
Budi <budimanfajarf@gmail.com>
2022-10-25 20:26:50 +0700

    Add .env to set proxy used in bud config

--- bud.config.mjs
+++ bud.config.mjs
@@ -29,7 +29,7 @@ export default async (app) => {
     /**
      * Proxy origin (`WP_HOME`)
      */
-    .proxy("http://example.test")
+    .proxy(process.env.PROXY || "http://example.test")
 
     /**
      * Development origin
