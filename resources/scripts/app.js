fe2885dd9ea6066147512d6b0ee822625739c464
Kelly Mears <developers@tinypixel.dev>
2022-12-23 10:53:52 -0800

    ✨ improve: js boilerplate

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,21 +1,13 @@
-import {domReady} from '@roots/sage/client';
+import { domReady } from '@roots/sage/client';
 
 /**
- * app.main
+ * Application entrypoint
  */
-const main = async (err) => {
-  if (err) {
-    // handle hmr errors
-    console.error(err);
-  }
-
-  // application code
-};
+domReady(async () => {
+  // ...
+});
 
 /**
- * Initialize
- *
- * @see https://webpack.js.org/api/hot-module-replacement
+ * @see {@link https://webpack.js.org/api/hot-module-replacement/}
  */
-domReady(main);
-import.meta.webpackHot?.accept(main);
+import.meta.webpackHot?.accept(console.error);
--- resources/scripts/editor.js
+++ resources/scripts/editor.js
@@ -1,27 +1,19 @@
-import {domReady} from '@roots/sage/client';
-import {registerBlockStyle, unregisterBlockStyle} from '@wordpress/blocks';
+import { domReady } from '@roots/sage/client';
+import { registerBlockStyle, unregisterBlockStyle } from '@wordpress/blocks';
 
 /**
- * editor.main
+ * Editor entrypoint
  */
-const main = (err) => {
-  if (err) {
-    // handle hmr errors
-    console.error(err);
-  }
-
+domReady(() => {
   unregisterBlockStyle('core/button', 'outline');
 
   registerBlockStyle('core/button', {
     name: 'outline',
     label: 'Outline',
   });
-};
+});
 
 /**
- * Initialize
- *
- * @see https://webpack.js.org/api/hot-module-replacement
+ * @see {@link https://webpack.js.org/api/hot-module-replacement/}
  */
-domReady(main);
-import.meta.webpackHot?.accept(main);
+import.meta.webpackHot?.accept(console.error);
