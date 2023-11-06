4fb52efcba6145aa030c8034d3d5ca863f4ca110
Jacek Gajewski <49660272+blift@users.noreply.github.com>
2023-11-06 16:02:32 +0100

    Update content-single.blade.php
    Check the wp_link_pages before render, to prevent empty footer element

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -11,9 +11,17 @@
     @php(the_content())
   </div>
 
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
+  @php($pagination_links = wp_link_pages([
+    'echo' => 0,
+    'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'),
+    'after' => '</p></nav>'
+  ]))
+
+  @if ($pagination_links)
+    <footer>
+      {!! $pagination_links !!}
+    </footer>
+  @endif
 
   @php(comments_template())
 </article>
