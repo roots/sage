96f32e6bc120ab527d303a50c129dbe9995872cd
Brandon <brandon@tendency.me>
2023-12-08 10:36:29 -0600

    🚨 Run phpcbf

--- app/View/Composers/Post.php
+++ app/View/Composers/Post.php
@@ -77,7 +77,7 @@ class Post extends Composer
     {
         return wp_link_pages([
             'echo' => 0,
-            'before' => '<p>'.__('Pages:', 'sage'),
+            'before' => '<p>' . __('Pages:', 'sage'),
             'after' => '</p>',
         ]);
     }
