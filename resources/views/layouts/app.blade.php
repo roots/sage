dd114a47981f5f7de301972bfc2f44d5f8a22ee9
Brandon <brandon@tendency.me>
2023-12-18 20:15:03 -0600

    🎨 Move default theme markup to the `app` layout 🐛 Fix uncontrollable HTML output (Fixes #3166)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,36 @@
-<a class="sr-only focus:not-sr-only" href="#main">
-  {{ __('Skip to content') }}
-</a>
+<!doctype html>
+<html @php(language_attributes())>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @php(wp_head())
+  </head>
 
-@include('sections.header')
+  <body @php(body_class())>
+    @php(wp_body_open())
+    @php(do_action('get_header'))
 
-  <main id="main" class="main">
-    @yield('content')
-  </main>
+    <div id="app">
+      <a class="sr-only focus:not-sr-only" href="#main">
+        {{ __('Skip to content') }}
+      </a>
 
-  @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-  @endif
+      @include('sections.header')
 
-@include('sections.footer')
+      <main id="main" class="main">
+        @yield('content')
+      </main>
+
+      @hasSection('sidebar')
+        <aside class="sidebar">
+          @yield('sidebar')
+        </aside>
+      @endif
+
+      @include('sections.footer')
+    </div>
+
+    @php(do_action('get_footer'))
+    @php(wp_footer())
+  </body>
+</html>
