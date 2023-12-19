b94e545e82f2f0e886a949462a152233308312eb
Brandon <brandon@tendency.me>
2023-12-18 20:20:26 -0600

    🔧 Remove `index.php` from the Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} config */
 const config = {
-  content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
+  content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
     extend: {
       colors: {}, // Extend Tailwind's default colors
