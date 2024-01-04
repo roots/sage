94a1787838f0490febe4f3543e3b69b41b750f6f
Brandon <brandon@tendency.me>
2024-01-04 00:30:48 -0600

    🩹 Move `get_header` hook above `wp_head()` (Fixes #3171)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @php(do_action('get_header'))
     @php(wp_head())
   </head>
 
   <body @php(body_class())>
     @php(wp_body_open())
-    @php(do_action('get_header'))
 
     <div id="app">
       <a class="sr-only focus:not-sr-only" href="#main">
