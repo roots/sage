a10fe894b75c7fd3da99b9a12d739a14fab4ba22
Ben Word <ben@benword.com>
2024-01-18 13:16:14 -0600

    Update resources/views/partials/content-page.blade.php
    Co-authored-by: Nathan Knowler <nathan@knowler.me>

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,7 +1,7 @@
 @php(the_content())
 
 @if ($pagination)
-  <nav class="page-nav" aria-label="Page navigation">
+  <nav class="page-nav" aria-label="Page">
     {!! $pagination !!}
   </nav>
 @endif
