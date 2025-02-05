78f98dcb1804702af912f6f7868376772ef67f72
Steve Jones <steve@codewithfeeling.com>
2025-02-05 18:53:06 +0000

    Compact fix for colour theme.json generation

--- resources/js/build/wordpress.js
+++ resources/js/build/wordpress.js
@@ -210,9 +210,8 @@ export function wordpressThemeJson({
         return;
       }
 
-      const startIndex = themeContent.indexOf('{') + 1;
       const endIndex = themeContent.lastIndexOf('}');
-      const rootContent = themeContent.slice(startIndex, endIndex === -1 ? undefined : endIndex);
+      const rootContent = themeContent.slice(themeContent.indexOf('{') + 1, endIndex === -1 ? undefined : endIndex);
       const colorVariables = {}
 
       const colorVarRegex = /--color-([^:]+):\s*([^;}]+)[;}]?/g
