f87f9e5bdda5de5133bfed28fe3581e0a5eb621d
Yasser Bal <yasserbal@icloud.com>
2025-06-06 08:15:20 +0200

    Acorn application now bootstraps in the after_setup_theme hook - No more 'translation loading errors in wp > 6.7' - Enables the use of Acorn's feature set

--- functions.php
+++ functions.php
@@ -19,42 +19,44 @@ if (! file_exists($composer = __DIR__.'/vendor/autoload.php')) {
 
 require $composer;
 
-/*
-|--------------------------------------------------------------------------
-| Register The Bootloader
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is schedule a new Acorn application container
-| to boot when WordPress is finished loading the theme. The application
-| serves as the "glue" for all the components of Laravel and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+add_action('after_setup_theme', function () {
+    /*
+    |--------------------------------------------------------------------------
+    | Register The Bootloader
+    |--------------------------------------------------------------------------
+    |
+    | The first thing we will do is schedule a new Acorn application container
+    | to boot when WordPress is finished loading the theme. The application
+    | serves as the "glue" for all the components of Laravel and is
+    | the IoC container for the system binding all of the various parts.
+    |
+    */
 
-Application::configure()
-    ->withProviders([
-        App\Providers\ThemeServiceProvider::class,
-    ])
-    ->boot();
+    Application::configure()
+        ->withProviders([
+            App\Providers\ThemeServiceProvider::class,
+        ])
+        ->boot();
 
-/*
-|--------------------------------------------------------------------------
-| Register Sage Theme Files
-|--------------------------------------------------------------------------
-|
-| Out of the box, Sage ships with categorically named theme files
-| containing common functionality and setup to be bootstrapped with your
-| theme. Simply add (or remove) files from the array below to change what
-| is registered alongside Sage.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Register Sage Theme Files
+    |--------------------------------------------------------------------------
+    |
+    | Out of the box, Sage ships with categorically named theme files
+    | containing common functionality and setup to be bootstrapped with your
+    | theme. Simply add (or remove) files from the array below to change what
+    | is registered alongside Sage.
+    |
+    */
 
-collect(['setup', 'filters'])
-    ->each(function ($file) {
-        if (! locate_template($file = "app/{$file}.php", true, true)) {
-            wp_die(
-                /* translators: %s is replaced with the relative file path */
-                sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file)
-            );
-        }
-    });
+    collect(['setup', 'filters'])
+        ->each(function ($file) {
+            if (! locate_template($file = "app/{$file}.php", true, true)) {
+                wp_die(
+                    /* translators: %s is replaced with the relative file path */
+                    sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file)
+                );
+            }
+        });
+}, 0);
